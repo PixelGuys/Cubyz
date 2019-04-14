08d9dd7de36aa64a11bccb098527cc90c3cfa02d
Randy <39484230+zenith391@users.noreply.github.com>
2019-04-14 16:20:40 +0200

    Quick Fix: Update to 0.3.1

--- cubyz-common/src/io/cubyz/Constants.java
+++ cubyz-common/src/io/cubyz/Constants.java
@@ -5,7 +5,7 @@ import io.cubyz.api.Side;
 public class Constants {
 
 	public static final String GAME_BUILD_TYPE = "alpha";
-	public static final String GAME_VERSION = "0.3";
+	public static final String GAME_VERSION = "0.3.1";
 	public static final String GAME_BRAND = "cubyz";
 	static Side currentSide = null;
 	
