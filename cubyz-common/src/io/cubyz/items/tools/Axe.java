db5c252db981e9a8f4ed8d1bb7d7af5664fef0a1
IntegratedQuantum <jahe788@gmail.com>
2019-10-20 11:34:57 +0200

    Upload Axe.java

---
+++ cubyz-common/src/io/cubyz/items/tools/Axe.java
@@ -0,0 +1,52 @@
+package io.cubyz.items.tools;
+
+import io.cubyz.base.init.MaterialInit;
+import io.cubyz.blocks.Block;
+import io.cubyz.blocks.Block.BlockClass;
+import io.cubyz.items.Item;
+import io.cubyz.items.ItemStack;
+
+public class Axe extends Tool {
+	private static final int HEAD = 300, BINDING = 50, HANDLE = 50;
+	private static final float baseSpeed = 2.0f;
+
+	public Axe(Material head, Material binding, Material handle) {
+		super(head, binding, handle, calculateSpeed(head, binding, handle), calculateDmg(head, binding, handle));
+		durability = maxDurability = head.headDurability + binding.bindingDurability + handle.handleDurability;
+		// The image is just an overlay of the part images:
+		texturePath = "assets/cubyz/textures/items/parts/"+handle.getRegistryID().getID()+"_handle.png#assets/cubyz/textures/items/parts/"+head.getRegistryID().getID()+"_axe_head.png#assets/cubyz/textures/items/parts/"+binding.getRegistryID().getID()+"_binding.png";
+	}
+	
+	private static float calculateSpeed(Material head, Material binding, Material handle) {
+		return head.miningSpeed * baseSpeed;
+	}
+	
+	private static float calculateDmg(Material head, Material binding, Material handle) {
+		return head.damage + 2;// An axe is better than shovel and pickaxe
+	}
+	
+	public static Item canCraft(ItemStack head, ItemStack binding, ItemStack handle) {
+		Material he = null, bi = null, ha = null;
+		if(head.getItem() != null)
+		for(Material ma : MaterialInit.MATERIALS) {
+			if(ma.getItems().containsKey(head.getItem()) && head.getAmount()*ma.getItems().get(head.getItem()) >= HEAD) {
+				he = ma;
+			}
+			if(ma.getItems().containsKey(binding.getItem()) && binding.getAmount()*ma.getItems().get(binding.getItem()) >= BINDING) {
+				bi = ma;
+			}
+			if(ma.getItems().containsKey(handle.getItem()) && handle.getAmount()*ma.getItems().get(handle.getItem()) >= HANDLE) {
+				ha = ma;
+			}
+		}
+		if(he == null || bi == null || ha == null)
+			return null;
+		return new Axe(he, bi, ha);
+			
+	}
+
+	@Override
+	public boolean canBreak(Block b) {
+		return b.getBlockClass() == BlockClass.WOOD;
+	}
+}
