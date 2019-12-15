5c4010a2a526ade8fc9a20c4d833fe39655a6d9c
IntegratedQuantum <jahe788@gmail.com>
2019-12-15 12:24:40 +0100

    Optimize Noise calculation: Grid gradients are only caclulated once instead of being recaclulated for every pixel. Increses speed by ~1ms/chunk

--- cubyz-common/src/io/cubyz/world/Noise.java
+++ cubyz-common/src/io/cubyz/world/Noise.java
@@ -10,14 +10,28 @@ public class Noise {
 	private static Random r = new Random();
 	private static long l1, l2, l3;
 
+	private static float[][][] xGridPoints; // [scale][x][y]
+	private static float[][][] yGridPoints; // [scale][x][y]
+	private static int x0, y0;
+
 	private static int resolution;
 	private static int resolution2;
 	// Calculate the gradient instead of storing it.
 	// This is inefficient(since it is called every time), but allows infinite chunk generation.
-	private static float getGradient(int x, int y, int i) {
+	private static float generateGradient(int x, int y, int i) {
 		r.setSeed(l1*x+l2*y+l3*i+resolution);
     	return 2 * r.nextFloat() - 1;
     }
+	
+	private static float getGradientX(int x, int y) {
+		int index = xGridPoints.length-numOfBits(resolution)+numOfBits(16)-1;
+		return xGridPoints[index][x][y];
+	}
+	
+	private static float getGradientY(int x, int y) {
+		int index = yGridPoints.length-numOfBits(resolution)+numOfBits(16)-1;
+		return yGridPoints[index][x][y];
+	}
 	/* Function to linearly interpolate between a0 and a1
 	 * Weight w should be in the range [0.0, 1.0]
 	 */
@@ -38,8 +52,8 @@ public class Noise {
 		float dy = y/(float)resolution - iy;
 
 	    // Compute the dot-product
-		float gx = getGradient(ix, iy, 0);
-		float gy = getGradient(ix, iy, 1);
+		float gx = getGradientX(ix, iy);
+		float gy = getGradientY(ix, iy);
 		float gr = (float)Math.sqrt((gx*gx+gy*gy));
 		gx /= gr;
 		gy /= gr;
@@ -47,19 +61,23 @@ public class Noise {
 	}
 
 	// Compute Perlin noise at coordinates x, y
-	private static float perlin(int x, int y) {
+	private static float perlin(int x, int y, int x00, int y00) {
 
 	    // Determine grid cell coordinates
 	    int x0 = x/resolution;
 	    if(x < 0) {
 	    	x0 = (x-resolution2)/resolution;
 	    }
+	    x0 -= x00;
 	    int x1 = x0 + 1;
 	    int y0 = y/resolution;
 	    if(y < 0) {
 	    	y0 = (y-resolution2)/resolution;
 	    }
+	    y0 -= y00;
 	    int y1 = y0 + 1;
+	    x -= x00*resolution;
+	    y -= y00*resolution;
 
 	    // Determine interpolation weights
 	    // Could also use higher order polynomial/s-curve here
@@ -88,7 +106,80 @@ public class Noise {
 		return generateMapFragment(0, 0, width, height, scale, seed);
 	}
 	
-	// Map generation is rather slow at the moment. It takes almost as much time as the chunk generation.
+	// Returns how many bits a number is long:
+	private static int numOfBits(int num) {
+		int log = 0;
+	    if( ( num & 0xffff0000 ) != 0 ) { num >>>= 16; log = 16; }
+	    if( num >= 256 ) { num >>>= 8; log += 8; }
+	    if( num >= 16  ) { num >>>= 4; log += 4; }
+	    if( num >= 4   ) { num >>>= 2; log += 2; }
+	    return log + ( num >>> 1 );
+	}
+	
+	// Calculate all grid points that will be needed to prevent double calculating them.
+	private static void calculateGridPoints(int x, int y, int width, int height, int scale) {
+		int bits = numOfBits(scale)-numOfBits(16)+1;
+		xGridPoints = new float[bits][][];
+		yGridPoints = new float[bits][][];
+		for(int i = 0; scale >= 16; scale >>= 1, ++i) {
+			resolution = scale;
+			resolution2 = resolution-1;
+		    // Determine grid cell coordinates of all cells that points can be in:
+			float[][] xGrid = new float[width/scale + 2][height/scale + 2]; // Simply assume the absolute maximum number of grid points are generated.
+			float[][] yGrid = new float[width/scale + 2][height/scale + 2]; // Simply assume the absolute maximum number of grid points are generated.
+			int numX = 0, numY = 0;
+			int x0 = 0;
+			for(int ix = x; ix < x+width; ix += scale) {
+				numY = 0;
+				x0 = ix/resolution;
+			    if(x < 0) {
+			    	x0 = (ix-resolution2)/resolution;
+			    }
+				int y0 = 0;
+				for(int iy = y; iy < y+height; iy += scale) {
+				    y0 = iy/resolution;
+				    if(y < 0) {
+				    	y0 = (iy-resolution2)/resolution;
+				    }
+					xGrid[numX][numY] = generateGradient(x0, y0, 0);
+					yGrid[numX][numY] = generateGradient(x0, y0, 1);
+					numY++;
+				}
+				xGrid[numX][numY] = generateGradient(x0, y0+1, 0);
+				yGrid[numX][numY] = generateGradient(x0, y0+1, 1);
+				numX++;
+			}
+			numY = 0;
+			int y0 = 0;
+			for(int iy = y; iy < y+height; iy += scale) {
+			    y0 = iy/resolution;
+			    if(y < 0) {
+			    	y0 = (iy-resolution2)/resolution;
+			    }
+				xGrid[numX][numY] = generateGradient(x0+1, y0, 0);
+				yGrid[numX][numY] = generateGradient(x0+1, y0, 1);
+				numY++;
+			}
+			xGrid[numX][numY] = generateGradient(x0+1, y0+1, 0);
+			yGrid[numX][numY] = generateGradient(x0+1, y0+1, 1);
+			numY++;
+			numX++;
+			// Copy the values into smaller arrays and put them into the array containing all grid points:
+			float[][] xGridR = new float[numX][numY];
+			float[][] yGridR = new float[numX][numY];
+			for(int ix = 0; ix < numX; ix++) {
+				System.arraycopy(xGrid, 0, xGridR, 0, numY);
+				System.arraycopy(yGrid, 0, yGridR, 0, numY);
+				for(int iy = 0; iy < numY; iy++) {
+					if(xGridR[ix][iy] < -1)
+						System.out.println(ix+" "+ iy);
+				}
+			}
+			xGridPoints[i] = xGridR;
+			yGridPoints[i] = yGridR;
+		}
+	}
+	
 	public static float[][] generateMapFragment(int x, int y, int width, int height, int scale, int seed) {
 		float[][] map = new float[width][height];
 		float factor = 0.45F;
@@ -97,12 +188,22 @@ public class Noise {
 		l1 = r.nextLong();
 		l2 = r.nextLong();
 		l3 = r.nextLong();
+		calculateGridPoints(x, y, width, height, scale);
 		for(; scale >= 16; scale >>= 1) {
 			resolution = scale;
-			resolution2 = resolution-1;		
+			resolution2 = resolution-1;	
+		    int x0 = x/resolution;
+		    if(x < 0) {
+		    	x0 = (x-resolution2)/resolution;
+		    }
+		    int y0 = y/resolution;
+		    if(y < 0) {
+		    	y0 = (y-resolution2)/resolution;
+		    }	
+			
 			for (int x1 = x; x1 < width + x; x1++) {
 				for (int y1 = y; y1 < height + y; y1++) {
-					map[x1 - x][y1 - y] += factor*perlin(x1, y1);
+					map[x1 - x][y1 - y] += factor*perlin(x1, y1, x0, y0);
 				}
 			}
 			sum += factor;
