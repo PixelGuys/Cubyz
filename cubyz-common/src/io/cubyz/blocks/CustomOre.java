1439c8e451d9a01887bb732c5692d1132391b4bd
IntegratedQuantum <jahe788@gmail.com>
2020-04-07 23:09:17 +0200

    Add a better name generator. Uses the relative occurence of every combinatation of 3 characters(in the file minerals.txt which contains all minerals from the wikipedia mineral list) to choose the next char.

--- cubyz-common/src/io/cubyz/blocks/CustomOre.java
+++ cubyz-common/src/io/cubyz/blocks/CustomOre.java
@@ -6,16 +6,19 @@ import io.cubyz.base.init.ItemInit;
 import io.cubyz.items.CustomItem;
 import io.cubyz.items.tools.CustomMaterial;
 import io.cubyz.ndt.NDTContainer;
+import io.cubyz.storage.ThingsZenithWantsInAnExtraFile;
 import io.cubyz.world.CustomObject;
 
 public class CustomOre extends Ore implements CustomObject {
-
+	static int getIndex(char c) {
+		if(c == ' ') return 0;
+		if('a' <= c && c <= 'z') return 1+c-'a';
+		return -1;
+	}
+	
 	private int color;
 	private String name;
 	public int template;
-
-	public static String[] prefixes = new String[] {"Al", "An", "Ar", "Be", "Bo", "Bro", "Ca", "Chlor", "Co", "Fer", "Fluo", "Gr", "Ha", "Hydro", "Ind", "Ka", "Kr", "Lith", "Magne", "Meta", "Natr", "Ni", "Osm", "Phos", "Pyr", "Rho", "Sider", "Str", "Tellur", "Thor", "Uran", "Vana", "Xanth", "Yttr", "Zinc", "Zirc"};
-	public static String[] phons = new String[] {"ay", "de", "pi", "er", "op", "ha", "do", "po", "na", "ye", "si", "re"};
 	
 	public String getName() {
 		return name;
@@ -24,87 +27,45 @@ public class CustomOre extends Ore implements CustomObject {
 		return color;
 	}
 	
-	private static boolean isVowel(char c) {
-		return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y';
-	}
-	
-	private static boolean doesntFit(char c1, char c2, char c3) {
-		if((int)c2*(int)c3 == 12705) return true; // Easy test using the ASCII table to see if i and y are following each other.
-		if(c2 == c3) return true;
-		if(c1 == c3) return true;
-		return isVowel(c1) == isVowel(c2) && isVowel(c2) == isVowel(c3);
+	private static char choose(char c1, char c2, float rand, int length) {
+		try {
+			int i1 = getIndex(c1);
+			int i2 = getIndex(c2);
+			int i3 = 0;
+			if(length >= 20 && ThingsZenithWantsInAnExtraFile.chars[i1*27 + i2][i3] == ' ') { // Make sure the word ends.
+				return ' ';
+			}
+			for(;;i3++) {
+				rand -= ThingsZenithWantsInAnExtraFile.probabilities[i1*27 + i2][i3];
+				if(rand <= 0 && (length >= 5 || ThingsZenithWantsInAnExtraFile.chars[i1*27 + i2][i3] != ' ')) {
+					break;
+				}
+			}
+			return ThingsZenithWantsInAnExtraFile.chars[i1*27 + i2][i3];
+		} catch(ArrayIndexOutOfBoundsException e) {
+			return ' ';
+		}
 	}
 	
 	private static String randomName(Random rand) {
 		StringBuilder sb = new StringBuilder();
 		
-		
-		float randomNumber = rand.nextFloat();
-		// Randomly add some common prefixes(you'll find each of them at least once in the wikipedia list of minerals).
-		sb.append(prefixes[rand.nextInt(prefixes.length)]);
-		
-		int length = rand.nextInt(3);
-		for (int i = 0; i < length; i++) {
-			String next;
-			do {
-				next = phons[rand.nextInt(phons.length)];
-			} while(doesntFit(sb.charAt(sb.length()-2), sb.charAt(sb.length()-1), next.charAt(0)));
-			sb.append(next);
-		}
-		randomNumber = rand.nextFloat();
-		
-		// Randomly add some common postfixes(you'll find each of them at least once in the wikipedia list of minerals).
-		if(randomNumber < 0.2) {
-			if(sb.charAt(sb.length()-1) == 'i')
-				sb.append("um");
-			else
-				sb.append("ium");
-		} else if(randomNumber < 0.25) {
-			sb.append("um");
-		} else if(randomNumber < 0.5) {
-			if(sb.charAt(sb.length()-1) == 'i')
-				sb.append("te");
-			else
-				sb.append("ite");
-		} else if(randomNumber < 0.6) {
-			if(sb.charAt(sb.length()-1) == 'i')
-				sb.append("ne");
-			else
-				sb.append("ine");
-		} else if(randomNumber < 0.62) {
-			sb.append("clase");
-		} else if(randomNumber < 0.65) {
-			sb.append("ase");
-		} else if(randomNumber < 0.7) {
-			sb.append("gar");
-		} else if(randomNumber < 0.73) {
-			sb.append("ene");
-		} else if(randomNumber < 0.75) {
-			sb.append("melane");
-		} else if(randomNumber < 0.8) {
-			sb.append("time");
-		} else if(randomNumber < 0.85) {
-			sb.append("sten");
-		} else if(randomNumber < 0.9) {
-			sb.append("on");
-		} else if(randomNumber < 0.92) { // A few rarer and longer endings:
-			sb.append("calcite");
-		} else if(randomNumber < 0.93) {
-			sb.append(" Quartz");
-		} else if(randomNumber < 0.94) {
-			sb.append("malachite");
-		} else if(randomNumber < 0.95) {
-			sb.append("uranylite");
-		} else if(randomNumber < 0.96) {
-			sb.append("ferrite");
-		} else if(randomNumber < 0.97) {
-			sb.append("chlore");
-		} else if(randomNumber < 0.98) {
-			sb.append("erupine");
-		} else if(randomNumber < 0.99) {
-			sb.append("lite");
-		} else {
-			sb.append("montite");
+		char c1 = ' ', c2 = ' ', c3 = choose(c1, c2, rand.nextFloat(), 0);
+		sb.append((char)(c3+'A'-'a'));
+		int i = 0;
+		while(true) {
+			char c4 = choose(c2, c3, rand.nextFloat(), ++i);
+			if(c3 == ' ') {
+				if(c4 == ' ') {
+					break;
+				}
+				sb.append(c3);
+			}
+			c1 = c2;
+			c2 = c3;
+			c3 = c4;
+			if(c4 != ' ')
+				sb.append(c4);
 		}
 		return sb.toString();
 	}
---
+++ utils/RandomNameGenerator/ProbabilityExtractor.java
@@ -0,0 +1,75 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+// Searches through the mineral names and generates a table of probabilities for every 3 char combination.
+
+public class ProbabilityExtractor {
+	static int getIndex(char c) {
+		if(c == ' ') return 0;
+		if('a' <= c && c <= 'z') return 1+c-'a';
+		if('A' <= c && c <= 'Z') return 1+c-'A';
+		System.err.println("Unknown "+c);
+		System.exit(1);
+		return 0;
+	}
+	static char getChar(int i) {
+		if(i == 0) return ' ';
+		if(i <= 26) return (char)('a'+i-1);
+		System.err.println("Unknown "+i);
+		System.exit(1);
+		return '0';
+	}
+	public static void main(String[] args) {
+		int[] number = new int[27*27*27];
+		try (BufferedReader br = new BufferedReader(new FileReader("./minerals.txt"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				char c1 = ' ', c2 = ' ', c3 = ' ';
+				for(char c : line.toCharArray()) {
+					c1 = c2;
+					c2 = c3;
+					c3 = c;
+					number[getIndex(c1)*27*27+getIndex(c2)*27+getIndex(c3)]++;
+				}
+					number[getIndex(c2)*27*27+getIndex(c3)*27]++;
+					number[getIndex(c3)*27*27]++;
+			}
+		} catch(Exception e) {}
+		System.out.print("	public static char[][] chars = {");
+		for(int i = 0; i < 27; i++) {
+			for(int j = 0; j < 27; j++) {
+				System.out.print("{");
+				int n = 0;
+				for(int k = 0; k < 27; k++) {
+					if(number[i*27*27 + j*27 + k] != 0) {
+						if(n != 0) System.out.print(",");
+						System.out.print("\'"+getChar(k)+"\'");
+						n++;
+					}
+				}
+				System.out.print("},");
+			}
+		}
+		System.out.println("};");
+		System.out.print("	public static float[][] probabilities = {");
+		for(int i = 0; i < 27; i++) {
+			for(int j = 0; j < 27; j++) {
+				System.out.print("{");
+				int total = 0;
+				for(int k = 0; k < 27; k++) {
+					total += number[i*27*27 + j*27 + k];
+				}
+				int n = 0;
+				for(int k = 0; k < 27; k++) {
+					if(number[i*27*27 + j*27 + k] != 0) {
+						if(n != 0) System.out.print(",");
+						System.out.print((number[i*27*27 + j*27 + k]/(float)total)+"f");
+						n++;
+					}
+				}
+				System.out.print("},");
+			}
+		}
+		System.out.println("};");
+	}
+}
