cea61c6e4118f0d198719e55bbd7a31f64680dd7
ZaUserA <moddedminecrafttutorials31@gmail.com>
2020-04-14 21:45:20 +0300

    Mouse Bug Fix

--- cubyz-client/src/io/jungle/MouseInput.java
+++ cubyz-client/src/io/jungle/MouseInput.java
@@ -111,9 +111,11 @@ public class MouseInput {
 
 	public void input(Window window) {
 		int[] pos = window.getPosition();
+//		System.out.println("Focused: " + window.isFocused());
+//		System.out.println("Grabbed: " + grabbed);
 		currentPos.x = MouseInfo.getPointerInfo().getLocation().getX() - pos[0];
 		currentPos.y = MouseInfo.getPointerInfo().getLocation().getY() - pos[1];
-		if(grabbed && window.isFocused()) {
+		if(grabbed /*&& window.isFocused()*/) {
 			displVec.y += currentPos.x - (window.getWidth() >> 1);
 			displVec.x += currentPos.y - (window.getHeight() >> 1);
 			r.mouseMove(pos[0] + (window.getWidth() >> 1), pos[1] + (window.getHeight() >> 1));
