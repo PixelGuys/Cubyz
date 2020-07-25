dbcdcde58171077bf9140c9ae54ace08c794bc1f
IntegratedQuantum <jahe788@gmail.com>
2020-07-25 18:40:46 +0200

    Use string parsing for EntityModels instead of weird unreadable int-arrays. Also adds different movement patterns(stable and fast) which are implementations of the two most common quadruped movement patterns(as far as I know). The stable movement features that one pair of diagonally relative feet is on the ground while the other pair is moving. In the fast movement either the front or the back feet are on the ground resulting in a more jumpy movement which is faster.

--- cubyz-client/src/io/cubyz/base/entity_models/Quadruped.java
+++ cubyz-client/src/io/cubyz/base/entity_models/Quadruped.java
@@ -16,9 +16,10 @@ import io.jungle.renderers.Transformation;
 import io.jungle.util.Material;
 import io.jungle.util.ShaderProgram;
 
-import static io.cubyz.CubyzLogger.logger;
-
 public class Quadruped implements EntityModel {
+	private enum MovementPattern {
+		STABLE, FAST,
+	};
 	// Registry stuff:
 	Resource id = new Resource("cuybz:quadruped");
 	public Quadruped() {}
@@ -27,54 +28,66 @@ public class Quadruped implements EntityModel {
 		return id;
 	}
 	@Override
-	public EntityModel createInstance(int[] args, EntityType source) {
-		if(args.length != 7) {
-			logger.warning("Wrong data for entity model "+id+": Found "+args.length+" ints but requiered "+7+". Skipping model.");
-			return this;
-		}
+	public EntityModel createInstance(String data, EntityType source) {
 		
-		return new Quadruped(args[0], args[1], args[2], args[3], args[4], args[5], args[6], source);
+		return new Quadruped(data, source);
 	}
 	
 	// Actual model stuff:
 	private Mesh leg, body, head;
-	float bodyWidth, bodyLength, bodyHeight, legHeight, legWidth, headHeight, headWidth;
+	float bodyWidth, bodyLength, bodyHeight, legWidth, legHeight, headWidth, headLength, headHeight;
+	MovementPattern movementPattern;
 	
-	public Quadruped(int bodyWidth, int bodyLength, int bodyHeight, int legHeight, int legWidth, int headHeight, int headWidth, EntityType source) {
-		this.bodyWidth = bodyWidth/16.0f;
-		this.bodyLength = bodyLength/16.0f;
-		this.bodyHeight = bodyHeight/16.0f;
-		this.legHeight = legHeight/16.0f;
-		this.legWidth = legWidth/16.0f;
-		this.headHeight = headHeight/16.0f;
-		this.headWidth = headWidth/16.0f;
+	public Quadruped(String data, EntityType source) {
+		// Parse data:
+		String[] lines = data.split("\n");
+		for(String line : lines) {
+			String[] parts = line.replaceAll("\\s", "").split(":");
+			if(parts[0].equals("body")) {
+				String[] arguments = parts[1].split("x");
+				bodyWidth = Integer.parseInt(arguments[0])/16.0f;
+				bodyLength = Integer.parseInt(arguments[1])/16.0f;
+				bodyHeight = Integer.parseInt(arguments[2])/16.0f;
+			} else if(parts[0].equals("head")) {
+				String[] arguments = parts[1].split("x");
+				headWidth = Integer.parseInt(arguments[0])/16.0f;
+				headLength = Integer.parseInt(arguments[1])/16.0f;
+				headHeight = Integer.parseInt(arguments[2])/16.0f;
+			} else if(parts[0].equals("leg")) {
+				String[] arguments = parts[1].split("x");
+				legWidth = Integer.parseInt(arguments[0])/16.0f;
+				legHeight = Integer.parseInt(arguments[1])/16.0f;
+			} else if(parts[0].equals("movement")) {
+				movementPattern = MovementPattern.valueOf(parts[1].toUpperCase());
+			}
+		}
 		float textureWidth = 4*legWidth + bodyLength + bodyHeight;
 		float textureHeight = Math.max(2*bodyWidth + 2*bodyHeight, legHeight + legWidth);
 		// leg obj:
-		int legOffset = bodyLength + bodyHeight;
+		float legOffset = bodyLength + bodyHeight;
 		float[] legPositions = new float[] {
 				// Top(each vertex two times(the top face is not rendered)):
-				-legWidth/32.0f,	0,						-legWidth/32.0f,	//0
-				-legWidth/32.0f,	0,						-legWidth/32.0f,	//1
-				-legWidth/32.0f,	0,						legWidth/32.0f,		//2
-				-legWidth/32.0f,	0,						legWidth/32.0f,		//3
-				legWidth/32.0f,		0,						-legWidth/32.0f,	//4
-				legWidth/32.0f,		0,						-legWidth/32.0f,	//5
-				legWidth/32.0f,		0,						legWidth/32.0f,		//6
-				legWidth/32.0f,		0,						legWidth/32.0f,		//7
+				-legWidth/2,		0,						-legWidth/2,	//0
+				-legWidth/2,		0,						-legWidth/2,	//1
+				-legWidth/2,		0,						legWidth/2,		//2
+				-legWidth/2,		0,						legWidth/2,		//3
+				legWidth/2,			0,						-legWidth/2,	//4
+				legWidth/2,			0,						-legWidth/2,	//5
+				legWidth/2,			0,						legWidth/2,		//6
+				legWidth/2,			0,						legWidth/2,		//7
 				// Bottom(each vertex three times):
-				-legWidth/32.0f,	-legHeight/16.0f,		-legWidth/32.0f,	//8
-				-legWidth/32.0f,	-legHeight/16.0f,		-legWidth/32.0f,	//9
-				-legWidth/32.0f,	-legHeight/16.0f,		-legWidth/32.0f,	//10
-				-legWidth/32.0f,	-legHeight/16.0f,		legWidth/32.0f,		//11
-				-legWidth/32.0f,	-legHeight/16.0f,		legWidth/32.0f,		//12
-				-legWidth/32.0f,	-legHeight/16.0f,		legWidth/32.0f,		//13
-				legWidth/32.0f,		-legHeight/16.0f,		-legWidth/32.0f,	//14
-				legWidth/32.0f,		-legHeight/16.0f,		-legWidth/32.0f,	//15
-				legWidth/32.0f,		-legHeight/16.0f,		-legWidth/32.0f,	//16
-				legWidth/32.0f,		-legHeight/16.0f,		legWidth/32.0f,		//17
-				legWidth/32.0f,		-legHeight/16.0f,		legWidth/32.0f,		//18
-				legWidth/32.0f,		-legHeight/16.0f,		legWidth/32.0f,		//19
+				-legWidth/2,		-legHeight,			-legWidth/2,	//8
+				-legWidth/2,		-legHeight,			-legWidth/2,	//9
+				-legWidth/2,		-legHeight,			-legWidth/2,	//10
+				-legWidth/2,		-legHeight,			legWidth/2,		//11
+				-legWidth/2,		-legHeight,			legWidth/2,		//12
+				-legWidth/2,		-legHeight,			legWidth/2,		//13
+				legWidth/2.0f,		-legHeight,			-legWidth/2,	//14
+				legWidth/2.0f,		-legHeight,			-legWidth/2,	//15
+				legWidth/2.0f,		-legHeight,			-legWidth/2,	//16
+				legWidth/2.0f,		-legHeight,			legWidth/2,		//17
+				legWidth/2.0f,		-legHeight,			legWidth/2,		//18
+				legWidth/2.0f,		-legHeight,			legWidth/2,		//19
 		};
 		float[] legTextCoords = new float[] {
 				// Top:
@@ -148,32 +161,32 @@ public class Quadruped implements EntityModel {
 		// body obj:
 		float[] bodyPositions = new float[] {
 				// Top(each vertex two times(the top face is not rendered)):
-				-bodyLength/32.0f,	bodyHeight/32.0f,		-bodyWidth/32.0f,		//0
-				-bodyLength/32.0f,	bodyHeight/32.0f,		-bodyWidth/32.0f,		//1
-				-bodyLength/32.0f,	bodyHeight/32.0f,		bodyWidth/32.0f,		//2
-				-bodyLength/32.0f,	bodyHeight/32.0f,		bodyWidth/32.0f,		//3
-				bodyLength/32.0f,	bodyHeight/32.0f,		-bodyWidth/32.0f,		//4
-				bodyLength/32.0f,	bodyHeight/32.0f,		-bodyWidth/32.0f,		//5
-				bodyLength/32.0f,	bodyHeight/32.0f,		bodyWidth/32.0f,		//6
-				bodyLength/32.0f,	bodyHeight/32.0f,		bodyWidth/32.0f,		//7
+				-bodyLength/2,	bodyHeight/2,		-bodyWidth/2,		//0
+				-bodyLength/2,	bodyHeight/2,		-bodyWidth/2,		//1
+				-bodyLength/2,	bodyHeight/2,		bodyWidth/2,		//2
+				-bodyLength/2,	bodyHeight/2,		bodyWidth/2,		//3
+				bodyLength/2,	bodyHeight/2,		-bodyWidth/2,		//4
+				bodyLength/2,	bodyHeight/2,		-bodyWidth/2,		//5
+				bodyLength/2,	bodyHeight/2,		bodyWidth/2,		//6
+				bodyLength/2,	bodyHeight/2,		bodyWidth/2,		//7
 				// Bottom(each vertex three times):
-				-bodyLength/32.0f,	-bodyHeight/32.0f,		-bodyWidth/32.0f,		//8
-				-bodyLength/32.0f,	-bodyHeight/32.0f,		-bodyWidth/32.0f,		//9
-				-bodyLength/32.0f,	-bodyHeight/32.0f,		-bodyWidth/32.0f,		//10
-				-bodyLength/32.0f,	-bodyHeight/32.0f,		bodyWidth/32.0f,		//11
-				-bodyLength/32.0f,	-bodyHeight/32.0f,		bodyWidth/32.0f,		//12
-				-bodyLength/32.0f,	-bodyHeight/32.0f,		bodyWidth/32.0f,		//13
-				bodyLength/32.0f,	-bodyHeight/32.0f,		-bodyWidth/32.0f,		//14
-				bodyLength/32.0f,	-bodyHeight/32.0f,		-bodyWidth/32.0f,		//15
-				bodyLength/32.0f,	-bodyHeight/32.0f,		-bodyWidth/32.0f,		//16
-				bodyLength/32.0f,	-bodyHeight/32.0f,		bodyWidth/32.0f,		//17
-				bodyLength/32.0f,	-bodyHeight/32.0f,		bodyWidth/32.0f,		//18
-				bodyLength/32.0f,	-bodyHeight/32.0f,		bodyWidth/32.0f,		//19
+				-bodyLength/2,	-bodyHeight/2,		-bodyWidth/2,		//8
+				-bodyLength/2,	-bodyHeight/2,		-bodyWidth/2,		//9
+				-bodyLength/2,	-bodyHeight/2,		-bodyWidth/2,		//10
+				-bodyLength/2,	-bodyHeight/2,		bodyWidth/2,		//11
+				-bodyLength/2,	-bodyHeight/2,		bodyWidth/2,		//12
+				-bodyLength/2,	-bodyHeight/2,		bodyWidth/2,		//13
+				bodyLength/2,	-bodyHeight/2,		-bodyWidth/2,		//14
+				bodyLength/2,	-bodyHeight/2,		-bodyWidth/2,		//15
+				bodyLength/2,	-bodyHeight/2,		-bodyWidth/2,		//16
+				bodyLength/2,	-bodyHeight/2,		bodyWidth/2,		//17
+				bodyLength/2,	-bodyHeight/2,		bodyWidth/2,		//18
+				bodyLength/2,	-bodyHeight/2,		bodyWidth/2,		//19
 				// Top face:
-				-bodyLength/32.0f,	bodyHeight/32.0f,		-bodyWidth/32.0f,		//20
-				-bodyLength/32.0f,	bodyHeight/32.0f,		bodyWidth/32.0f,		//21
-				bodyLength/32.0f,	bodyHeight/32.0f,		-bodyWidth/32.0f,		//22
-				bodyLength/32.0f,	bodyHeight/32.0f,		bodyWidth/32.0f,		//23
+				-bodyLength/2,	bodyHeight/2,		-bodyWidth/2,		//20
+				-bodyLength/2,	bodyHeight/2,		bodyWidth/2,		//21
+				bodyLength/2,	bodyHeight/2,		-bodyWidth/2,		//22
+				bodyLength/2,	bodyHeight/2,		bodyWidth/2,		//23
 		};
 		float[] bodyTextCoords = new float[] {
 				// Top:
@@ -257,7 +270,7 @@ public class Quadruped implements EntityModel {
 				22, 20, 23,
 				20, 21, 23,
 		};
-		Cubyz.renderDeque.add(new Runnable() { // TODO: Why is this so complex?
+		Cubyz.renderDeque.add(new Runnable() {
 			@Override
 			public void run() {
 				try {
@@ -309,7 +322,7 @@ public class Quadruped implements EntityModel {
 		// Front side2:
 		pos.x += zNorm*width;
 		pos.z += -xNorm*width;
-		rotation.z = legAngle2;
+		rotation.z = movementPattern == MovementPattern.STABLE ? legAngle2 : legAngle1;
 		leg.renderOne(() -> {
 			Matrix4f modelViewMatrix = Transformation.getModelViewMatrix(Transformation.getModelMatrix(pos, rotation, 1), viewMatrix);
 			((ShaderProgram)entityShader).setUniform("modelViewMatrix", modelViewMatrix);
@@ -317,7 +330,7 @@ public class Quadruped implements EntityModel {
 		// Back side2:
 		pos.x += -xNorm*length;
 		pos.z += -zNorm*length;
-		rotation.z = legAngle1;
+		rotation.z = movementPattern == MovementPattern.STABLE ? legAngle1 : legAngle2;
 		leg.renderOne(() -> {
 			Matrix4f modelViewMatrix = Transformation.getModelViewMatrix(Transformation.getModelMatrix(pos, rotation, 1), viewMatrix);
 			((ShaderProgram)entityShader).setUniform("modelViewMatrix", modelViewMatrix);
