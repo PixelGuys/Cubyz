00c79c80580fd8eb8f793c45962181afb6bb754a
IntegratedQuantum <jahe788@gmail.com>
2020-09-17 11:46:01 +0200

    Add files I forgot in last commit.

---
+++ cubyz-common/src/io/cubyz/items/BlockDrop.java
@@ -0,0 +1,10 @@
+package io.cubyz.items;
+
+public class BlockDrop {
+	public final Item item;
+	public final float amount;
+	public BlockDrop(Item item, float amount) {
+		this.item = item;
+		this.amount = amount;
+	}
+}
