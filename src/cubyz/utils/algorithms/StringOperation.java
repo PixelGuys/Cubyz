372ae9f7fd378f6fac84f4526022e8ace624dc09
tilpp <tillplusplus@gmail.com>
2021-12-14 11:11:47 +0100

    fixed order of escaping in strings

--- src/cubyz/utils/algorithms/StringOperation.java
+++ src/cubyz/utils/algorithms/StringOperation.java
@@ -2,9 +2,9 @@ package cubyz.utils.algorithms;
 
 public abstract class StringOperation {
     public static String escape(String string){
-        return string   .replace("\n", "\\\n") //escaping new line
+        return string   .replace("\\", "\\\\") //escaping  \
+                        .replace("\n", "\\\n") //escaping new line
                         .replace("\"", "\\\"") //escaping "
-                        .replace("\\", "\\\\") //escaping  \
                         .replace("\t", "\\t")  //escaping  tabs
                 ;
     }
