b1c4d27870041f537ee99910b5446889b90ccfde
Marco Santos <52864251+lazynessmind@users.noreply.github.com>
2021-12-30 17:13:38 +0000

    Input: Increase fly speed when pressing LEFT_CONTROL (#41)
    * Input: Parity with faster fly movement when left control is pressed.
    
    - Add the same increase in speed when trying to get altitude while pressing the left control key. 24 is the value to be exact.
    
    * Input: Implemented same behavior for falling.
    
    - 6f0c647

--- src/cubyz/gui/input/Input.java
+++ src/cubyz/gui/input/Input.java
@@ -89,14 +89,14 @@ public class Input {
 				}
 				if (Keybindings.isPressed("jump")) {
 					if (Cubyz.player.isFlying()) {
-						Cubyz.player.vy = 5.45F;
+						Cubyz.player.vy = Keyboard.isKeyPressed(GLFW.GLFW_KEY_LEFT_CONTROL) ? 29.45F : 5.45F;
 					} else if (Cubyz.player.isOnGround()) {
 						Cubyz.player.vy = 5.45F;
 					}
 				}
 				if (Keybindings.isPressed("fall")) {
 					if (Cubyz.player.isFlying()) {
-						Cubyz.player.vy = -5;
+						Cubyz.player.vy = Keyboard.isKeyPressed(GLFW.GLFW_KEY_LEFT_CONTROL) ? -29F : -5F;
 					}
 				}
 				if (Keyboard.isKeyPressed(GLFW.GLFW_KEY_F)) {
