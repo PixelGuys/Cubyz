8377a9ecdd3b8cbc8fcf47b38f227ff1c9394aaa
IntegratedQuantum <jahe788@gmail.com>
2022-04-22 10:34:44 +0200

    Render emission texture in block preview as well.

--- assets/cubyz/shaders/blockPreview/fragment.fs
+++ assets/cubyz/shaders/blockPreview/fragment.fs
@@ -6,10 +6,14 @@ in vec3 mvVertexNormal;
 out vec4 fragColor;
 
 uniform sampler2DArray texture_sampler;
+uniform sampler2DArray emissionSampler;
 uniform vec3 light;
 uniform vec3 dirLight;
 
 void main()
 {
     fragColor = texture(texture_sampler, outTexCoord)*vec4(light*(dot(dirLight, mvVertexNormal)*0.5 + 0.5), 1);
+    fragColor.rgb += texture(emissionSampler, outTexCoord).rgb;
+	float maxColor = max(1, max(fragColor.r, max(fragColor.g, fragColor.b)));
+	fragColor.rgb = fragColor.rgb/maxColor;
 }
