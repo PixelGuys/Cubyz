786b65d5565930a85126f981f9d2ff64ddef1d73
IntegratedQuantum <jahe788@gmail.com>
2022-08-24 22:08:07 +0200

    Add glfw and glad and make a window.

--- build.zig
+++ build.zig
@@ -12,6 +12,32 @@ pub fn build(b: *std.build.Builder) void {
 	const mode = b.standardReleaseOptions();
 
 	const exe = b.addExecutable("Cubyzig", "src/main.zig");
+	exe.addIncludeDir("include");
+	exe.linkLibC();
+	{ // compile glfw from source:
+		if(target.getOsTag() == .windows) {
+			exe.addCSourceFiles(&[_][]const u8 {
+				"lib/glfw/src/win32_init.c", "lib/glfw/src/win32_joystick.c", "lib/glfw/src/win32_monitor.c", "lib/glfw/src/win32_time.c", "lib/glfw/src/win32_thread.c", "lib/glfw/src/win32_window.c", "lib/glfw/src/wgl_context.c", "lib/glfw/src/egl_context.c", "lib/glfw/src/osmesa_context.c", "lib/glfw/src/context.c", "lib/glfw/src/init.c", "lib/glfw/src/input.c", "lib/glfw/src/monitor.c", "lib/glfw/src/vulkan.c", "lib/glfw/src/window.c"
+			}, &[_][]const u8{"-gdwarf-4", "-std=c99", "-D_GLFW_WIN32"});
+			exe.linkSystemLibrary("gdi32");
+			exe.linkSystemLibrary("opengl32");
+		} else if(target.getOsTag() == .linux) {
+			// TODO: if(isWayland) {
+			//	exe.addCSourceFiles(&[_][]const u8 {
+			//		"lib/glfw/src/linux_joystick.c", "lib/glfw/src/wl_init.c", "lib/glfw/src/wl_monitor.c", "lib/glfw/src/wl_window.c", "lib/glfw/src/posix_time.c", "lib/glfw/src/posix_thread.c", "lib/glfw/src/xkb_unicode.c", "lib/glfw/src/egl_context.c", "lib/glfw/src/osmesa_context.c", "lib/glfw/src/context.c", "lib/glfw/src/init.c", "lib/glfw/src/input.c", "lib/glfw/src/monitor.c", "lib/glfw/src/vulkan.c", "lib/glfw/src/window.c"
+			//	}, &[_][]const u8{"-gdwarf-4",});
+			//} else {
+				exe.addCSourceFiles(&[_][]const u8 {
+					"lib/glfw/src/linux_joystick.c", "lib/glfw/src/x11_init.c", "lib/glfw/src/x11_monitor.c", "lib/glfw/src/x11_window.c", "lib/glfw/src/xkb_unicode.c", "lib/glfw/src/posix_time.c", "lib/glfw/src/posix_thread.c", "lib/glfw/src/glx_context.c", "lib/glfw/src/egl_context.c", "lib/glfw/src/osmesa_context.c", "lib/glfw/src/context.c", "lib/glfw/src/init.c", "lib/glfw/src/input.c", "lib/glfw/src/monitor.c", "lib/glfw/src/vulkan.c", "lib/glfw/src/window.c"
+				}, &[_][]const u8{"-gdwarf-4", "-std=c99", "-D_GLFW_X11"});
+				exe.linkSystemLibrary("x11");
+			//}
+			exe.linkSystemLibrary("GL");
+		} else {
+			std.log.err("Unsupported target: {}\n", .{ target.getOsTag() });
+		}
+	}
+	exe.addCSourceFiles(&[_][]const u8{"lib/glad.c"}, &[_][]const u8{"-gdwarf-4",});
 	exe.setTarget(target);
 	exe.setBuildMode(mode);
 	exe.install();
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
 
+pub const c = @cImport ({
+	@cInclude("glad/glad.h");
+	@cInclude("GLFW/glfw3.h");
+});
+
 var logFile: std.fs.File = undefined;
 
 pub fn log(
@@ -29,8 +34,92 @@ pub fn log(
 	nosuspend std.io.getStdErr().writeAll(terminalMessage) catch return;
 }
 
+pub const Window = struct {
+	var isFullscreen: bool = false;
+	pub var width: u31 = 1280;
+	pub var height: u31 = 720;
+	var window: *c.GLFWwindow = undefined;
+	const GLFWCallbacks = struct {
+		fn errorCallback(errorCode: c_int, description: [*c]const u8) callconv(.C) void {
+			std.log.err("GLFW Error({}): {s}", .{errorCode, description});
+		}
+		fn keyCallback(_: ?*c.GLFWwindow, key: c_int, scancode: c_int, action: c_int, mods: c_int) callconv(.C) void {
+			std.log.info("Key pressed: {}, {}, {}, {}", .{key, scancode, action, mods});
+			if(key == c.GLFW_KEY_F11 and action == c.GLFW_RELEASE) {
+				toggleFullscreen();
+			}
+		}
+		fn framebufferSize(_: ?*c.GLFWwindow, newWidth: c_int, newHeight: c_int) callconv(.C) void {
+			std.log.info("Framebuffer: {}, {}", .{newWidth, newHeight});
+			width = @intCast(u31, newWidth);
+			height = @intCast(u31, newHeight);
+		}
+	};
+
+	fn init() !void {
+		_ = c.glfwSetErrorCallback(GLFWCallbacks.errorCallback);
+
+		if(c.glfwInit() == 0) {
+			return error.GLFWFailed;
+		}
+
+		c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 4);
+		c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
+
+		window = c.glfwCreateWindow(width, height, "Cubyz", null, null) orelse return error.GLFWFailed;
+
+		_ = c.glfwSetKeyCallback(window, GLFWCallbacks.keyCallback);
+		_ = c.glfwSetFramebufferSizeCallback(window, GLFWCallbacks.framebufferSize);
+
+		c.glfwMakeContextCurrent(window);
+
+		if(c.gladLoadGL() == 0) {
+			return error.GLADFailed;
+		}
+		c.glfwSwapInterval(1);
+	}
+
+	fn deinit() void {
+		c.glfwDestroyWindow(window);
+		c.glfwTerminate();
+	}
+
+	var oldX: c_int = 0;
+	var oldY: c_int = 0;
+	var oldWidth: c_int = 0;
+	var oldHeight: c_int = 0;
+	pub fn toggleFullscreen() void {
+		isFullscreen = !isFullscreen;
+		if (isFullscreen) {
+			c.glfwGetWindowPos(window, &oldX, &oldY);
+			c.glfwGetWindowSize(window, &oldWidth, &oldHeight);
+			const monitor = c.glfwGetPrimaryMonitor();
+			if(monitor == null) {
+				isFullscreen = false;
+				return;
+			}
+			const vidMode = c.glfwGetVideoMode(monitor).?;
+			c.glfwSetWindowMonitor(window, monitor, 0, 0, vidMode.width, vidMode.height, c.GLFW_DONT_CARE);
+		} else {
+			c.glfwSetWindowMonitor(window, null, oldX, oldY, oldWidth, oldHeight, c.GLFW_DONT_CARE);
+			c.glfwSetWindowAttrib(window, c.GLFW_DECORATED, c.GLFW_TRUE);
+		}
+	}
+};
+
 pub fn main() !void {
-	logFile = std.fs.cwd().createFile("logs/latest.log", .{}) catch unreachable; // init logging.
+	// init logging.
+	logFile = std.fs.cwd().createFile("logs/latest.log", .{}) catch unreachable;
+	defer logFile.close();
+
+	try Window.init();
+	defer Window.deinit();
+
+	while(c.glfwWindowShouldClose(Window.window) == 0) {
+		c.glfwSwapBuffers(Window.window);
+		c.glfwPollEvents();
+		c.glViewport(0, 0, Window.width, Window.height);
+	}
 
 	std.log.info("Hello zig.", .{});
 }
