39f51115280fcefdf7d0011a49817c2895b91bcc
IntegratedQuantum <jahe788@gmail.com>
2023-10-15 11:25:03 +0200

    Fix the bat file.

--- run.bat
+++ run.bat
@@ -2,23 +2,23 @@
 
 set version=zig-windows-x86_64-0.12.0-dev.706+62a0fbdae
 
-if not exist "./compiler" mkdir "./compiler"
-if not exist "./compiler/zig" mkdir "./compiler/zig"
-if not exist "./compiler/version.txt" copy NUL "./compiler/version.txt"
+if not exist compiler mkdir compiler
+if not exist compiler\version.txt copy NUL compiler\version.txt >NUL
 
-set /p curVersion=<"./compiler/version.txt"
+set currVersion=
+set /p currVersion=<"compiler\version.txt"
 
-if not "%version%" == "%curVersion%" (
-	echo "Deleting old zig installation..."
-	rmdir /s /q "./compiler/zig"
-	mkdir "./compiler/zig"
-	echo "Downloading %version%..."
-	powershell -Command "Invoke-WebRequest -uri https://ziglang.org/builds/%version%.zip -OutFile ./compiler/archive.zip"
-	echo "Extracting zip file..."
-	powershell Expand-Archive ".\compiler\archive.zip" -DestinationPath ".\compiler\zig"
-	echo "Done."
-	del ./compiler/archive.zip
-	echo %version%> "./compiler/version.txt"
+if not "%version%" == "%currVersion%" (
+    echo Deleting old zig installation ...
+	if exist compiler\zig rmdir /s /q compiler\zig
+    echo Downloading zig version %version% ...
+    powershell -Command $ProgressPreference = 'SilentlyContinue'; "Invoke-WebRequest -uri https://ziglang.org/builds/%version%.zip -OutFile compiler\archive.zip"
+    echo Extracting zip file ...
+    powershell $ProgressPreference = 'SilentlyContinue'; Expand-Archive compiler\archive.zip -DestinationPath compiler
+	ren compiler\%version% zig
+    echo Done.
+    del compiler\archive.zip
+    echo %version%> compiler\version.txt
 )
 
-./compiler/zig/zig build run %*
+compiler\zig\zig build run %*
