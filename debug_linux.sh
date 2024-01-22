71117990fc7c49871339d42c7e54345286207b78
Archbirdplus <archbirdplus@gmail.com>
2024-01-22 08:07:59 -0800

    Replace spaces with tabs in run scripts.

--- debug_linux.sh
+++ debug_linux.sh
@@ -3,9 +3,9 @@
 cd "$(dirname "$0")"
 
 fail () {
-    echo "Press enter key to continue."
-    read
-    exit 1
+	echo "Press enter key to continue."
+	read
+	exit 1
 }
 
 echo "Detecting Zig compiler..."
@@ -19,24 +19,24 @@ touch compiler/version.txt
 CURRENT_VERSION=$(< compiler/version.txt)
 
 if [[ "$CURRENT_VERSION" != "$VERSION" ]]; then
-    echo "Your Zig is the wrong version."
+	echo "Your Zig is the wrong version."
 	echo "Deleting current Zig installation..."
 	rm -r compiler/zig
 	mkdir compiler/zig
 	echo "Downloading $VERSION..."
 	wget -O compiler/archive.tar.xz https://ziglang.org/builds/"$VERSION".tar.xz
-    if [ $? != 0 ]
-    then
-        echo "Failed to download the Zig compiler."
-        fail
-    fi
+	if [ $? != 0 ]
+	then
+		echo "Failed to download the Zig compiler."
+		fail
+	fi
 	echo "Extracting tar file..."
 	tar --xz -xf compiler/archive.tar.xz --directory compiler/zig --strip-components 1
 	rm compiler/archive.tar.xz
 	echo "$VERSION" > compiler/version.txt
 	echo "Done updating Zig."
 else
-    echo "Zig compiler is valid."
+	echo "Zig compiler is valid."
 fi
 
 echo "Building Cubyzig from source. This may take up to 10 minutes..."
@@ -45,8 +45,8 @@ echo "Building Cubyzig from source. This may take up to 10 minutes..."
 
 if [ $? != 0 ]
 then
-    echo "Failed to build Cubyz."
-    fail
+	echo "Failed to build Cubyz."
+	fail
 fi
 
 echo "Cubyz successfully built!"
--- debug_windows.bat
+++ debug_windows.bat
@@ -14,25 +14,25 @@ set currVersion=
 set /p currVersion=<"compiler\version.txt"
 
 if not "%version%" == "%currVersion%" (
-    echo Your Zig is the wrong version.
-    echo Deleting current Zig installation ...
+	echo Your Zig is the wrong version.
+	echo Deleting current Zig installation ...
 	if exist compiler\zig rmdir /s /q compiler\zig
-    echo Downloading %version% ...
-    powershell -Command $ProgressPreference = 'SilentlyContinue'; "Invoke-WebRequest -uri https://ziglang.org/builds/%version%.zip -OutFile compiler\archive.zip"
-    if errorlevel 1 (
-        echo Failed to download the Zig compiler.
-        echo Press any key to continue.
-        pause
-        exit /b 1
-    )
-    echo Extracting zip file ...
-    powershell $ProgressPreference = 'SilentlyContinue'; Expand-Archive compiler\archive.zip -DestinationPath compiler
+	echo Downloading %version% ...
+	powershell -Command $ProgressPreference = 'SilentlyContinue'; "Invoke-WebRequest -uri https://ziglang.org/builds/%version%.zip -OutFile compiler\archive.zip"
+	if errorlevel 1 (
+		echo Failed to download the Zig compiler.
+		echo Press any key to continue.
+		pause
+		exit /b 1
+	)
+	echo Extracting zip file ...
+	powershell $ProgressPreference = 'SilentlyContinue'; Expand-Archive compiler\archive.zip -DestinationPath compiler
 	ren compiler\%version% zig
-    del compiler\archive.zip
-    echo %version%> compiler\version.txt
-    echo Done updating Zig.
+	del compiler\archive.zip
+	echo %version%> compiler\version.txt
+	echo Done updating Zig.
 ) ELSE (
-    echo Zig compiler is valid.
+	echo Zig compiler is valid.
 )
 
 echo Building Cubyzig from source. This may take up to 10 minutes...
@@ -40,10 +40,10 @@ echo Building Cubyzig from source. This may take up to 10 minutes...
 compiler\zig\zig build %*
 
 if errorlevel 1 (
-    echo Failed to build Cubyz.
-    echo Press any key to continue.
-    pause
-    exit /b 1
+	echo Failed to build Cubyz.
+	echo Press any key to continue.
+	pause
+	exit /b 1
 )
 
 echo Cubyz successfully built!
