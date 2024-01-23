88ec7c94e91447f8ae160bcddd43f61b0385b43e
IntegratedQuantum <43880493+IntegratedQuantum@users.noreply.github.com>
2024-01-23 19:00:10 +0100

    Use `call` to call the batch script on windows
    Without `call` windows won't execute the lines after calling the batch script.

--- run_windows.bat
+++ run_windows.bat
@@ -2,7 +2,7 @@
 
 cd /D "%~dp0"
 
-.\debug_windows.bat -Doptimize=ReleaseFast %*
+call debug_windows.bat -Doptimize=ReleaseFast %*
 
 IF "%NO_PAUSE%" == "" (
 	echo Press enter key to continue. (Or set NO_PAUSE=1 to skip this prompt.^)
