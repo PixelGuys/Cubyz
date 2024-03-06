4d5ead9617db40616402bc9dfb8f5934f375d395
Charleston314 <c.p.loyd@outlook.com>
2024-03-06 12:18:23 -0800

    Restore previous escape key functionality

--- src/main.zig
+++ src/main.zig
@@ -324,8 +324,6 @@ pub fn setNextKeypressListener(listener: ?*const fn(c_int, c_int, c_int) void) !
 fn escape() void {
 	if(game.world == null) return;
 	Window.setMouseGrabbed(!Window.grabbed);
-
-	gui.openWindow("pause_menu");
 }
 fn ungrabMouse() void {
 	Window.setMouseGrabbed(false);
