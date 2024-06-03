cd76edd682ebc6002f09afe3be1c14fe8bc80cdf
OneAvargeCoder193 <mgiakimenko@outlook.com>
2024-06-02 22:05:59 -0400

    Added fallen trees to forests and plains biomes

--- src/server/terrain/structures/_list.zig
+++ src/server/terrain/structures/_list.zig
@@ -4,4 +4,5 @@ pub const Boulder = @import("Boulder.zig");
 pub const GroundPatch = @import("GroundPatch.zig");
 pub const Lake = @import("Lake.zig");
 pub const SimpleTreeModel = @import("SimpleTreeModel.zig");
+pub const FallenTree = @import("FallenTree.zig");
 pub const SimpleVegetation = @import("SimpleVegetation.zig");
