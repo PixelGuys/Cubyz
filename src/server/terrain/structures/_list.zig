da5a1c492e13e8247318041082d09e122cb00e2d
OneAvargeCoder193 <85588535+OneAvargeCoder193@users.noreply.github.com>
2024-06-05 09:25:49 -0400

    Upgraded the trees and added falling trees (#441)
    * Upgraded the trees and added falling trees
    
    * Fix formatting
    
    * Fixing some stuff i guess
    
    * Changes

--- src/server/terrain/structures/_list.zig
+++ src/server/terrain/structures/_list.zig
@@ -3,4 +3,5 @@
 pub const Boulder = @import("Boulder.zig");
 pub const GroundPatch = @import("GroundPatch.zig");
 pub const SimpleTreeModel = @import("SimpleTreeModel.zig");
+pub const FallenTree = @import("FallenTree.zig");
 pub const SimpleVegetation = @import("SimpleVegetation.zig");
