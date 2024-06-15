145072e70be9ca1b2554a3e267fc75d5f82716bd
OneAvargeCoder193 <mgiakimenko@outlook.com>
2024-06-15 15:14:44 -0400

    Fix day cycle length

--- src/game.zig
+++ src/game.zig
@@ -103,7 +103,7 @@ pub const Player = struct {
 };
 
 pub const World = struct {
-	const dayCycle: u63 = 300; // Length of one in-game day in 100ms. Midnight is at DAY_CYCLE/2. Sunrise and sunset each take about 1/16 of the day. Currently set to 20 minutes
+	const dayCycle: u63 = 12000; // Length of one in-game day in 100ms. Midnight is at DAY_CYCLE/2. Sunrise and sunset each take about 1/16 of the day. Currently set to 20 minutes
 
 	conn: *Connection,
 	manager: *ConnectionManager,
