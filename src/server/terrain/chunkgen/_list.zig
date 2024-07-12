5c5347357b5566ca24b8d6d5a2505a5a356ddedd
IntegratedQuantum <jahe788@gmail.com>
2024-07-12 10:11:58 +0200

    Remove the old StalagmiteGenerator.
    follow-up on #562

--- src/server/terrain/chunkgen/_list.zig
+++ src/server/terrain/chunkgen/_list.zig
@@ -1,7 +1,6 @@
 
 
 pub const CrystalGenerator = @import("CrystalGenerator.zig");
-pub const StalagmiteGenerator = @import("StalagmiteGenerator.zig");
 pub const OreGenerator = @import("OreGenerator.zig");
 pub const TerrainGenerator = @import("TerrainGenerator.zig");
 pub const StructureGenerator = @import("StructureGenerator.zig");
