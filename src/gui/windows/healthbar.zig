d658e57601d60dae51ab92a5869fdc7fbc4551a7
careeoki <122191047+careeoki@users.noreply.github.com>
2024-07-13 15:07:00 -0600

    Fix indentation again

--- src/gui/windows/healthbar.zig
+++ src/gui/windows/healthbar.zig
@@ -13,16 +13,16 @@ const GuiComponent = gui.GuiComponent;
 const hotbar = @import("hotbar.zig");
 
 pub var window = GuiWindow{
-    .scale = 0.75,
-    .relativePosition = .{
-        .{ .attachedToWindow = .{ .reference = &hotbar.window, .selfAttachmentPoint = .upper, .otherAttachmentPoint = .upper } },
-        .{ .attachedToWindow = .{ .reference = &hotbar.window, .selfAttachmentPoint = .upper, .otherAttachmentPoint = .lower } },
-    },
-    .contentSize = Vec2f{ 80, 10 },
-    .isHud = true,
-    .showTitleBar = false,
-    .hasBackground = false,
-    .hideIfMouseIsGrabbed = false,
+	.scale = 0.75,
+	.relativePosition = .{
+		.{ .attachedToWindow = .{ .reference = &hotbar.window, .selfAttachmentPoint = .upper, .otherAttachmentPoint = .upper } },
+		.{ .attachedToWindow = .{ .reference = &hotbar.window, .selfAttachmentPoint = .upper, .otherAttachmentPoint = .lower } },
+	},
+	.contentSize = Vec2f{ 80, 10 },
+	.isHud = true,
+	.showTitleBar = false,
+	.hasBackground = false,
+	.hideIfMouseIsGrabbed = false,
 };
 
 var heartTexture: Texture = undefined;
@@ -30,40 +30,40 @@ var halfHeartTexture: Texture = undefined;
 var deadHeartTexture: Texture = undefined;
 
 pub fn init() void {
-    heartTexture = Texture.initFromFile("assets/cubyz/ui/hud/heart.png");
-    halfHeartTexture = Texture.initFromFile("assets/cubyz/ui/hud/half_heart.png");
-    deadHeartTexture = Texture.initFromFile("assets/cubyz/ui/hud/dead_heart.png");
+	heartTexture = Texture.initFromFile("assets/cubyz/ui/hud/heart.png");
+	halfHeartTexture = Texture.initFromFile("assets/cubyz/ui/hud/half_heart.png");
+	deadHeartTexture = Texture.initFromFile("assets/cubyz/ui/hud/dead_heart.png");
 }
 
 pub fn deinit() void {
-    heartTexture.deinit();
-    halfHeartTexture.deinit();
-    deadHeartTexture.deinit();
+	heartTexture.deinit();
+	halfHeartTexture.deinit();
+	deadHeartTexture.deinit();
 }
 
 pub fn render() void {
-    draw.setColor(0xffffffff);
-    var y: f32 = 0;
-    var x: f32 = 0;
-    var health: f32 = 0;
-    while (health < main.game.Player.maxHealth) : (health += 1) {
-        if (x >= window.contentSize[0]) {
-            x = 0;
-            y += 10;
-        }
-        if (health + 1 <= main.game.Player.health) {
-            heartTexture.bindTo(0);
-        } else if (health + 0.5 <= main.game.Player.health) {
-            halfHeartTexture.bindTo(0);
-        } else {
-            deadHeartTexture.bindTo(0);
-        }
-        draw.boundImage(Vec2f{ x, window.contentSize[1] - y - 10 }, .{ 10, 10 });
-        x += 10;
-    }
-    y += 10;
-    if (y != window.contentSize[1]) {
-        window.contentSize[1] = y;
-        gui.updateWindowPositions();
-    }
+	draw.setColor(0xffffffff);
+	var y: f32 = 0;
+	var x: f32 = 0;
+	var health: f32 = 0;
+	while (health < main.game.Player.maxHealth) : (health += 1) {
+		if (x >= window.contentSize[0]) {
+			x = 0;
+			y += 10;
+		}
+		if (health + 1 <= main.game.Player.health) {
+			heartTexture.bindTo(0);
+		} else if (health + 0.5 <= main.game.Player.health) {
+			halfHeartTexture.bindTo(0);
+		} else {
+			deadHeartTexture.bindTo(0);
+		}
+		draw.boundImage(Vec2f{ x, window.contentSize[1] - y - 10 }, .{ 10, 10 });
+		x += 10;
+	}
+	y += 10;
+	if (y != window.contentSize[1]) {
+		window.contentSize[1] = y;
+		gui.updateWindowPositions();
+	}
 }
