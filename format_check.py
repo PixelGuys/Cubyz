1e05bd42982ec3516a4f273a5ba3972d70efe1d8
ITR <ITR@live.no>
2024-09-01 22:30:44 +0200

    Made format_check log the file path if an exception occurs

--- format_check.py
+++ format_check.py
@@ -1,6 +1,6 @@
 import os
-
 import sys
+import traceback
 
 directory = os.fsencode(".")
 
@@ -17,28 +17,31 @@ for subdir, dirs, files in os.walk("."):
 		if filepath.startswith(f".{os.sep}.zig-cache"): continue
 
 		if filepath.endswith(".json") or filepath.endswith(".zig") or filepath.endswith(".py") or filepath.endswith(".zon") or filepath.endswith(".vs") or filepath.endswith(".fs") or filepath.endswith(".glsl"):
-			with open(filepath, "r", newline = '') as f:
-				string = f.read()
-				line = 1
-				lineStart = True
-				for i, c in enumerate(string):
-					if(c == '\r'):
-						print("Incorrect line ending \\r in file ", filepath, " in line ", line, ". Please configure your editor to use LF instead of CRLF.")
-						fails += 1
-					elif(c == '\n'):
-						line += 1
-						lineStart = True
-					elif(c == ' '):
-						if(lineStart):
-							print("Incorrect indentation in file ", filepath, " in line ", line, ". Please use tabs instead of spaces.")
+			try:
+				with open(filepath, "r", newline = '') as f:
+					string = f.read()
+					line = 1
+					lineStart = True
+					for i, c in enumerate(string):
+						if(c == '\r'):
+							print("Incorrect line ending \\r in file ", filepath, " in line ", line, ". Please configure your editor to use LF instead of CRLF.")
 							fails += 1
-							lineStart = False # avoid repeating this error multiple times
-					elif(c == '\t'):
-						continue
-					elif(lineStart):
-						lineStart = False
-					else:
-						continue
+						elif(c == '\n'):
+							line += 1
+							lineStart = True
+						elif(c == ' '):
+							if(lineStart):
+								print("Incorrect indentation in file ", filepath, " in line ", line, ". Please use tabs instead of spaces.")
+								fails += 1
+								lineStart = False # avoid repeating this error multiple times
+						elif(c == '\t'):
+							continue
+						elif(lineStart):
+							lineStart = False
+						else:
+							continue
+			except:
+				print("Exception when reading file ", filepath, ": ", traceback.format_exc())
 if(fails != 0):
 	sys.exit(1)
 sys.exit(0)
