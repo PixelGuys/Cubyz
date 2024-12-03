518bc0bf1f7145a55324c52e4d226dc28e8d8e96
IntegratedQuantum <jahe788@gmail.com>
2024-12-03 21:11:05 +0100

    Add flower patches
    fixes #557

--- src/server/terrain/simple_structures/_list.zig
+++ src/server/terrain/simple_structures/_list.zig
@@ -1,8 +1,9 @@
 
 
 pub const Boulder = @import("Boulder.zig");
+pub const FallenTree = @import("FallenTree.zig");
+pub const FlowerPatch = @import("FlowerPatch.zig");
 pub const GroundPatch = @import("GroundPatch.zig");
 pub const SimpleTreeModel = @import("SimpleTreeModel.zig");
-pub const FallenTree = @import("FallenTree.zig");
 pub const SimpleVegetation = @import("SimpleVegetation.zig");
 pub const Stalagmite = @import("Stalagmite.zig");
