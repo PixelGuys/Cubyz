44547e3898631e03654b36c3bca03fbf944b281f
OneAvargeCoder193 <mgiakimenko@outlook.com>
2025-01-18 12:37:28 -0500

    make an icon

--- src/gui/windows/error_prompt.zig
+++ src/gui/windows/error_prompt.zig
@@ -6,12 +6,15 @@ const main = @import("root");
 const files = main.files;
 const settings = main.settings;
 const Vec2f = main.vec.Vec2f;
+const Texture = main.graphics.Texture;
 
 const gui = @import("../gui.zig");
 const GuiWindow = gui.GuiWindow;
 const Label = @import("../components/Label.zig");
 const VerticalList = @import("../components/VerticalList.zig");
 const Button = @import("../components/Button.zig");
+
+var fileExplorerIcon: Texture = undefined;
 
 pub var window = GuiWindow {
 	.contentSize = Vec2f{128, 64},
@@ -22,6 +25,14 @@ pub var window = GuiWindow {
 		.{ .attachedToFrame = .{.selfAttachmentPoint = .lower, .otherAttachmentPoint = .lower} },
 	},
 };
+
+pub fn init() void {
+	fileExplorerIcon = Texture.initFromFile("assets/cubyz/ui/file_explorer_icon.png");
+}
+
+pub fn deinit() void {
+	fileExplorerIcon.deinit();
+}
 
 fn openLog(_: usize) void {
 	main.files.openDirInWindow("logs");
@@ -37,7 +48,7 @@ pub fn update() void {
 pub fn onOpen() void {
 	const list = VerticalList.init(.{padding, 16 + padding}, 300, 16);
 	list.add(Label.init(.{padding, 16 + padding}, 128, "#ffff00The game encountered errors. Check the logs for details", .center));
-	list.add(Button.initText(.{0, 0}, 128, "Open Log",  .{.callback = &openLog}));
+	list.add(Button.initIcon(.{0, 0}, .{16, 16}, fileExplorerIcon, false, .{.callback = &openLog}));
 	list.finish(.center);
 	window.rootComponent = list.toComponent();
 	window.contentSize = window.rootComponent.?.pos() + window.rootComponent.?.size() + @as(Vec2f, @splat(padding));
