8e159c98c2661755700b4fb45e34e6af295956af
OneAvargeCoder193 <mgiakimenko@outlook.com>
2025-01-25 16:28:19 -0500

    return serverFailure if target not found

--- src/Inventory.zig
+++ src/Inventory.zig
@@ -1610,9 +1610,7 @@ pub const Command = struct { // MARK: Command
 					}
 				}
 
-				if (target == null) {
-					return;
-				}
+				if (target == null) return .serverFailure;
 
 				if (target.?.gamemode.raw == .creative) return;
 			} else {
