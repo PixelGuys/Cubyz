0a8c2dc9a1ae1030b4ea0f57535c6514d02d6497
ikabod-kee <redurestract@gmail.com>
2025-02-23 14:17:59 -0500

    More changes

--- src/gui/windows/save_creation.zig
+++ src/gui/windows/save_creation.zig
@@ -66,11 +66,11 @@ fn flawedCreateWorld() !void {
 		mapGenerator.put("id", "cubyz:mapgen_v1"); // TODO: Make this configurable
 		generatorSettings.put("mapGenerator", mapGenerator);
 		const climateWavelengths = main.ZonElement.initObject(main.stackAllocator);
-		climateWavelengths.put("hot_cold", 2400);
+		climateWavelengths.put("hot_cold", 1600);
 		climateWavelengths.put("land_ocean", 3200);
-		climateWavelengths.put("wet_dry", 2400);
-		climateWavelengths.put("vegetation", 2400);
-		climateWavelengths.put("mountain", 500);
+		climateWavelengths.put("wet_dry", 1600);
+		climateWavelengths.put("vegetation", 1600);
+		climateWavelengths.put("mountain", 512);
 		generatorSettings.put("climateWavelengths", climateWavelengths);
 		try main.files.writeZon(generatorSettingsPath, generatorSettings);
 	}
