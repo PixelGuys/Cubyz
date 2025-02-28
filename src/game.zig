adc00c1b049c6b998f93898647be38da7678a2de
gfh <@>
2025-02-28 23:55:03 +0100

    Better Block Touch Detection

--- src/game.zig
+++ src/game.zig
@@ -768,6 +768,114 @@ pub fn hyperSpeedToggle() void {
 	Player.hyperSpeed.store(!Player.hyperSpeed.load(.monotonic), .monotonic);
 }
 
+const touchOffset = 0.01;
+
+fn isBlockIntersecting(block: ?Block, posX: i32, posY: i32, posZ: i32, center: Vec3d, extent: Vec3d) bool {
+	const model = &models.models.items[block.?.mode().model(block.?)];
+	const position = Vec3d {@floatFromInt(posX), @floatFromInt(posY), @floatFromInt(posZ)};
+	for (model.neighborFacingQuads) |quads| {
+		for (quads) |quadIndex| {
+			const quad = &models.quads.items[quadIndex];
+			if (
+				collision.triangleAABB(.{quad.corners[0] + quad.normal + position, quad.corners[2] + quad.normal + position, quad.corners[1] + quad.normal + position}, center, extent) or
+				collision.triangleAABB(.{quad.corners[1] + quad.normal + position, quad.corners[2] + quad.normal + position, quad.corners[3] + quad.normal + position}, center, extent)
+			) return true;
+		}
+	}
+	for (model.internalQuads) |quadIndex| {
+		const quad = &models.quads.items[quadIndex];
+		if (
+			collision.triangleAABB(.{quad.corners[0] + position, quad.corners[2] + position, quad.corners[1] + position}, center, extent) or
+			collision.triangleAABB(.{quad.corners[1] + position, quad.corners[2] + position, quad.corners[3] + position}, center, extent)
+		) return true;
+	}
+	return false;
+}
+
+fn touchBlocksFromDirection(entity: main.server.Entity, boundingBox: collision.Box, comptime side: main.utils.Side, comptime swizzle: Vec3i, comptime uniformMin: bool, comptime neighbor: chunk.Neighbor) void {
+	var skewedBox: collision.Box = boundingBox;
+
+	skewedBox.min[swizzle[0]] += touchOffset;
+	skewedBox.min[swizzle[1]] += touchOffset;
+	skewedBox.min[swizzle[2]] -= touchOffset;
+
+	skewedBox.max[swizzle[0]] -= touchOffset;
+	skewedBox.max[swizzle[1]] -= touchOffset;
+	skewedBox.max[swizzle[2]] += touchOffset;
+
+	const center = skewedBox.center();
+	const extent = skewedBox.extent();
+
+	const minX: i32 = @intFromFloat(@floor(skewedBox.min[swizzle[0]]));
+	const maxX: i32 = @intFromFloat(@floor(skewedBox.max[swizzle[0]]));
+	const minY: i32 = @intFromFloat(@floor(skewedBox.min[swizzle[1]]));
+	const maxY: i32 = @intFromFloat(@floor(skewedBox.max[swizzle[1]]));
+	const uniformZ: i32 =
+		if (uniformMin) @intFromFloat(@floor(skewedBox.min[swizzle[2]]))
+		else @intFromFloat(@floor(skewedBox.max[swizzle[2]]));
+
+	var posX: i32 = minX;
+	while (posX <= maxX) {
+		var posY: i32 = minY;
+		while (posY <= maxY) {
+			var blockPos: Vec3i = undefined;
+			blockPos[swizzle[0]] = posX;
+			blockPos[swizzle[1]] = posY;
+			blockPos[swizzle[2]] = uniformZ;
+			const block =
+				if (side == .client) main.renderer.mesh_storage.getBlock(blockPos[0], blockPos[1], blockPos[2])
+				else main.server.world.?.getBlock(blockPos[0], blockPos[1], blockPos[2]);
+			if (block != null and isBlockIntersecting(block, blockPos[0], blockPos[1], blockPos[2], center, extent)) {
+				block.?.onEntityTouching(entity, blockPos[0], blockPos[1], blockPos[2], neighbor);
+			}
+			posY += 1;
+		}
+		posX += 1;
+	}
+}
+
+fn touchBlocks(entity: main.server.Entity, hitBox: collision.Box, comptime side: main.utils.Side) void {
+	const boundingBox: collision.Box = .{
+		.min = entity.pos + hitBox.min + @as(Vec3d, @splat(0.0001)),
+		.max = entity.pos + hitBox.max - @as(Vec3d, @splat(0.0001))
+	};
+
+	const center = boundingBox.center();
+	const extent = boundingBox.extent();
+
+	const minX: i32 = @intFromFloat(@floor(boundingBox.min[0]));
+	const maxX: i32 = @intFromFloat(@floor(boundingBox.max[0]));
+	const minY: i32 = @intFromFloat(@floor(boundingBox.min[1]));
+	const maxY: i32 = @intFromFloat(@floor(boundingBox.max[1]));
+	const minZ: i32 = @intFromFloat(@floor(boundingBox.min[2]));
+	const maxZ: i32 = @intFromFloat(@floor(boundingBox.max[2]));
+
+	var posX: i32 = minX;
+	while(posX <= maxX) {
+		var posY: i32 = minY;
+		while(posY <= maxY) {
+			var posZ: i32 = minZ;
+			while(posZ <= maxZ) {
+				const block =
+					if (side == .client) main.renderer.mesh_storage.getBlock(posX, posY, posZ)
+					else main.server.world.?.getBlock(posX, posY, posZ);
+				if (block != null and isBlockIntersecting(block, posX, posY, posZ, center, extent)) {
+					block.?.onEntityInside(entity, posX, posY, posZ);
+				}
+				posZ += 1;
+			}
+			posY += 1;
+		}
+		posX += 1;
+	}
+	touchBlocksFromDirection(entity, boundingBox, side, Vec3i {0, 1, 2}, true, chunk.Neighbor.dirUp);
+	touchBlocksFromDirection(entity, boundingBox, side, Vec3i {0, 1, 2}, false, chunk.Neighbor.dirDown);
+	touchBlocksFromDirection(entity, boundingBox, side, Vec3i {1, 2, 0}, true, chunk.Neighbor.dirPosX);
+	touchBlocksFromDirection(entity, boundingBox, side, Vec3i {1, 2, 0}, false, chunk.Neighbor.dirNegX);
+	touchBlocksFromDirection(entity, boundingBox, side, Vec3i {0, 2, 1}, true, chunk.Neighbor.dirPosY);
+	touchBlocksFromDirection(entity, boundingBox, side, Vec3i {0, 2, 1}, false, chunk.Neighbor.dirNegY);
+}
+
 pub fn update(deltaTime: f64) void { // MARK: update()
 	const gravity = 30.0;
 	const terminalVelocity = 90.0;
@@ -1132,6 +1240,7 @@ pub fn update(deltaTime: f64) void { // MARK: update()
 		} else if (Player.eyeCoyote > 0) {
 			Player.eyePos[2] -= move[2];
 		}
+		touchBlocks(Player.super, hitBox, .client);
 	} else {
 		Player.super.pos += move;
 	}
