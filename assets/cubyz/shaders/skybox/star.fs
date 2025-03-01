420dfa75a7def6e3b067150ee5cc3d68243cfb00
OneAvargeCoder193 <mgiakimenko@outlook.com>
2025-03-01 18:49:22 -0500

    make stars triangles instead of points

--- assets/cubyz/shaders/skybox/star.fs
+++ assets/cubyz/shaders/skybox/star.fs
@@ -1,13 +1,16 @@
 #version 430
 
+in vec3 pos;
+in vec3 starPos;
 in vec3 color;
 
 uniform float starOpacity;
-uniform float starScale;
 
 layout (location = 0, index = 0) out vec4 fragColor;
 
 void main() {
-	float starScaleOpacity = min(starScale, 1);
-	fragColor = vec4(color * starOpacity * starScaleOpacity, 1);
+	if (distance(pos, starPos) > 1.0/3.0)
+		discard;
+
+	fragColor = vec4(color * starOpacity, 1);
 }
