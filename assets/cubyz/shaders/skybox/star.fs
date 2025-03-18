dd51c88cf2314d811c750778c90e235973401a82
OneAvargeCoder193 <mgiakimenko@outlook.com>
2025-03-18 16:55:00 -0400

    fix some issues

--- assets/cubyz/shaders/skybox/star.fs
+++ assets/cubyz/shaders/skybox/star.fs
@@ -4,13 +4,11 @@ in vec3 pos;
 in vec3 starPos;
 in vec3 color;
 
-uniform float starOpacity;
-
 layout (location = 0, index = 0) out vec4 fragColor;
 
 void main() {
-	if (distance(pos, starPos) > 1.0/3.0)
+	if (distance(pos, starPos) > 0.288)
 		discard;
 
-	fragColor = vec4(color * starOpacity, 1);
+	fragColor = vec4(color, 1);
 }
