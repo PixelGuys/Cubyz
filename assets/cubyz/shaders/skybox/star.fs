0bc7fd62c36b4b7eb3dc41b6a291018cdcdeadd0
OneAvargeCoder193 <mgiakimenko@outlook.com>
2025-03-18 16:56:11 -0400

    use square distance

--- assets/cubyz/shaders/skybox/star.fs
+++ assets/cubyz/shaders/skybox/star.fs
@@ -7,7 +7,7 @@ in vec3 color;
 layout (location = 0, index = 0) out vec4 fragColor;
 
 void main() {
-	if (distance(pos, starPos) > 0.288)
+	if (dot(pos - starPos, pos - starPos) > 0.0829)
 		discard;
 
 	fragColor = vec4(color, 1);
