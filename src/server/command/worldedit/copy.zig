11d8e4dd9145c4656e93fa42426980c96c9c97da
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2025-03-20 17:53:21 +0100

    Apply review suggestions

--- src/server/command/worldedit/copy.zig
+++ src/server/command/worldedit/copy.zig
@@ -14,30 +14,28 @@ pub fn execute(args: []const u8, source: *User) void {
 		source.sendMessage("#ff0000Too many arguments for command /copy. Expected no arguments.", .{});
 		return;
 	}
+	const pos1 = source.worldEditData.selectionPosition1 orelse {
+		return source.sendMessage("#ff0000Position 1 isn't set", .{});
+	};
+	const pos2 = source.worldEditData.selectionPosition2 orelse {
+		return source.sendMessage("#ff0000Position 2 isn't set", .{});
+	};
 
-	if(source.worldEditData.selectionPosition1) |pos1| {
-		if(source.worldEditData.selectionPosition2) |pos2| {
-			source.sendMessage("Copying: {} {}", .{pos1, pos2});
+	source.sendMessage("Copying: {} {}", .{pos1, pos2});
 
-			const result = Blueprint.capture(main.globalAllocator, pos1, pos2);
-			switch(result) {
-				.success => {
-					if(source.worldEditData.clipboard != null) {
-						source.worldEditData.clipboard.?.deinit(main.globalAllocator);
-					}
-					source.worldEditData.clipboard = result.success;
-
-					source.sendMessage("Copied selection to clipboard.", .{});
-				},
-				.failure => |e| {
-					source.sendMessage("#ff0000Error while copying block {}: {s}", .{e.pos, e.message});
-					std.log.warn("Error while copying block {}: {s}", .{e.pos, e.message});
-				},
+	const result = Blueprint.capture(main.globalAllocator, pos1, pos2);
+	switch(result) {
+		.success => {
+			if(source.worldEditData.clipboard != null) {
+				source.worldEditData.clipboard.?.deinit(main.globalAllocator);
 			}
-		} else {
-			source.sendMessage("#ff0000Position 2 isn't set", .{});
-		}
-	} else {
-		source.sendMessage("#ff0000Position 1 isn't set", .{});
+			source.worldEditData.clipboard = result.success;
+
+			source.sendMessage("Copied selection to clipboard.", .{});
+		},
+		.failure => |e| {
+			source.sendMessage("#ff0000Error while copying block {}: {s}", .{e.pos, e.message});
+			std.log.warn("Error while copying block {}: {s}", .{e.pos, e.message});
+		},
 	}
 }
