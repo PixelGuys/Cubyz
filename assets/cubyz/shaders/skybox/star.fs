a74c1404d7cbe364eae8a00465c2560ad5ff5c55
OneAvargeCoder193 <mgiakimenko@outlook.com>
2025-03-22 16:31:30 -0400

    more stuff

--- assets/cubyz/shaders/skybox/star.fs
+++ assets/cubyz/shaders/skybox/star.fs
@@ -7,7 +7,7 @@ in vec3 color;
 layout (location = 0, index = 0) out vec4 fragColor;
 
 void main() {
-	if (dot(pos - starPos, pos - starPos) > 1.0/12.0) // 1.0 / 12.0 is the radius of an 
+	if (dot(pos - starPos, pos - starPos) > 1.0/12.0)
 		discard;
 
 	fragColor = vec4(color, 1);
