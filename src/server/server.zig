80aeca1dc2d0f7b3f3570b95937a8f7fd87cac5d
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2025-03-24 05:08:12 +0100

    Remove redo code

--- src/server/server.zig
+++ src/server/server.zig
@@ -27,7 +27,6 @@ pub const WorldEditData = struct {
 	selectionPosition2: ?Vec3i = null,
 	clipboard: ?Blueprint = null,
 	undoHistory: History = .{},
-	redoHistory: History = .{},
 
 	const History = struct {
 		const DLList = std.DoublyLinkedList(Value);
@@ -82,7 +81,6 @@ pub const WorldEditData = struct {
 			self.clipboard.?.deinit(main.globalAllocator);
 		}
 		self.undoHistory.deinit();
-		self.redoHistory.deinit();
 	}
 };
 
