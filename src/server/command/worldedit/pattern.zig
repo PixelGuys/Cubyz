30393928ee37c1d38bb8c8fb7e533110abda7a07
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2025-03-27 21:39:20 +0100

    Update import

--- src/server/command/worldedit/pattern.zig
+++ src/server/command/worldedit/pattern.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const main = @import("root");
+const main = @import("main");
 const AliasTable = main.utils.AliasTable;
 const Block = main.blocks.Block;
 const ListUnmanaged = main.ListUnmanaged;
