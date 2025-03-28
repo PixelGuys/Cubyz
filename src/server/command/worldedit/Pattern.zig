b0babbd80c97091a338abacefc89712fc9ce2f6e
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2025-03-28 19:23:17 +0100

    Fix compilation errors

--- src/server/command/worldedit/Pattern.zig
+++ src/server/command/worldedit/Pattern.zig
@@ -15,23 +15,23 @@ const Entry = struct {
 
 pub fn initFromString(allocator: NeverFailingAllocator, source: []const u8) !@This() {
 	var specifiers = std.mem.splitScalar(u8, source, ',');
-	var totalWeight: f64 = 0;
+	var totalWeight: f32 = 0;
 
-	var weightedEntries: ListUnmanaged(struct {block: Block, weight: f64}) = .{};
+	var weightedEntries: ListUnmanaged(struct {block: Block, weight: f32}) = .{};
 	defer weightedEntries.deinit(main.stackAllocator);
 
 	while(specifiers.next()) |specifier| {
 		var iterator = std.mem.splitScalar(u8, specifier, '%');
 
 		// This code first assumes that specifier has form `weight%addon:block` and only if parsing of weight fails it tries to parse it as `addon:block`.
-		var weight: f64 = undefined;
+		var weight: f32 = undefined;
 		var block = main.blocks.parseBlock(iterator.rest());
 
 		const first = iterator.first();
 
-		weight = std.fmt.parseFloat(f64, first) catch blk: {
+		weight = std.fmt.parseFloat(f32, first) catch blk: {
 			// To distinguish somehow between mistyped numeric values and actual block IDs we check for addon name separator.
-			if(!std.mem.containsAtLeastScalar(u8, first, 1, ":")) return error.PatternSyntaxError;
+			if(!std.mem.containsAtLeastScalar(u8, first, 1, ':')) return error.PatternSyntaxError;
 			block = main.blocks.parseBlock(first);
 			break :blk 1.0;
 		};
@@ -41,7 +41,7 @@ pub fn initFromString(allocator: NeverFailingAllocator, source: []const u8) !@Th
 
 	const entries = allocator.alloc(Entry, weightedEntries.items.len);
 	for(weightedEntries.items, 0..) |entry, i| {
-		entries[i] = .{.block = entry.block, .chance = @as(f32, @floatFromInt(entry.weight))/@as(f32, @floatFromInt(totalWeight))};
+		entries[i] = .{.block = entry.block, .chance = entry.weight/totalWeight};
 	}
 
 	return .{.blocks = .init(allocator, entries)};
