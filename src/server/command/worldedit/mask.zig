89a819f796a2f9f338a9349be1a869d33eb15207
Krzysztof Wisniewski <argmaster.world@gmail.com>
2025-05-02 16:18:47 +0200

    Polish mask command

--- src/server/command/worldedit/mask.zig
+++ src/server/command/worldedit/mask.zig
@@ -2,15 +2,17 @@ const std = @import("std");
 
 const main = @import("main");
 const User = main.server.User;
-const EditMask = @import("EditMask.zig");
 
 const Block = main.blocks.Block;
 const Blueprint = main.blueprint.Blueprint;
+const Mask = main.blueprint.Mask;
 
-pub const description = "Set global or per brush edit mask.";
+pub const description = "Set global or per brush edit mask. When used with no mask expression it will clear current mask.";
 pub const usage =
 	\\/mask brush <mask>
+	\\/mask brush
 	\\/mask global <mask>
+	\\/mask global
 ;
 
 const SubCommands = enum {brush, global};
@@ -28,7 +30,11 @@ pub fn execute(args: []const u8, source: *User) void {
 }
 
 pub fn brush(args: []const u8, source: *User) void {
-	const mask = EditMask.initFromString(main.globalAllocator, args) catch |err| {
+	if(args.len == 0) {
+		source.sendMessage("Brushes are not implemented yet.", .{});
+		return;
+	}
+	const mask = Mask.initFromString(main.globalAllocator, args) catch |err| {
 		source.sendMessage("#ff0000Error parsing mask: {}", .{err});
 		return;
 	};
@@ -37,7 +43,13 @@ pub fn brush(args: []const u8, source: *User) void {
 }
 
 pub fn global(args: []const u8, source: *User) void {
-	const mask = EditMask.initFromString(main.globalAllocator, args) catch |err| {
+	if(args.len == 0) {
+		if(source.worldEditData.mask) |mask| mask.deinit(main.globalAllocator);
+		source.worldEditData.mask = null;
+		source.sendMessage("#00ff00Mask cleared.", .{});
+		return;
+	}
+	const mask = Mask.initFromString(main.globalAllocator, args) catch |err| {
 		source.sendMessage("#ff0000Error parsing mask: {}", .{err});
 		return;
 	};
