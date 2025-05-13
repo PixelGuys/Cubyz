f8c12ccc5eda0fab51df9cecb7a15a325c00cb6f
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2025-05-13 21:21:42 +0200

    Update test/runner.zig

--- test/runner.zig
+++ test/runner.zig
@@ -351,7 +351,7 @@ var is_fuzz_test: bool = undefined;
 extern fn fuzzer_set_name(name_ptr: [*]const u8, name_len: usize) void;
 extern fn fuzzer_init(cache_dir: FuzzerSlice) void;
 extern fn fuzzer_init_corpus_elem(input_ptr: [*]const u8, input_len: usize) void;
-extern fn fuzzer_start(testOne: *const fn([*]const u8, usize) callconv(.c) void) void;
+extern fn fuzzer_start(testOne: *const fn([*]const u8, usize) callconv(.C) void) void;
 extern fn fuzzer_coverage_id() u64;
 
 pub fn fuzz(
