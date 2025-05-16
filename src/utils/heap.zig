8f520f7c1b2219b645698069cbf89ce1dede328c
OneAvargeCoder193 <mgiakimenko@outlook.com>
2025-05-16 16:23:48 -0400

    rename

--- src/utils/heap.zig
+++ src/utils/heap.zig
@@ -3,8 +3,8 @@ const Allocator = std.mem.Allocator;
 
 const main = @import("main");
 
-var testingErrorAllocator = main.heap.ErrorHandlingAllocator.init(std.testing.allocator);
-pub var testingAllocator = testingErrorAllocator.allocator();
+var testingErrorHandlingAllocator = main.heap.ErrorHandlingAllocator.init(std.testing.allocator);
+pub var testingAllocator = testingErrorHandlingAllocator .allocator();
 
 /// Allows for stack-like allocations in a fast and safe way.
 /// It is safe in the sense that a regular allocator will be used when the buffer is full.
