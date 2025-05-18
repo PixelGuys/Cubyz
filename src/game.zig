29cad3de4f4ac0eb4716903947a23a4e011055d0
RanPix <darkorden16@gmail.com>
2025-05-18 22:50:25 +0300

    remove useless code, spawn less particles

--- src/game.zig
+++ src/game.zig
@@ -832,7 +832,6 @@ pub fn hyperSpeedToggle() void {
 	Player.hyperSpeed.store(!Player.hyperSpeed.load(.monotonic), .monotonic);
 }
 
-var timer: f64 = 0; // remove
 pub fn update(deltaTime: f64) void { // MARK: update()
 	const gravity = 30.0;
 	const terminalVelocity = 90.0;
@@ -1211,14 +1210,12 @@ pub fn update(deltaTime: f64) void { // MARK: update()
 
 	const t = 1 - @as(f32, @floatCast(@exp(-2*deltaTime)));
 
-	timer += deltaTime;
-	particles.ParticleSystem.spawn("cubyz:spark", 20, Player.getEyePosBlocking() + Vec3d{0, 0, -0.5}, true, .{
+	particles.ParticleSystem.spawn("cubyz:spark", 5, Player.getEyePosBlocking() + Vec3d{0, 0, -0.5}, true, .{
 		.shapeType = .point,
 		.size = 0,
 		.directionMode = .scatter,
 		.dir = .{0, 0, 1},
 	});
-	timer = 0;
 
 	fog.fogColor = (biome.fogColor - fog.fogColor)*@as(Vec3f, @splat(t)) + fog.fogColor;
 	fog.density = (biome.fogDensity - fog.density)*t + fog.density;
