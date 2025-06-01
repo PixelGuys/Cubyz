95f35dc7620a3a678a939b3b0f496c1513ac4d07
OneAvargeCoder193 <mgiakimenko@outlook.com>
2025-06-01 12:33:48 -0400

    fix

--- src/server/terrain/simple_structures/FallenTree.zig
+++ src/server/terrain/simple_structures/FallenTree.zig
@@ -12,6 +12,7 @@ const Vec3d = vec.Vec3d;
 const Vec3f = vec.Vec3f;
 const Vec3i = vec.Vec3i;
 const NeverFailingAllocator = main.heap.NeverFailingAllocator;
+const Neighbor = main.chunk.Neighbor;
 
 pub const id = "cubyz:fallen_tree";
 
@@ -20,7 +21,6 @@ pub const generationMode = .floor;
 const FallenTree = @This();
 
 woodBlock: u16,
-topWoodBlock: u16,
 height0: u32,
 deltaHeight: u31,
 
@@ -28,7 +28,6 @@ pub fn loadModel(arenaAllocator: NeverFailingAllocator, parameters: ZonElement) 
 	const self = arenaAllocator.create(FallenTree);
 	self.* = .{
 		.woodBlock = main.blocks.getTypeById(parameters.get([]const u8, "log", "cubyz:oak_log")),
-		.topWoodBlock = main.blocks.getTypeById(parameters.get([]const u8, "top", "cubyz:oak_top")),
 		.height0 = parameters.get(u32, "height", 6),
 		.deltaHeight = parameters.get(u31, "height_variation", 3),
 	};
@@ -37,7 +36,7 @@ pub fn loadModel(arenaAllocator: NeverFailingAllocator, parameters: ZonElement) 
 
 pub fn generateStump(self: *FallenTree, x: i32, y: i32, z: i32, chunk: *main.chunk.ServerChunk) void {
 	if(chunk.liesInChunk(x, y, z))
-		chunk.updateBlockIfDegradable(x, y, z, .{.typ = self.woodBlock, .data = 0});
+		chunk.updateBlockIfDegradable(x, y, z, .{.typ = self.woodBlock, .data = @as(u16, 1) << @intFromEnum(Neighbor.dirDown) | @as(u16, 1) << @intFromEnum(Neighbor.dirUp)});
 }
 
 pub fn generateFallen(self: *FallenTree, x: i32, y: i32, z: i32, length: u32, chunk: *main.chunk.ServerChunk, caveMap: CaveMapView, seed: *u64) void {
@@ -81,22 +80,31 @@ pub fn generateFallen(self: *FallenTree, x: i32, y: i32, z: i32, length: u32, ch
 
 	var dx: i32 = 0;
 	var dy: i32 = 0;
+	var dataLog: u16 = 0;
+	var dataTop: u16 = 0;
 
 	if(d.? == 0) {
 		dx = 1;
+		dataLog = @as(u16, 1) << @intFromEnum(Neighbor.dirPosX) | @as(u16, 1) << @intFromEnum(Neighbor.dirNegX);
+		dataTop = @as(u16, 1) << @intFromEnum(Neighbor.dirNegX);
 	} else if(d.? == 1) {
 		dx = -1;
+		dataLog = @as(u16, 1) << @intFromEnum(Neighbor.dirPosX) | @as(u16, 1) << @intFromEnum(Neighbor.dirNegX);
+		dataTop = @as(u16, 1) << @intFromEnum(Neighbor.dirPosX);
 	} else if(d.? == 2) {
 		dy = 1;
+		dataLog = @as(u16, 1) << @intFromEnum(Neighbor.dirPosY) | @as(u16, 1) << @intFromEnum(Neighbor.dirNegY);
+		dataTop = @as(u16, 1) << @intFromEnum(Neighbor.dirNegY);
 	} else if(d.? == 3) {
 		dy = -1;
+		dataLog = @as(u16, 1) << @intFromEnum(Neighbor.dirPosY) | @as(u16, 1) << @intFromEnum(Neighbor.dirNegY);
+		dataTop = @as(u16, 1) << @intFromEnum(Neighbor.dirPosY);
 	}
 
 	for(0..length) |val| {
 		const v: i32 = @intCast(val);
 		if(chunk.liesInChunk(x + dx*(v + 2), y + dy*(v + 2), z)) {
-			const typ = if(v == (length - 1)) self.topWoodBlock else self.woodBlock;
-			chunk.updateBlockIfDegradable(x + dx*(v + 2), y + dy*(v + 2), z, .{.typ = typ, .data = @intCast(d.? + 2)});
+			chunk.updateBlockIfDegradable(x + dx*(v + 2), y + dy*(v + 2), z, .{.typ = self.woodBlock, .data = if(v == (length - 1)) dataTop else dataLog});
 		}
 	}
 }
--- src/server/terrain/simple_structures/SimpleTreeModel.zig
+++ src/server/terrain/simple_structures/SimpleTreeModel.zig
@@ -12,6 +12,7 @@ const Vec3d = vec.Vec3d;
 const Vec3f = vec.Vec3f;
 const Vec3i = vec.Vec3i;
 const NeverFailingAllocator = main.heap.NeverFailingAllocator;
+const Neighbor = main.chunk.Neighbor;
 
 pub const id = "cubyz:simple_tree";
 
@@ -27,7 +28,6 @@ const Type = enum {
 typ: Type,
 leavesBlock: main.blocks.Block,
 woodBlock: main.blocks.Block,
-topWoodBlock: main.blocks.Block,
 height0: i32,
 deltaHeight: u31,
 leafRadius: f32,
@@ -44,8 +44,7 @@ pub fn loadModel(arenaAllocator: NeverFailingAllocator, parameters: ZonElement) 
 			break :blk .round;
 		},
 		.leavesBlock = main.blocks.parseBlock(parameters.get([]const u8, "leaves", "cubyz:oak_leaves")),
-		.woodBlock = main.blocks.parseBlock(parameters.get([]const u8, "log", "cubyz:oak_log")),
-		.topWoodBlock = main.blocks.parseBlock(parameters.get([]const u8, "top", "cubyz:oak_top")),
+		.woodBlock = main.blocks.parseBlock(parameters.get([]const u8, "log", "cubyz:log/oak")),
 		.height0 = parameters.get(i32, "height", 6),
 		.deltaHeight = parameters.get(u31, "height_variation", 3),
 		.leafRadius = parameters.get(f32, "leafRadius", (1 + parameters.get(f32, "height", 6))/2),
@@ -57,37 +56,58 @@ pub fn loadModel(arenaAllocator: NeverFailingAllocator, parameters: ZonElement) 
 	return self;
 }
 
+fn getWoodBlock(self: *SimpleTreeModel, data: u16) main.blocks.Block {
+	if(self.woodBlock.mode() == main.rotation.getByID("log")) {
+		return .{.typ = self.woodBlock.typ, .data = data};
+	}
+	return self.woodBlock;
+}
+
 pub fn generateStem(self: *SimpleTreeModel, x: i32, y: i32, z: i32, height: i32, chunk: *main.chunk.ServerChunk, seed: *u64) void {
 	if(chunk.super.pos.voxelSize <= 2) {
 		var pz: i32 = chunk.startIndex(z);
 		while(pz < z + height) : (pz += chunk.super.pos.voxelSize) {
 			if(chunk.liesInChunk(x, y, pz)) {
-				chunk.updateBlockIfDegradable(x, y, pz, if(pz == z + height - 1) self.topWoodBlock else self.woodBlock);
+				var data: u16 = 0;
+				if(pz != z + height - 1) {
+					data |= @as(u16, 1) << @intFromEnum(Neighbor.dirUp);
+				}
+				data |= @as(u16, 1) << @intFromEnum(Neighbor.dirDown);
 
 				if(self.branched) {
 					const chance = @sqrt(@as(f32, @floatFromInt(pz - z))/@as(f32, @floatFromInt(height*2)));
 					if(main.random.nextFloat(seed) < chance) {
 						const d = main.random.nextIntBounded(u32, seed, 4);
-						generateBranch(self, x, y, pz, d, chunk, seed);
+						if(generateBranch(self, x, y, pz, d, chunk, seed)) |side| {
+							data |= @as(u16, 1) << @intFromEnum(side);
+						}
 					}
 				}
+
+				chunk.updateBlockIfDegradable(x, y, pz, self.getWoodBlock(data));
 			}
 		}
 	}
 }
 
-pub fn generateBranch(self: *SimpleTreeModel, x: i32, y: i32, z: i32, d: u32, chunk: *main.chunk.ServerChunk, seed: *u64) void {
+pub fn generateBranch(self: *SimpleTreeModel, x: i32, y: i32, z: i32, d: u32, chunk: *main.chunk.ServerChunk, seed: *u64) ?Neighbor {
 	_ = seed;
 
 	if(d == 0 and chunk.liesInChunk(x + 1, y, z)) {
-		chunk.updateBlockIfDegradable(x + 1, y, z, .{.typ = self.topWoodBlock.typ, .data = 2});
+		chunk.updateBlockIfDegradable(x + 1, y, z, self.getWoodBlock(@as(u16, 1) << @intFromEnum(Neighbor.dirNegX)));
+		return .dirPosX;
 	} else if(d == 1 and chunk.liesInChunk(x - 1, y, z)) {
-		chunk.updateBlockIfDegradable(x - 1, y, z, .{.typ = self.topWoodBlock.typ, .data = 3});
+		chunk.updateBlockIfDegradable(x - 1, y, z, self.getWoodBlock(@as(u16, 1) << @intFromEnum(Neighbor.dirPosX)));
+		return .dirNegX;
 	} else if(d == 2 and chunk.liesInChunk(x, y + 1, z)) {
-		chunk.updateBlockIfDegradable(x, y + 1, z, .{.typ = self.topWoodBlock.typ, .data = 4});
+		chunk.updateBlockIfDegradable(x, y + 1, z, self.getWoodBlock(@as(u16, 1) << @intFromEnum(Neighbor.dirNegY)));
+		return .dirPosY;
 	} else if(d == 3 and chunk.liesInChunk(x, y - 1, z)) {
-		chunk.updateBlockIfDegradable(x, y - 1, z, .{.typ = self.topWoodBlock.typ, .data = 5});
+		chunk.updateBlockIfDegradable(x, y - 1, z, self.getWoodBlock(@as(u16, 1) << @intFromEnum(Neighbor.dirPosY)));
+		return .dirNegY;
 	}
+
+	return null;
 }
 
 pub fn generate(self: *SimpleTreeModel, _: GenerationMode, x: i32, y: i32, z: i32, chunk: *main.chunk.ServerChunk, caveMap: CaveMapView, _: CaveBiomeMapView, seed: *u64, _: bool) void {
