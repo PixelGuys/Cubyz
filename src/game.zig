65050676b5787ed14095fff64adb46d5daf9f10d
codemob-dev <69110900+codemob-dev@users.noreply.github.com>
2025-07-14 11:28:30 -0400

    Improve how lerpedDir is handled.

--- src/game.zig
+++ src/game.zig
@@ -942,8 +942,8 @@ pub fn update(deltaTime: f64) void { // MARK: update()
 		const fricMul = speedMultiplier*baseFrictionCoefficient*if(Player.isFlying.load(.monotonic)) 1.0 else mobility;
 
 		const forward = vec.rotateZ(Vec3d{0, 1, 0}, -camera.rotation[2]);
-		const forward3d = vec.rotateZ(vec.rotateX(Vec3d{0, 1, 0}, -camera.rotation[0]), -camera.rotation[2]);
-		const lerpedDir = if(density > 0.1) vec.lerp(forward, forward3d, density) else forward;
+		const lerpAmount: Vec3d = @splat(density);
+		const lerpedDir = if(density > 0.1) std.math.lerp(forward, camera.direction, lerpAmount) else forward;
 		const right = Vec3d{-forward[1], forward[0], 0};
 		var movementDir: Vec3d = .{0, 0, 0};
 		var movementSpeed: f64 = 0;
