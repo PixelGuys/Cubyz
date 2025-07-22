d6199c330ec8f413894b923e966e764d32c691eb
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2025-07-23 00:09:15 +0200

    Add getId()

--- src/Inventory.zig
+++ src/Inventory.zig
@@ -2057,7 +2057,7 @@ pub fn fromBytes(self: Inventory, reader: *BinaryReader) void {
 	for(0..remainingCount) |_| {
 		var stack = ItemStack.fromBytes(reader) catch continue;
 		if(stack.item) |item| {
-			std.log.err("Lost {} of {s}", .{stack.amount, item.getName()});
+			std.log.err("Lost {} of {s}", .{stack.amount, item.getId()});
 		}
 		stack.deinit();
 	}
