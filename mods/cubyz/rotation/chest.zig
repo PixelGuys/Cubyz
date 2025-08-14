5963f63c9b66bdf493ffb89f8d4bbe6783aeb3dc
OneAvargeCoder193 <mgiakimenko@outlook.com>
2025-08-14 11:15:55 -0400

    formatting

--- mods/cubyz/rotation/chest.zig
+++ mods/cubyz/rotation/chest.zig
@@ -58,7 +58,7 @@ pub fn createBlockModel(_: Block, _: *u16, zon: ZonElement) ModelIndex {
 	_ = openModel.transformModel(rotation.rotationMatrixTransform, .{Mat4f.rotationZ(-std.math.pi/2.0)});
 	_ = openModel.transformModel(rotation.rotationMatrixTransform, .{Mat4f.rotationZ(std.math.pi)});
 	_ = openModel.transformModel(rotation.rotationMatrixTransform, .{Mat4f.identity()});
-	
+
 	const lidModel = main.models.getModelIndex(lidId).model();
 
 	_ = lidModel.transformModel(rotation.rotationMatrixTransform, .{Mat4f.identity()});
