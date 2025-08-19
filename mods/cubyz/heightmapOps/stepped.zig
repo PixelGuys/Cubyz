3a2440abdb44d809eb211a352fcf9add6c55e742
codemob-dev <69110900+codemob-dev@users.noreply.github.com>
2025-08-19 19:58:22 -0400

    Improve ferrock mountains

--- mods/cubyz/heightmapOps/stepped.zig
+++ mods/cubyz/heightmapOps/stepped.zig
@@ -1,9 +1,14 @@
 pub fn run(roughnessValue: f32, hillsValue: f32, mountainsValue: f32, roughness: f32, hills: f32, mountains: f32) f32 {
     const variation = roughness * roughnessValue + mountainsValue * mountains;
-    const steps = stepFunction(hillsValue*hills/16)*16;
+    const steps = stepFunction(hillsValue*hills/8)*8;
     return variation + steps;
 }
 
 fn stepFunction(x: f32) f32 {
-    return @abs(@mod(x, 2.0) - 1.0) + x;
+    const xmod2 = @mod(x, 2);
+    if(xmod2 < 1) {
+        return 2.0*xmod2*xmod2 - xmod2 + x;
+    } else {
+        return @mod(2 - x, 2) + x;
+    }
 }
