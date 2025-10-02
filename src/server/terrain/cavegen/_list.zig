1541c719ff4b00bf93190ad4fb27165098623cfd
IntegratedQuantum <jahe788@gmail.com>
2025-10-02 23:10:05 +0200

    hemisphere geneator prototype

--- src/server/terrain/cavegen/_list.zig
+++ src/server/terrain/cavegen/_list.zig
@@ -1,3 +1,3 @@
 pub const FractalCaveGenerator = @import("FractalCaveGenerator.zig");
-pub const NoiseCaveGenerator = @import("NoiseCaveGenerator.zig");
+pub const NoiseCaveGenerator = @import("HemiSphereGenerator.zig");
 pub const SurfaceGenerator = @import("SurfaceGenerator.zig");
