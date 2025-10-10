f9f824104ffefe92529e1bce5442715ea0cb821d
eve <145144394+eve-v0@users.noreply.github.com>
2025-10-10 14:44:38 +0200

    style according to style guide

--- src/gui/GuiWindow.zig
+++ src/gui/GuiWindow.zig
@@ -176,8 +176,8 @@ pub fn mainButtonReleased(self: *GuiWindow, mousePosition: Vec2f) void {
 		const grabPositionRelative = if (grabPosition) |gp| gp - self.pos else @as(@Vector(2, f32), .{0.0, 0.0});
 		const windowEnd: f32 = closePos + (btnPos[0]-btnPos[1]);
 		
-		if(utils.inRange(f32, mousePositionRelative[1], 0, btnPos[0]-btnPos[1])){
-			if(utils.inRange(f32, mousePositionRelative[0], zoomInPos, zoomOutPos) and utils.inRange(f32, grabPositionRelative[0], zoomInPos, zoomOutPos)) {
+		if(utils.InRange(f32, mousePositionRelative[1], 0, btnPos[0]-btnPos[1])){
+			if(utils.InRange(f32, mousePositionRelative[0], zoomInPos, zoomOutPos) and utils.InRange(f32, grabPositionRelative[0], zoomInPos, zoomOutPos)) {
 				//Zoom in
 				if(self.scale >= 1) {
 					self.scale += 0.5;
@@ -187,7 +187,7 @@ pub fn mainButtonReleased(self: *GuiWindow, mousePosition: Vec2f) void {
 				gui.updateWindowPositions();
 				gui.save();
 			}
-			if(utils.inRange(f32, mousePositionRelative[0], zoomOutPos, closePos) and utils.inRange(f32, grabPositionRelative[0],zoomOutPos, closePos)) {
+			if(utils.InRange(f32, mousePositionRelative[0], zoomOutPos, closePos) and utils.InRange(f32, grabPositionRelative[0],zoomOutPos, closePos)) {
 				//Zoom out
 				if(self.scale > 1) {
 					self.scale -= 0.5;
@@ -198,7 +198,7 @@ pub fn mainButtonReleased(self: *GuiWindow, mousePosition: Vec2f) void {
 				gui.updateWindowPositions();
 				gui.save();
 			}
-			if(utils.inRange(f32, mousePositionRelative[0], closePos, windowEnd) and utils.inRange(f32, grabPositionRelative[0], closePos, windowEnd)) {
+			if(utils.InRange(f32, mousePositionRelative[0], closePos, windowEnd) and utils.InRange(f32, grabPositionRelative[0], closePos, windowEnd)) {
 				//Close
 				if(self.closeable) gui.closeWindowFromRef(self);
 			}
