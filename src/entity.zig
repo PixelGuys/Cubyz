0592f0ab4fbd26489495a2d95ed5642b997d1e10
eve <145144394+eve-v0@users.noreply.github.com>
2025-10-12 14:14:59 +0200

    removed old commented code

--- src/entity.zig
+++ src/entity.zig
@@ -210,8 +210,6 @@ pub const ClientEntityManager = struct {
 					@floatCast(pos[2] - 1.0 + 0.09375),
 				}))
 				.mul(Mat4f.rotationZ(-ent.rot[2]))
-				// .mul(Mat4f.rotationY(-ent.rot[1]))
-				// .mul(Mat4f.rotationX(-ent.rot[0]))
 			);
 			const modelViewMatrix = game.camera.viewMatrix.mul(modelMatrix);
 			c.glUniformMatrix4fv(uniforms.viewMatrix, 1, c.GL_TRUE, @ptrCast(&modelViewMatrix));
