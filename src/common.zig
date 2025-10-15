d49aaec77a99b1369253de797f0f0d9d002f497a
BoySanic <boysonicrevived@gmail.com>
2025-10-15 12:22:22 -0700

    Split Gamemode and DamageType out of game.zig
    The purpose of doing this is to prepare for a future headless server.
    We would not want to use game.zig as it currently stands in a headless
    server. We only really care about the Gamemode and DamageType types it
    provides.
    
    As a stopgap, and to reduce the blast radius of this change, I've made
    them accessible via game.Gamemode and game.DamageType as they had been
    before.
    
    In the future it might be a good idea to encapsulate it in its own
    struct (i.e. Game) and game.zig becomes client.zig or something, maybe.

---
+++ src/common.zig
@@ -0,0 +1,17 @@
+const main = @import("main");
+
+pub const Gamemode = enum(u8) { survival = 0, creative = 1 };
+
+pub const DamageType = enum(u8) {
+    heal = 0, // For when you are adding health
+    kill = 1,
+    fall = 2,
+
+    pub fn sendMessage(self: DamageType, name: []const u8) void {
+        switch (self) {
+            .heal => main.server.sendMessage("{s}§#ffffff was healed", .{name}),
+            .kill => main.server.sendMessage("{s}§#ffffff was killed", .{name}),
+            .fall => main.server.sendMessage("{s}§#ffffff died of fall damage", .{name}),
+        }
+    }
+};
