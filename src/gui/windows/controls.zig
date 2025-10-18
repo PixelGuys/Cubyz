badea17fbcf4d56f316f59adccf1c3710cdb3b8b
MaxHobbyDev <maxidoo@live.co.uk>
2025-10-18 20:31:50 +0100

    format

--- src/gui/windows/controls.zig
+++ src/gui/windows/controls.zig
@@ -115,7 +115,7 @@ pub fn onOpen() void {
 		list.add(titleRow);
 
 		for(&main.KeyBoard.keys) |*key| {
-			if (key.group != group) continue;
+			if(key.group != group) continue;
 			const label = Label.init(.{0, 0}, 128, key.name, .left);
 			const button = if(key == selectedKey) (Button.initText(.{16, 0}, 128, "...", .{})) else (Button.initText(.{16, 0}, 128, if(editingKeyboard) key.getName() else key.getGamepadName(), .{.callback = if(editingKeyboard) &keyFunction else &gamepadFunction, .arg = @intFromPtr(key)}));
 			const unbindBtn = Button.initText(.{16, 0}, 64, "Unbind", .{.callback = &unbindKey, .arg = @intFromPtr(key)});
