4c33ca4d39bd311632ecfd365fa58a50c4e6fe30
BoySanic <boysonicrevived@gmail.com>
2025-10-19 14:39:43 -0700

    Fix incorrectly named setting

--- src/main.zig
+++ src/main.zig
@@ -541,7 +541,7 @@ pub fn main() void { // MARK: main()
 	server.terrain.globalInit();
 	defer server.terrain.globalDeinit();
 
-	if(settings.launchConfig.headlessServerMode) {
+	if(settings.launchConfig.headlessServer) {
 		blocks.meshes.init();
 		defer blocks.meshes.deinit();
 
