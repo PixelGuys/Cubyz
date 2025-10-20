6e0a6791265364fa08b55128217666f0bd02b688
IntegratedQuantum <jahe788@gmail.com>
2025-10-20 20:08:06 +0200

    How did this even compile?

--- src/network.zig
+++ src/network.zig
@@ -2079,7 +2079,7 @@ pub const Connection = struct { // MARK: Connection
 			if(@errorReturnTrace()) |trace| {
 				std.log.info("{f}", .{trace});
 			}
-			std.log.debug("Packet data: {any}", data);
+			std.log.debug("Packet data: {any}", .{data});
 			self.disconnect();
 		};
 	}
