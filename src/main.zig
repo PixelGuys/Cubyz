381b9a920a155f0c8927f48c47e10ce699be32cc
notcancername <notcancername@protonmail.com>
2025-10-23 09:10:03 +0200

    call `@breakpoint` and `@trap` to die on panic
    in favor of calling `std.log.defaultPanic`, as that logs the stack
    trace twice and it gets sent to stderr by the logging machinery anyway

--- src/main.zig
+++ src/main.zig
@@ -253,7 +253,9 @@ pub fn panicToLog(msg: []const u8, first_trace_address: ?usize) noreturn {
 		std.log.err("failed to dump stack trace", .{});
 	};
 	std.log.err("stack trace: {s}", .{fbw.buffered()});
-	std.debug.defaultPanic(msg, addr);
+
+	@breakpoint();
+	@trap();
 }
 
 pub const panic = std.debug.FullPanic(panicToLog);
