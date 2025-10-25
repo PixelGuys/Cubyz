399fa04048ceb4115301a7ca4677c9e333ceade8
BoySanic <boysonicrevived@gmail.com>
2025-10-24 17:27:00 -0700

    Refactor my own refactor

--- src/server/terrain/biomes.zig
+++ src/server/terrain/biomes.zig
@@ -10,66 +10,9 @@ const vec = @import("main.vec");
 const Vec3f = main.vec.Vec3f;
 const Vec3d = main.vec.Vec3d;
 
-pub const SimpleStructureModel = struct { // MARK: SimpleStructureModel
-	pub const GenerationMode = enum {
-		floor,
-		ceiling,
-		floor_and_ceiling,
-		air,
-		underground,
-		water_surface,
-	};
-	const VTable = struct {
-		loadModel: *const fn(parameters: ZonElement) *anyopaque,
-		generate: *const fn(self: *anyopaque, generationMode: GenerationMode, x: i32, y: i32, z: i32, chunk: *ServerChunk, caveMap: terrain.CaveMap.CaveMapView, biomeMap: terrain.CaveBiomeMap.CaveBiomeMapView, seed: *u64, isCeiling: bool) void,
-		hashFunction: *const fn(self: *anyopaque) u64,
-		generationMode: GenerationMode,
-	};
-
-	vtable: VTable,
-	data: *anyopaque,
-	chance: f32,
-	priority: f32,
-	generationMode: GenerationMode,
-
-	pub fn initModel(parameters: ZonElement) ?SimpleStructureModel {
-		const id = parameters.get([]const u8, "id", "");
-		const vtable = modelRegistry.get(id) orelse {
-			std.log.err("Couldn't find structure model with id {s}", .{id});
-			return null;
-		};
-		return SimpleStructureModel{
-			.vtable = vtable,
-			.data = vtable.loadModel(parameters),
-			.chance = parameters.get(f32, "chance", 0.1),
-			.priority = parameters.get(f32, "priority", 1),
-			.generationMode = std.meta.stringToEnum(GenerationMode, parameters.get([]const u8, "generationMode", "")) orelse vtable.generationMode,
-		};
-	}
-
-	pub fn generate(self: SimpleStructureModel, x: i32, y: i32, z: i32, chunk: *ServerChunk, caveMap: terrain.CaveMap.CaveMapView, biomeMap: terrain.CaveBiomeMap.CaveBiomeMapView, seed: *u64, isCeiling: bool) void {
-		self.vtable.generate(self.data, self.generationMode, x, y, z, chunk, caveMap, biomeMap, seed, isCeiling);
-	}
-
-	var modelRegistry: std.StringHashMapUnmanaged(VTable) = .{};
-
-	pub fn registerGenerator(comptime Generator: type) void {
-		var self: VTable = undefined;
-		self.loadModel = main.utils.castFunctionReturnToAnyopaque(Generator.loadModel);
-		self.generate = main.utils.castFunctionSelfToAnyopaque(Generator.generate);
-		self.hashFunction = main.utils.castFunctionSelfToAnyopaque(struct {
-			fn hash(ptr: *Generator) u64 {
-				return hashGeneric(ptr.*);
-			}
-		}.hash);
-		self.generationMode = Generator.generationMode;
-		modelRegistry.put(main.globalArena.allocator, Generator.id, self) catch unreachable;
-	}
-
-	fn getHash(self: SimpleStructureModel) u64 {
-		return self.vtable.hashFunction(self.data);
-	}
-};
+const structures_zig = @import("structures.zig");
+const SimpleStructureModel = structures_zig.SimpleStructureModel;
+const StructureTable = structures_zig.StructureTable;
 
 const Stripe = struct { // MARK: Stripe
 	direction: ?Vec3d,
@@ -405,7 +348,7 @@ pub const Biome = struct { // MARK: Biome
 	}
 
 	fn getCheckSum(self: *Biome) u64 {
-		return hashGeneric(self.*);
+		return terrain.hashGeneric(self.*);
 	}
 };
 
