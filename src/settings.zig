c03a1f1473238fef89de2d7872c9c0b42cc79a59
H41ogen <znelsen@proton.me>
2025-10-31 03:27:14 -0500

    example commit

--- src/settings.zig
+++ src/settings.zig
@@ -68,7 +68,7 @@ pub var developerGPUInfiniteLoopDetection: bool = false;
 pub var controllerAxisDeadzone: f32 = 0.0;
 
 pub var verticalHotbar: bool = true;
-
+//example
 const settingsFile = if(builtin.mode == .Debug) "debug_settings.zig.zon" else "settings.zig.zon";
 
 pub fn init() void {
