273a7a011f82d7c595edf11e05da23a2ad760a06
BoySanic <boysonicrevived@gmail.com>
2025-11-06 08:12:09 -0800

    Change type to avoid a cast

--- src/Inventory.zig
+++ src/Inventory.zig
@@ -1644,7 +1644,7 @@ pub const Command = struct { // MARK: Command
 			if(self.amount > sourceStack.amount) return;
 
 			var remainingAmount = self.amount;
-			var selectedEmptySlot: ?i32 = null;
+			var selectedEmptySlot: ?u32 = null;
 			for(self.dest._items, 0..) |*destStack, destSlot| {
 				if(destStack.item == null and selectedEmptySlot == null) {
 					selectedEmptySlot = @intCast(destSlot);
@@ -1663,7 +1663,7 @@ pub const Command = struct { // MARK: Command
 			}
 			if(remainingAmount > 0 and selectedEmptySlot != null) {
 				cmd.executeBaseOperation(allocator, .{.move = .{
-					.dest = .{.inv = self.dest, .slot = @intCast(selectedEmptySlot.?)},
+					.dest = .{.inv = self.dest, .slot = selectedEmptySlot.?},
 					.source = self.source,
 					.amount = remainingAmount,
 				}}, side);
