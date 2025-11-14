67ef01773bdd5ca192a5d7c676e04b4197162bc4
Vinywar123 <bahnsenvincent@gmail.com>
2025-11-13 18:04:30 -0800

    Oops forgot to upload server

--- src/server/server.zig
+++ src/server/server.zig
@@ -110,6 +110,7 @@ pub const User = struct { // MARK: User
 	lastRenderDistance: u16 = 0,
 	lastPos: Vec3i = @splat(0),
 	gamemode: std.atomic.Value(main.game.Gamemode) = .init(.creative),
+	playerSpawnPos: Vec3d = .{0,0,0},
 	worldEditData: WorldEditData = undefined,
 
 	lastSentBiomeId: u32 = 0xffffffff,
