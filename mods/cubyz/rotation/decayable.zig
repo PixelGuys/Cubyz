6bc99514d86947dd1c0e0faefb6ba81790408c74
till++ <tillplusplus@gmail.com>
2025-11-22 13:31:50 +0100

    fmt

--- mods/cubyz/rotation/decayable.zig
+++ mods/cubyz/rotation/decayable.zig
@@ -15,14 +15,12 @@ const Vec3f = vec.Vec3f;
 const Vec3i = vec.Vec3i;
 const ZonElement = main.ZonElement;
 
-
 pub fn init() void {}
 pub fn deinit() void {}
 pub fn reset() void {}
 
-
 pub fn generateData(_: *main.game.World, _: Vec3i, _: Vec3f, _: Vec3f, _: Vec3i, _: ?Neighbor, block: *Block, _: Block, blockPlacing: bool) bool {
-	if(blockPlacing){
+	if(blockPlacing) {
 		block.data = 1;
 	}
 	return blockPlacing;
