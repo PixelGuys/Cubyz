186ce8bb0b25936f2815d5f3a4c17207a37b176f
till++ <tillplusplus@gmail.com>
2025-11-22 12:18:57 +0100

    use mutex and ListUnmanaged instead

--- src/server/UpdateSystem.zig
+++ src/server/UpdateSystem.zig
@@ -7,35 +7,42 @@ const Vec3i = vec.Vec3i;
 const NeverFailingAllocator = main.heap.NeverFailingAllocator;
 
 pub const UpdateSystem = struct {
-	queue: main.utils.CircularBufferQueue(Vec3i),
+	list: main.ListUnmanaged(Vec3i) = .{},
+	mutex: std.Thread.Mutex = .{},
 
 	pub fn init() UpdateSystem {
-		return .{
-			.queue = .init(main.globalAllocator, 32),
-		};
+		return .{};
 	}
-	pub fn deinit(self: UpdateSystem) void {
-		self.queue.deinit();
+	pub fn deinit(self: *UpdateSystem) void {
+		self.mutex.lock();
+		defer self.mutex.unlock();
+		self.list.deinit(main.globalAllocator);
 	}
 	pub fn add(self: *UpdateSystem, position: Vec3i) void {
-		self.queue.pushBack(position);
+		self.mutex.lock();
+		defer self.mutex.unlock();
+		self.list.append(main.globalAllocator, position);
 	}
 	pub fn update(self: *UpdateSystem, world: *main.server.ServerWorld) void {
-		// event queue
-		const amountToUpdate = self.queue.len;
-		for(0..amountToUpdate) |_| {
-			if(self.queue.popFront()) |event| {
-				var ch = world.getChunkFromCacheAndIncreaseRefCount(main.chunk.ChunkPosition.initFromWorldPos(event, 1)) orelse continue;
-				defer ch.decreaseRefCount();
-				if(world.getBlock(event[0], event[1], event[2])) |block| {
-					_ = block.onUpdate().run(.{
-						.block = block,
-						.chunk = ch,
-						.x = event[0] & main.chunk.chunkMask,
-						.y = event[1] & main.chunk.chunkMask,
-						.z = event[2] & main.chunk.chunkMask,
-					});
-				}
+		//swap
+		self.mutex.lock();
+		const list = self.list;
+		defer list.deinit(main.globalAllocator);
+		self.list = .{};
+		self.mutex.unlock();
+
+		// handle events
+		for(list.items) |event| {
+			var ch = world.getChunkFromCacheAndIncreaseRefCount(main.chunk.ChunkPosition.initFromWorldPos(event, 1)) orelse continue;
+			defer ch.decreaseRefCount();
+			if(world.getBlock(event[0], event[1], event[2])) |block| {
+				_ = block.onUpdate().run(.{
+					.block = block,
+					.chunk = ch,
+					.x = event[0] & main.chunk.chunkMask,
+					.y = event[1] & main.chunk.chunkMask,
+					.z = event[2] & main.chunk.chunkMask,
+				});
 			}
 		}
 	}
