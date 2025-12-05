2848144f2e66fa0aa68900a35ff05d9f0ac7f4af
till++ <tillplusplus@gmail.com>
2025-12-05 21:31:22 +0100

    fmt

--- src/callbacks/block/server/decay.zig
+++ src/callbacks/block/server/decay.zig
@@ -25,10 +25,10 @@ pub fn init(zon: ZonElement) ?*@This() {
 	if(zon.getChildOrNull("prevention")) |tagNames| {
 		if(tagNames == .array) {
 			const prevention = main.worldArena.alloc(main.Tag, tagNames.array.items.len);
-			var index:usize = 0;
+			var index: usize = 0;
 			for(tagNames.array.items) |value| {
 				const tagName = value.as(?[]const u8, null) orelse {
-					std.log.err("Invalid TagName for decay prevention.",.{});
+					std.log.err("Invalid TagName for decay prevention.", .{});
 					continue;
 				};
 				prevention[index] = main.Tag.find(tagName);
