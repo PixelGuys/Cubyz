PixelGuys/Cubyz

commit d25cdc530d37f50d0f8c82a005bd13abe230696d

Update to the new Zig version (0.16.0-dev.1424+d3e20e71b) (#2009)
progress towards #1854

src/server/server.zig
@@ -98,7 +98,7 @@ pub const User = struct { // MARK: User
 	timeDifference: utils.TimeDifference = .{},
 	interpolation: utils.GenericInterpolation(3) = undefined,
 	lastTime: i16 = undefined,
-	lastSaveTime: i64 = 0,
+	lastSaveTime: std.Io.Timestamp = .fromNanoseconds(0),
 	name: []const u8 = "",
 	renderDistance: u16 = undefined,
 	clientUpdatePos: Vec3i = .{0, 0, 0},
@@ -249,13 +249,13 @@ pub const User = struct { // MARK: User
 	pub fn update(self: *User) void {
 		self.mutex.lock();
 		defer self.mutex.unlock();
-		var time = @as(i16, @truncate(std.time.milliTimestamp())) -% main.settings.entityLookback;
+		var time = @as(i16, @truncate(main.timestamp().toMilliseconds())) -% main.settings.entityLookback;
 		time -%= self.timeDifference.difference.load(.monotonic);
 		self.interpolation.update(time, self.lastTime);
 		self.lastTime = time;
 
-		const saveTime = std.time.milliTimestamp();
-		if(saveTime -% self.lastSaveTime > 5000) {
+		const saveTime = main.timestamp();
+		if(self.lastSaveTime.durationTo(saveTime).toSeconds() > 5) {
 			world.?.savePlayer(self) catch |err| {
 				std.log.err("Failed to save player {s}: {s}", .{self.name, @errorName(err)});
 			};
@@ -288,7 +288,7 @@ pub const User = struct { // MARK: User
 };
 
 pub const updatesPerSec: u32 = 20;
-const updateNanoTime: u32 = 1000000000/20;
+const updateTime: std.Io.Duration = .fromNanoseconds(1000000000/20);
 
 pub var world: ?*ServerWorld = null;
 var userMutex: std.Thread.Mutex = .{};
@@ -299,7 +299,7 @@ var userConnectList: main.utils.ConcurrentQueue(*User) = undefined;
 pub var connectionManager: *ConnectionManager = undefined;
 
 pub var running: std.atomic.Value(bool) = .init(false);
-var lastTime: i128 = undefined;
+var lastTime: std.Io.Timestamp = undefined;
 
 pub var thread: ?std.Thread = null;
 
@@ -310,7 +310,7 @@ fn init(name: []const u8, singlePlayerPort: ?u16) void { // MARK: init()
 	users = .init(main.globalAllocator);
 	userDeinitList = .init(main.globalAllocator, 16);
 	userConnectList = .init(main.globalAllocator, 16);
-	lastTime = std.time.nanoTimestamp();
+	lastTime = main.timestamp();
 	connectionManager = ConnectionManager.init(main.settings.defaultPort, false) catch |err| {
 		std.log.err("Couldn't create socket: {s}", .{@errorName(err)});
 		@panic("Could not open Server.");
@@ -449,12 +449,12 @@ pub fn start(name: []const u8, port: ?u16) void {
 	running.store(true, .release);
 	while(running.load(.monotonic)) {
 		main.heap.GarbageCollection.syncPoint();
-		const newTime = std.time.nanoTimestamp();
-		if(newTime -% lastTime < updateNanoTime) {
-			std.Thread.sleep(@intCast(lastTime +% updateNanoTime -% newTime));
-			lastTime +%= updateNanoTime;
+		const newTime = main.timestamp();
+		if(lastTime.durationTo(newTime).nanoseconds < updateTime.nanoseconds) {
+			main.io.sleep(lastTime.durationTo(newTime.addDuration(updateTime)), .awake) catch {};
+			lastTime = lastTime.addDuration(updateTime);
 		} else {
-			std.log.warn("The server is lagging behind by {d:.1} ms", .{@as(f32, @floatFromInt(newTime -% lastTime -% updateNanoTime))/1000000.0});
+			std.log.warn("The server is lagging behind by {d:.1} ms", .{@as(f32, @floatFromInt(newTime.nanoseconds -% lastTime.nanoseconds -% updateTime.nanoseconds))/1000000.0});
 			lastTime = newTime;
 		}
 		update();

.github/workflows/ci.yml
@@ -19,7 +19,7 @@ jobs:
         with:
           version: ${{ env.VERSION }}
       - run: sudo apt install glslang-tools
-      - run: wget -O ./Cubyz-fmt https://github.com/PixelGuys/Cubyz-formatter/releases/download/0.15.0-dev.1034%2Bbd97b6618/Cubyz-fmt-x86_64-linux
+      - run: wget -O ./Cubyz-fmt https://github.com/PixelGuys/Cubyz-formatter/releases/download/0.16.0-dev.1424+d3e20e71b/Cubyz-fmt-x86_64-linux
       - run: chmod +x ./Cubyz-fmt
       - run: |
           if ./Cubyz-fmt *.zig src/*.zig src/*/*.zig src/*/*/*.zig src/*/*/*/*.zig src/*/*/*/*/*.zig src/*/*/*/*/*/*.zig src/*/*/*/*/*/*/*.zig src/*/*/*/*/*/*/*/*.zig src/*/*/*/*/*/*/*/*/*.zig src/*/*/*/*/*/*/*/*/*/*.zig | grep -q . ; then
@@ -44,7 +44,7 @@ jobs:
       - run: zig build
       - run: zig build -Dtarget=x86_64-windows-gnu
       - run: zig build -Dtarget=aarch64-linux-gnu
-      - run: zig build -Dtarget=aarch64-windows-gnu
+#      - run: zig build -Dtarget=aarch64-windows-gnu
       - run: zig build test
       - run: zig build format --summary none
       - run: find assets/cubyz/shaders -type f | xargs -L1 glslangValidator -G100

.github/workflows/release.yml
@@ -29,9 +29,9 @@ jobs:
       - run: tar -czf Windows-x86_64.tar.gz --transform 's,^,Cubyz/,' assets/cubyz launchConfig.zon -C zig-out/bin Cubyz.exe
       - run: tar -xf Windows-x86_64.tar.gz && zip -r Windows-x86_64.zip Cubyz && rm -r Cubyz
 
-      - run: zig build -Dtarget=aarch64-windows-gnu -Doptimize=ReleaseFast -Drelease=true -Dcpu=baseline
-      - run: tar -czf Windows-aarch64.tar.gz --transform 's,^,Cubyz/,' assets/cubyz launchConfig.zon -C zig-out/bin Cubyz.exe
-      - run: tar -xf Windows-aarch64.tar.gz && zip -r Windows-aarch64.zip Cubyz && rm -r Cubyz
+#      - run: zig build -Dtarget=aarch64-windows-gnu -Doptimize=ReleaseFast -Drelease=true -Dcpu=baseline
+#      - run: tar -czf Windows-aarch64.tar.gz --transform 's,^,Cubyz/,' assets/cubyz launchConfig.zon -C zig-out/bin Cubyz.exe
+#      - run: tar -xf Windows-aarch64.tar.gz && zip -r Windows-aarch64.zip Cubyz && rm -r Cubyz
 
       - name: Create release
         env:
@@ -43,6 +43,5 @@ jobs:
               --title="${GITHUB_REPOSITORY#*/} ${tag#v}" \
               Linux-x86_64.tar.gz \
               Linux-aarch64.tar.gz \
-              Windows-x86_64.zip \
-              Windows-aarch64.zip
+              Windows-x86_64.zip
           gh release

.zigversion
@@ -1,1 +1,1 @@
-0.15.0-dev.1034+bd97b6618
+0.16.0-dev.1424+d3e20e71b

build.zig.zon
@@ -47,8 +47,8 @@
 			.hash = "N-V-__8AACQYrweKK0-hEabTekSwmGwxKrIbdY_2QTOEA7TN",
 		},
 		.cubyz_test_runner = .{
-			.url = "https://github.com/PixelGuys/Cubyz-std-lib/archive/refs/tags/0.15.0-dev.1034+bd97b6618+runner-build.zig.tar.gz",
-			.hash = "zig-0.0.0-Fp4XJConDA0olqTe3d3oYNH6UOlQb_jIZ0YOWKZGHLYQ",
+			.url = "https://github.com/PixelGuys/Cubyz-std-lib/archive/refs/tags/0.16.0-dev.1424+1.tar.gz",
+			.hash = "zig-0.0.0-Fp4XJAXmXQ1r23tqehV14LpPFILnnXSyg7bPitqs8mrf",
 			.lazy = true,
 		},
 	},

debug_linux.sh
@@ -14,7 +14,7 @@ fi
 
 echo "Building Zig Cubyz ($@) from source. This may take a few minutes..."
 
-./compiler/zig/zig build --prominent-compile-errors "$@"
+./compiler/zig/zig build --error-style minimal "$@"
 
 if [ $? != 0 ]
 then

debug_windows.bat
@@ -10,7 +10,7 @@ if errorlevel 1 (
 
 echo Building Zig Cubyz (%*^) from source. This may take a few minutes...
 
-compiler\zig\zig build --prominent-compile-errors %*
+compiler\zig\zig build --error-style minimal %*
 
 if errorlevel 1 (
 	exit /b 1

run_windows.bat
@@ -2,7 +2,7 @@
 
 cd /D "%~dp0"
 
-call debug_windows.bat -Doptimize=ReleaseSafe %*
+call debug_windows.bat -Doptimize=ReleaseFast %*
 
 IF "%NO_PAUSE%" == "" (
 	echo Press enter key to continue. (Or set NO_PAUSE=1 to skip this prompt.^)

commit 902a91f19f4cf38179a2c60ac23241ff1a46e6c2

Fix server thread sleeping for too long (#2373)
The calculation for how long the server thread should sleep was broken
by #2009. This fixes it.

src/server/server.zig
@@ -457,7 +457,7 @@ pub fn startFromExistingThread(name: []const u8, port: ?u16) void {
 		main.heap.GarbageCollection.syncPoint();
 		const newTime = main.timestamp();
 		if(lastTime.durationTo(newTime).nanoseconds < updateTime.nanoseconds) {
-			main.io.sleep(lastTime.durationTo(newTime.addDuration(updateTime)), .awake) catch {};
+			main.io.sleep(newTime.durationTo(lastTime.addDuration(updateTime)), .awake) catch {};
 			lastTime = lastTime.addDuration(updateTime);
 		} else {
 			std.log.warn("The server is lagging behind by {d:.1} ms", .{@as(f32, @floatFromInt(newTime.nanoseconds -% lastTime.nanoseconds -% updateTime.nanoseconds))/1000000.0});