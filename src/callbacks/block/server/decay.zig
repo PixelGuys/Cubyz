d7867f97dfd92020d26ff0ac0f0064b4aad6849d
till++ <tillplusplus@gmail.com>
2025-12-06 11:42:47 +0100

    use ListUnmanaged

--- src/callbacks/block/server/decay.zig
+++ src/callbacks/block/server/decay.zig
@@ -24,17 +24,17 @@ pub fn init(zon: ZonElement) ?*@This() {
 	result.prevention = &.{};
 	if(zon.getChildOrNull("prevention")) |tagNames| {
 		if(tagNames == .array) {
-			const prevention = main.worldArena.alloc(main.Tag, tagNames.array.items.len);
+			var prevention = main.ListUnmanaged(main.Tag).initCapacity(main.worldArena, tagNames.array.items.len);
 			var index: usize = 0;
 			for(tagNames.array.items) |value| {
 				const tagName = value.as(?[]const u8, null) orelse {
 					std.log.err("Invalid TagName for decay prevention.", .{});
 					continue;
 				};
-				prevention[index] = main.Tag.find(tagName);
+				prevention.appendAssumeCapacity(main.Tag.find(tagName));
 				index += 1;
 			}
-			result.prevention = prevention[0..index];
+			result.prevention = prevention.items[0..index];
 		}
 	}
 	// branchRotation
