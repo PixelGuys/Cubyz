3b4b5ef36eb3c1cfbaf51132a16e0eb8f4ac229c
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2025-12-13 23:26:36 +0100

    Add vine decay

--- src/callbacks/block/server/decay.zig
+++ src/callbacks/block/server/decay.zig
@@ -121,7 +121,7 @@ pub fn run(self: *@This(), params: main.callbacks.ServerBlockCallback.Params) ma
 		const bd = Branch.BranchData.init(params.block.data);
 		if(bd.placedByHuman)
 			return .ignored;
-	} else {
+	} else if(params.block.mode() == main.rotation.getByID("cubyz:hanging")) {} else {
 		std.log.err("Expected {s} to have cubyz:decayable or cubyz:branch as rotation", .{params.block.id()});
 	}
 
