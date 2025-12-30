6f1cedb4babf234e546c503c792214eb0404a20c
IntegratedQuantum <jahe788@gmail.com>
2025-12-30 13:19:07 +0100

    Add a night brightness slider and make night light slightly blue

--- src/settings.zig
+++ src/settings.zig
@@ -59,6 +59,8 @@ pub var @"lod0.5Distance": f32 = 200;
 
 pub var blockContrast: f32 = 0;
 
+pub var nightBrightness: f32 = 0.5;
+
 pub var storageTime: std.Io.Duration = .fromSeconds(5);
 
 pub var updateRepeatSpeed: std.Io.Duration = .fromMilliseconds(200);
