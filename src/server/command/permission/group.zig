ab985141bd854e740b9ac53bff63043d981a3325
Wunka <56411012+Wunka@users.noreply.github.com>
2026-02-07 17:48:29 +0100

    update group command

--- src/server/command/permission/group.zig
+++ src/server/command/permission/group.zig
@@ -16,13 +16,13 @@ const operations = enum {
 
 pub fn execute(args: []const u8, source: *User) void {
 	if (args.len == 0) {
-		source.sendMessage("#ff0000Too few arguments for command /group. Expected one argument.", .{});
+		source.sendMessage("#ff0000Too few arguments for command /group. Usage: " ++ usage, .{});
 		return;
 	}
 
 	var split = std.mem.splitScalar(u8, args, ' ');
 	const opString = split.next() orelse {
-		source.sendMessage("#ff0000Too few arguments for command /group. Expected one argument.", .{});
+		source.sendMessage("#ff0000Too few arguments for command /group. Usage: " ++ usage, .{});
 		return;
 	};
 	const op: operations = std.meta.stringToEnum(operations, opString) orelse {
@@ -31,30 +31,32 @@ pub fn execute(args: []const u8, source: *User) void {
 	};
 	if (split.next()) |arg| {
 		if (split.next() != null) {
-			source.sendMessage("#ff0000Too many arguments for command /group", .{});
+			source.sendMessage("#ff0000Too many arguments for command /group. Usage: " ++ usage, .{});
 			return;
 		}
 		switch (op) {
 			.create => {
 				permission.createGroup(arg) catch {
-					source.sendMessage("#ff0000Group with name {s} already exists.", .{arg});
+					source.sendMessage("#ff0000Group {s}#ff0000 already exists.", .{arg});
 				};
 			},
 			.delete => {
 				if (!permission.deleteGroup(arg)) {
-					source.sendMessage("#ff0000Group with name {s} did not exists.", .{arg});
+					source.sendMessage("#ff0000Group {s}#ff0000 could not be removed as it already doesn't exist.", .{arg});
 				}
 			},
 			.join => {
 				source.addToGroup(arg) catch {
-					source.sendMessage("#ff0000Group {s} does not exist.", .{arg});
+					source.sendMessage("#ff0000Group {s}#ff0000 does not exist.", .{arg});
 				};
 			},
 			.leave => {
 				if (!source.removeFromGroup(arg)) {
-					source.sendMessage("#ff0000User {s} was already not in Group {s}.", .{source.name, arg});
+					source.sendMessage("#ff0000Could not leave group {s}#ff0000 as user was already not a member", .{arg});
 				}
 			},
 		}
+	} else {
+		source.sendMessage("#ff0000Too few arguments for command /group. Usage: " ++ usage, .{});
 	}
 }
