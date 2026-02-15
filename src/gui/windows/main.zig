59ffc7cb5de889fa0fa37a0d3144cbf8cd1ccdbb
OneAvargeCoder193 <michael.iakimenko@icloud.com>
2026-02-15 03:16:11 -0500

    update main menu

--- src/gui/windows/main.zig
+++ src/gui/windows/main.zig
@@ -1,30 +1,50 @@
 const std = @import("std");
 
 const main = @import("main");
+const graphics = main.graphics;
+const Texture = graphics.Texture;
+const draw = graphics.draw;
 const Vec2f = main.vec.Vec2f;
 
 const gui = @import("../gui.zig");
 const GuiComponent = gui.GuiComponent;
 const GuiWindow = gui.GuiWindow;
 const Button = @import("../components/Button.zig");
+const Label = @import("../components/Label.zig");
+const Icon = @import("../components/Icon.zig");
 const VerticalList = @import("../components/VerticalList.zig");
 
 pub var window = GuiWindow{
 	.contentSize = Vec2f{128, 256},
 	.closeable = false,
+	.hasBackground = false,
+	.showTitleBar = false,
 };
 
 const padding: f32 = 8;
+
+var logo: Texture = undefined;
+
+pub fn init() void {
+	logo = Texture.initFromFile("assets/cubyz/ui/logo.png");
+}
+
+pub fn deinit() void {
+	logo.deinit();
+}
 
 fn exitGame() void {
 	main.Window.c.glfwSetWindowShouldClose(main.Window.window, main.Window.c.GLFW_TRUE);
 }
 pub fn onOpen() void {
-	const list = VerticalList.init(.{padding, 16 + padding}, 300, 16);
-	list.add(Button.initText(.{0, 0}, 128, "Singleplayer", gui.openWindowCallback("save_selection")));
-	list.add(Button.initText(.{0, 0}, 128, "Multiplayer", gui.openWindowCallback("multiplayer")));
-	list.add(Button.initText(.{0, 0}, 128, "Settings", gui.openWindowCallback("settings")));
-	list.add(Button.initText(.{0, 0}, 128, "Touch Grass", .init(exitGame)));
+	const list = VerticalList.init(.{padding, 16 + padding}, 300, 2);
+	list.add(Icon.init(.{0, 0}, .{384, 96}, logo, true));
+	list.add(Icon.init(.{0, 0}, .{0, 64}, .{.textureID = 0}, false));
+	list.add(Button.initText(.{0, 0}, 192, "Singleplayer", gui.openWindowCallback("save_selection")));
+	list.add(Button.initText(.{0, 0}, 192, "Multiplayer", gui.openWindowCallback("multiplayer")));
+	list.add(Button.initText(.{0, 0}, 192, "Settings", gui.openWindowCallback("settings")));
+	list.add(Icon.init(.{0, 0}, .{0, 8}, .{.textureID = 0}, false));
+	list.add(Button.initText(.{0, 0}, 128, "Quit Game", .init(exitGame)));
 	list.finish(.center);
 	window.rootComponent = list.toComponent();
 	window.contentSize = window.rootComponent.?.pos() + window.rootComponent.?.size() + @as(Vec2f, @splat(padding));
@@ -35,4 +55,14 @@ pub fn onClose() void {
 	if (window.rootComponent) |*comp| {
 		comp.deinit();
 	}
+}
+
+pub fn render() void {
+	const old = draw.setTranslation(.{0, 0});
+	draw.restoreTranslation(.{0, 0});
+	defer draw.restoreTranslation(old);
+
+	const windowSize = main.Window.getWindowSize()/@as(Vec2f, @splat(gui.scale));
+	draw.setColor(0x30ffffff);
+	draw.rectBorder(.{5, 5}, windowSize - Vec2f{10, 10}, 1);
 }
