28e785b7b438e3b1d2b6944adbd3c152e633d519
IntegratedQuantum <jahe788@gmail.com>
2026-02-23 16:55:15 +0100

    Add cylinders and use them for ice caves

--- src/server/terrain/sdf_models/_list.zig
+++ src/server/terrain/sdf_models/_list.zig
@@ -1,3 +1,4 @@
+pub const cylinder = @import("cylinder.zig");
 pub const hemisphere = @import("hemisphere.zig");
 pub const rectangular_cuboid = @import("rectangular_cuboid.zig");
 pub const sphere = @import("sphere.zig");
--- src/server/terrain/sdf_models/cylinder.zig
+++ src/server/terrain/sdf_models/cylinder.zig
@@ -4,30 +4,37 @@ const main = @import("main");
 const Array3D = main.utils.Array3D;
 const sdf = main.server.terrain.sdf;
 const vec = main.vec;
+const Vec2f = vec.Vec2f;
+const Vec2i = vec.Vec2i;
 const Vec3f = vec.Vec3f;
 const Vec3i = vec.Vec3i;
 const ZonElement = main.ZonElement;
 
-pub const id = "cubyz:rectangular_cuboid";
+pub const id = "cubyz:cylinder";
 
-minRadii: Vec3f,
-maxRadii: Vec3f,
+minRadius: f32,
+maxRadius: f32,
+minHalfHeight: f32,
+maxHalfHeight: f32,
 
 pub fn init(zon: ZonElement) ?*@This() {
 	const result = main.worldArena.create(@This());
-	result.minRadii = zon.get(Vec3f, "minSideLengths", @splat(32))/@as(Vec3f, @splat(2));
-	result.maxRadii = zon.get(Vec3f, "maxSideLengths", result.minRadii*@as(Vec3f, @splat(2)))/@as(Vec3f, @splat(2));
+	result.minRadius = zon.get(f32, "minRadius", 16);
+	result.maxRadius = zon.get(f32, "maxRadius", result.minRadius);
+	result.minHalfHeight = zon.get(f32, "minHeight", 32)/2;
+	result.maxHalfHeight = zon.get(f32, "maxfHeight", result.minHalfHeight*2)/2;
 	return result;
 }
 
 pub fn generate(self: *@This(), output: main.utils.Array3D(f32), interpolationSmoothness: main.utils.Array3D(f32), relPos: Vec3i, _seed: u64, perimeter: f32, voxelSize: u31, voxelSizeShift: u5) void {
 	var seed = _seed;
-	const radii = self.minRadii + (self.maxRadii - self.minRadii)*main.random.nextFloatVector(3, &seed);
+	const radius = self.minRadius + (self.maxRadius - self.minRadius)*main.random.nextFloat(&seed);
+	const halfHeight = self.minHalfHeight + (self.maxHalfHeight - self.minHalfHeight)*main.random.nextFloat(&seed);
 
 	const relPosF32: Vec3f = @floatFromInt(relPos);
 	const dimVector: Vec3f = @floatFromInt(@Vector(3, u32){output.width*voxelSize, output.depth*voxelSize, output.height*voxelSize});
-	const min = @max(@as(Vec3f, @splat(0)), relPosF32 - radii - @as(Vec3f, @splat(perimeter)));
-	const max = @min(dimVector, relPosF32 + radii + @as(Vec3f, @splat(perimeter)));
+	const min = @max(@as(Vec3f, @splat(0)), relPosF32 - Vec3f{radius, radius, halfHeight} - @as(Vec3f, @splat(perimeter)));
+	const max = @min(dimVector, relPosF32 + Vec3f{radius, radius, halfHeight} + @as(Vec3f, @splat(perimeter)));
 
 	const minInt: @Vector(3, u31) = @intFromFloat(min);
 	const maxInt: Vec3i = @intFromFloat(@ceil(max));
@@ -38,8 +45,9 @@ pub fn generate(self: *@This(), output: main.utils.Array3D(f32), interpolationSm
 		while (y < maxInt[1]) : (y += voxelSize) {
 			var z = minInt[2] & ~(voxelSize - 1);
 			while (z < maxInt[2]) : (z += voxelSize) {
-				const dimensionalSdf: Vec3f = @as(Vec3f, @floatFromInt(@abs(Vec3i{x, y, z} - relPos))) - radii;
-				const fullSdf = vec.length(@max(dimensionalSdf, @as(Vec3f, @splat(0)))) + @min(0, @max(dimensionalSdf[0], dimensionalSdf[1], dimensionalSdf[2]));
+				const circleSdf: f32 = vec.length(@as(Vec2f, @floatFromInt(Vec2i{x, y} - vec.xy(relPos)))) - radius;
+				const heightSdf: f32 = @as(f32, @floatFromInt(@abs(z - relPos[2]))) - halfHeight;
+				const fullSdf = vec.length(@max(Vec2f{heightSdf, circleSdf}, Vec2f{0, 0})) + @min(0, @max(circleSdf, heightSdf));
 				if (fullSdf > perimeter) continue;
 
 				const out = output.ptr(x >> voxelSizeShift, y >> voxelSizeShift, z >> voxelSizeShift);
