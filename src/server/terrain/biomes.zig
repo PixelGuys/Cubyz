6e4d64d24939d9a06e66eb0fbd53b6952607714c
BoySanic <boysonicrevived@gmail.com>
2026-02-25 09:43:43 -0800

    Split structures out of biomes (#2131)
    This PR splits out structures and hashing from biomes.zig into
    structures.zig and src/utils.zig respectively.
    
    This change should be done for the following reasons:
    
    1. It cleans up #2129. A lot.
    2. Splitting it out from biomes.zig into another file makes it a bit
    cleaner once we add the changes from #2129 that introduces the
    StructureTable struct for example. It's technically feasible to leave it
    all in biomes.zig, splitting it out made the most sense to me at the
    time for organization purposes.

--- src/server/terrain/biomes.zig
+++ src/server/terrain/biomes.zig
@@ -10,70 +10,7 @@ const vec = @import("main.vec");
 const Vec3f = main.vec.Vec3f;
 const Vec3d = main.vec.Vec3d;
 
-pub const SimpleStructureModel = struct { // MARK: SimpleStructureModel
-	pub const GenerationMode = enum {
-		floor,
-		ceiling,
-		floor_and_ceiling,
-		air,
-		underground,
-		water_surface,
-	};
-	const VTable = struct {
-		loadModel: *const fn (parameters: ZonElement) ?*anyopaque,
-		generate: *const fn (self: *anyopaque, generationMode: GenerationMode, x: i32, y: i32, z: i32, chunk: *ServerChunk, caveMap: terrain.CaveMap.CaveMapView, biomeMap: terrain.CaveBiomeMap.CaveBiomeMapView, seed: *u64, isCeiling: bool) void,
-		hashFunction: *const fn (self: *anyopaque) u64,
-		generationMode: GenerationMode,
-	};
-
-	vtable: VTable,
-	data: *anyopaque,
-	chance: f32,
-	priority: f32,
-	generationMode: GenerationMode,
-
-	pub fn initModel(parameters: ZonElement) ?SimpleStructureModel {
-		const id = parameters.get([]const u8, "id", "");
-		const vtable = modelRegistry.get(id) orelse {
-			std.log.err("Couldn't find structure model with id {s}", .{id});
-			return null;
-		};
-		const vtableModel = vtable.loadModel(parameters) orelse {
-			std.log.err("Error occurred while loading structure with id '{s}'. Dropping model from biome.", .{id});
-			return null;
-		};
-		return SimpleStructureModel{
-			.vtable = vtable,
-			.data = vtableModel,
-			.chance = parameters.get(f32, "chance", 0.1),
-			.priority = parameters.get(f32, "priority", 1),
-			.generationMode = std.meta.stringToEnum(GenerationMode, parameters.get([]const u8, "generationMode", "")) orelse vtable.generationMode,
-		};
-	}
-
-	pub fn generate(self: SimpleStructureModel, x: i32, y: i32, z: i32, chunk: *ServerChunk, caveMap: terrain.CaveMap.CaveMapView, biomeMap: terrain.CaveBiomeMap.CaveBiomeMapView, seed: *u64, isCeiling: bool) void {
-		self.vtable.generate(self.data, self.generationMode, x, y, z, chunk, caveMap, biomeMap, seed, isCeiling);
-	}
-
-	var modelRegistry: std.StringHashMapUnmanaged(VTable) = .{};
-
-	pub fn registerGenerator(comptime Generator: type) void {
-		var self: VTable = undefined;
-		self.loadModel = main.meta.castFunctionReturnToOptionalAnyopaque(Generator.loadModel);
-		self.generate = main.meta.castFunctionSelfToAnyopaque(Generator.generate);
-		self.hashFunction = main.meta.castFunctionSelfToAnyopaque(struct {
-			fn hash(ptr: *Generator) u64 {
-				return hashGeneric(ptr.*);
-			}
-		}.hash);
-		self.generationMode = Generator.generationMode;
-		modelRegistry.put(main.globalArena.allocator, Generator.id, self) catch unreachable;
-	}
-
-	fn getHash(self: SimpleStructureModel) u64 {
-		return self.vtable.hashFunction(self.data);
-	}
-};
+pub const SimpleStructureModel = terrain.structures.SimpleStructureModel;
 
 const Stripe = struct { // MARK: Stripe
 	direction: ?Vec3d,
@@ -139,7 +76,7 @@ const Stripe = struct { // MARK: Stripe
 	}
 };
 
-fn hashGeneric(input: anytype) u64 {
+pub fn hashGeneric(input: anytype) u64 {
 	const T = @TypeOf(input);
 	return switch (@typeInfo(T)) {
 		.bool => hashCombine(hashInt(@intFromBool(input)), 0xbf58476d1ce4e5b9),
