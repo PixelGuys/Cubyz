76a9a9be47d317ed5cc5734c12b03d905830c5b2
H41ogen <znelsen@proton.me>
2026-02-28 13:22:39 -0600

    /spawn now utilizes parseCoordinates

--- src/server/command/spawn.zig
+++ src/server/command/spawn.zig
@@ -13,42 +13,14 @@ pub const usage =
 
 pub fn execute(args: []const u8, source: *User) void {
 	var split = std.mem.splitScalar(u8, args, ' ');
-	//const pos = command.parseCoordinates(&split, source);
-
-	var x: ?f64 = null;
-	var y: ?f64 = null;
-	var z: ?f64 = null;
-	while (split.next()) |arg| {
-		if (arg.len == 0) break;
-		const num: f64 = std.fmt.parseFloat(f64, arg) catch {
-			source.sendMessage("#ff0000Expected number, found \"{s}\"", .{arg});
-			return;
-		};
-		if (x == null) {
-			x = num;
-		} else if (y == null) {
-			y = num;
-		} else if (z == null) {
-			z = num;
-		} else {
+	if (!std.mem.eql(u8, split.peek().?, "")) {
+		const pos = command.parseCoordinates(&split, source) catch return;
+		if (split.next()) |_| {
 			source.sendMessage("#ff0000Too many arguments for command /spawn", .{});
 			return;
 		}
-	}
-	if (x == null) {
+		source.spawnPos = pos;
+	} else {
 		source.sendMessage("#ffff00{}", .{source.spawnPos});
-		return;
 	}
-	if (y == null) {
-		source.sendMessage("#ff0000Invalid number of arguments for /spawn.\nUsage: \n" ++ usage, .{});
-		return;
-	}
-	if (z == null) {
-		z = source.player.pos[2];
-	}
-	x = std.math.clamp(x.?, -1e9, 1e9); // TODO: Remove after #310 is implemented
-	y = std.math.clamp(y.?, -1e9, 1e9);
-	z = std.math.clamp(z.?, -1e9, 1e9);
-
-	source.spawnPos = .{x.?, y.?, z.?};
 }
