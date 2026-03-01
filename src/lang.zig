1921588b7260776bcbacd9f6f00217613af12473
H41ogen <znelsen@proton.me>
2026-02-28 20:47:54 -0600

    initial commit

---
+++ src/lang.zig
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+
+
+// lang.translateGame("ui", "buttonMultiplayer"): []u8 (everything fallbacks to "unknown" if not found for convenience)
+// lang.translateTag(.metal) : ?[]u8
+// lang.translateProperty(.dry): ?[]u8
+// lang.translateItem("cubyz:iron_ore"): ?[]u8
