24e4b6e52269a2a0bdc323b99915400c37545283
Wunka <56411012+Wunka@users.noreply.github.com>
2026-03-04 19:14:58 +0100

    parsePlayerById

--- src/server/server.zig
+++ src/server/server.zig
@@ -793,3 +793,12 @@ pub fn sendMessage(comptime fmt: []const u8, args: anytype) void {
 	defer main.stackAllocator.free(msg);
 	sendRawMessage(msg);
 }
+
+pub fn getUserById(id: u32) !*User {
+	const userList = getUserListAndIncreaseRefCount(main.stackAllocator);
+	defer freeUserListAndDecreaseRefCount(main.stackAllocator, userList);
+	for (userList) |user| {
+		if (user.id == id) return user;
+	}
+	return error.UserNotFound;
+}
