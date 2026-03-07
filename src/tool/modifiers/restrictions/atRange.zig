cf763cc9d9c548d3762782fc39c85869e3b1c3ff
Vinywar123 <bahnsenvincent@gmail.com>
2026-03-07 14:03:35 -0800

    adding atRange

---
+++ src/tool/modifiers/restrictions/atRange.zig
@@ -0,0 +1,54 @@
+const std = @import("std");
+
+const main = @import("main");
+const NeverFailingAllocator = main.heap.NeverFailingAllocator;
+const ModifierRestriction = main.items.ModifierRestriction;
+const Tool = main.items.Tool;
+const ZonElement = main.ZonElement;
+
+const Encased = struct {
+	tag: main.Tag,
+	amount: usize,
+	range: usize,
+};
+
+pub fn satisfied(self: *const Encased, tool: *const Tool, x: i32, y: i32) bool {
+	var count: usize = 0;
+	const lowBound = 0;
+	const highBound = self.range*2 + 1;
+	for (lowBound..highBound) |dx| {
+		const checkedX = x + @as(i32, @intCast(dx - self.range));
+		const checkedY = y + @as(i32, @intCast(2));
+		if ((tool.getItemAt(checkedX, checkedY) orelse continue).hasTag(self.tag)) count += 1;
+	}
+	for (lowBound..highBound) |dx| {
+		const checkedX = x + @as(i32, @intCast(dx - self.range));
+		const checkedY = y + @as(i32, @intCast(-2));
+		if ((tool.getItemAt(checkedX, checkedY) orelse continue).hasTag(self.tag)) count += 1;
+	}
+	for ((lowBound + 1)..(highBound - 1)) |dy| {
+		const checkedX = x + @as(i32, @intCast(-2));
+		const checkedY = y + @as(i32, @intCast(dy - self.range));
+		if ((tool.getItemAt(checkedX, checkedY) orelse continue).hasTag(self.tag)) count += 1;
+	}
+	for ((lowBound + 1)..(highBound - 1)) |dy| {
+		const checkedX = x + @as(i32, @intCast(2));
+		const checkedY = y + @as(i32, @intCast(dy - self.range));
+		if ((tool.getItemAt(checkedX, checkedY) orelse continue).hasTag(self.tag)) count += 1;
+	}
+	return count >= self.amount;
+}
+
+pub fn loadFromZon(allocator: NeverFailingAllocator, zon: ZonElement) *const Encased {
+	const result = allocator.create(Encased);
+	result.* = .{
+		.tag = main.Tag.find(zon.get([]const u8, "tag", "not specified")),
+		.amount = zon.get(usize, "amount", 8),
+		.range = zon.get(usize, "range", 2),
+	};
+	return result;
+}
+
+pub fn printTooltip(self: *const Encased, outString: *main.List(u8)) void {
+	outString.print("{} .{s} {s} {}", .{self.amount, self.tag.getName(), "is at range", self.range});
+}
