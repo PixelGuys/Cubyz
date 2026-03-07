4b4fed17469bff5faf04e5ff10e025d1b8a8e586
IntegratedQuantum <jahe788@gmail.com>
2026-03-07 08:54:16 +0100

    Allow cutting hemispheres into arbitrary directions

--- src/server/terrain/sdf_models/hemisphere.zig
+++ src/server/terrain/sdf_models/hemisphere.zig
@@ -12,11 +12,17 @@ pub const id = "cubyz:hemisphere";
 
 minRadius: f32,
 maxRadius: f32,
+cutPercentage: f32,
+cutDirection: Vec3f,
+cutDirectionRandomness: f32,
 
 pub fn init(zon: ZonElement) ?*@This() {
 	const result = main.worldArena.create(@This());
 	result.minRadius = zon.get(f32, "minRadius", 16);
 	result.maxRadius = zon.get(f32, "maxRadius", result.minRadius);
+	result.cutPercentage = zon.get(f32, "cutPercentage", 0.5);
+	result.cutDirection = vec.normalize(zon.get(Vec3f, "cutDirection", .{0, 0, -1}));
+	result.cutDirectionRandomness = zon.get(f32, "cutDirectionRandomness", 0);
 	return result;
 }
 
@@ -24,10 +30,11 @@ pub fn generate(self: *@This(), output: main.utils.Array3D(f32), interpolationSm
 	var seed = _seed;
 	const radius = self.minRadius + (self.maxRadius - self.minRadius)*main.random.nextFloat(&seed);
 
-	const relPosF32: Vec3f = @floatFromInt(relPos);
+	var relPosF32: Vec3f = @floatFromInt(relPos);
+	const cutDirection = vec.normalize(self.cutDirection + main.random.nextFloatVectorSigned(3, &seed)*@as(Vec3f, @splat(self.cutDirectionRandomness)));
+	relPosF32 += cutDirection*@as(Vec3f, @splat(self.cutPercentage/2));
 	const dimVector: Vec3f = @floatFromInt(@Vector(3, u32){output.width*voxelSize, output.depth*voxelSize, output.height*voxelSize});
-	var min = @max(@as(Vec3f, @splat(0)), relPosF32 - @as(Vec3f, @splat(radius + perimeter)));
-	min[2] = @max(min[2], relPosF32[2] - perimeter);
+	const min = @max(@as(Vec3f, @splat(0)), relPosF32 - @as(Vec3f, @splat(radius + perimeter)));
 	const max = @min(dimVector, relPosF32 + @as(Vec3f, @splat(radius + perimeter)));
 
 	const minInt: @Vector(3, u31) = @intFromFloat(min);
@@ -39,11 +46,15 @@ pub fn generate(self: *@This(), output: main.utils.Array3D(f32), interpolationSm
 		while (y < maxInt[1]) : (y += voxelSize) {
 			var z = minInt[2] & ~(voxelSize - 1);
 			while (z < maxInt[2]) : (z += voxelSize) {
-				const distanceSquare: f32 = @floatFromInt((x - relPos[0])*(x - relPos[0]) + (y - relPos[1])*(y - relPos[1]) + (z - relPos[2])*(z - relPos[2]));
+				const centerDistance = @as(Vec3f, @floatFromInt(Vec3i{x, y, z})) - relPosF32;
+				const distanceSquare: f32 = vec.lengthSquare(centerDistance);
 				if (distanceSquare > (radius + perimeter)*(radius + perimeter)) continue;
 
+				const directionDistance = vec.dot(cutDirection, centerDistance) - radius + self.cutPercentage*radius*2;
+				if (directionDistance > perimeter) continue;
+
 				const sphereSdf = @sqrt(distanceSquare) - radius;
-				const fullSdf: f32 = sdf.intersection(sphereSdf, @as(f32, @floatFromInt(relPos[2] - z)));
+				const fullSdf: f32 = sdf.intersection(sphereSdf, directionDistance);
 
 				const out = output.ptr(x >> voxelSizeShift, y >> voxelSizeShift, z >> voxelSizeShift);
 				out.* = sdf.smoothUnion(fullSdf, out.*, interpolationSmoothness.get(x >> voxelSizeShift, y >> voxelSizeShift, z >> voxelSizeShift));
