4bf0e913a348da70d72cf95d3eac39115550bdf5
Vinywar123 <bahnsenvincent@gmail.com>
2026-03-14 23:13:26 -0700

    renaming and default checking whole axis

--- src/tool/modifiers/restrictions/onDiagonal.zig
+++ src/tool/modifiers/restrictions/onDiagonal.zig
@@ -6,45 +6,52 @@ const ModifierRestriction = main.items.ModifierRestriction;
 const Tool = main.items.Tool;
 const ZonElement = main.ZonElement;
 
-const Encased = struct {
-	tag: main.Tag,
-	amount: usize,
-	range: usize,
+const OnDiagonal = struct {
+    tag: main.Tag,
+    amount: usize,
+    range: usize,
 };
 
-pub fn satisfied(self: *const Encased, tool: *const Tool, x: i32, y: i32) bool {
-	var count: usize = 0;
-	const lowBound = 0;
-	const highBound = self.range*2 + 1;
-	for (lowBound..highBound) |dx| {
-		const checkedX = x + @as(i32, @intCast(dx - self.range));
-		const checkedY = y + @as(i32, @intCast(dx - self.range));
-		if ((tool.getItemAt(checkedX, checkedY) orelse continue).hasTag(self.tag)) count += 1;
-	}
-	for (lowBound..highBound) |dx| {
-		const checkedX = x + @as(i32, @intCast(dx - self.range));
-		const checkedY = y - @as(i32, (@intCast(dx - self.range)));
-		if (!(dx == 0)) {
-			if ((tool.getItemAt(checkedX, checkedY) orelse continue).hasTag(self.tag)) count += 1;
-		}
-	}
-	return count >= self.amount;
+pub fn satisfied(self: *const OnDiagonal, tool: *const Tool, x: i32, y: i32) bool {
+    var count: usize = 0;
+    const gridSize: usize = @sqrt(tool.craftingGrid.len);
+    var rangeChecked: usize = -1;
+    if (self.range > gridSize) {
+        rangeChecked = gridSize;
+    } else {
+        rangeChecked = self.range;
+    }
+    const lowBound = 0;
+    const highBound = rangeChecked * 2 + 1;
+    for (lowBound..highBound) |dx| {
+        const checkedX = x + @as(i32, @intCast(dx - rangeChecked));
+        const checkedY = y + @as(i32, @intCast(dx - rangeChecked));
+        if ((tool.getItemAt(checkedX, checkedY) orelse continue).hasTag(self.tag)) count += 1;
+    }
+    for (lowBound..highBound) |dx| {
+        const checkedX = x + @as(i32, @intCast(dx - rangeChecked));
+        const checkedY = y - @as(i32, (@intCast(dx - rangeChecked)));
+        if (!(dx == 0)) {
+            if ((tool.getItemAt(checkedX, checkedY) orelse continue).hasTag(self.tag)) count += 1;
+        }
+    }
+    return count >= self.amount;
 }
 
-pub fn loadFromZon(allocator: NeverFailingAllocator, zon: ZonElement) *const Encased {
-	const result = allocator.create(Encased);
-	result.* = .{
-		.tag = main.Tag.find(zon.get([]const u8, "tag", "not specified")),
-		.amount = zon.get(usize, "amount", 8),
-		.range = zon.get(usize, "range", 5),
-	};
-	return result;
+pub fn loadFromZon(allocator: NeverFailingAllocator, zon: ZonElement) *const OnDiagonal {
+    const result = allocator.create(OnDiagonal);
+    result.* = .{
+        .tag = main.Tag.find(zon.get([]const u8, "tag", "not specified")),
+        .amount = zon.get(usize, "amount", 8),
+        .range = zon.get(usize, "range", 100),
+    };
+    return result;
 }
 
-pub fn printTooltip(self: *const Encased, outString: *main.List(u8)) void {
-	if (self.range < 5) {
-		outString.print("{} .{s} {s} {}", .{self.amount, self.tag.getName(), "in diagonal range", self.range});
-	} else {
-		outString.print("{} .{s} {s}", .{self.amount, self.tag.getName(), "on diagonal axis"});
-	}
+pub fn printTooltip(self: *const OnDiagonal, outString: *main.List(u8)) void {
+    if (self.range == 5) {
+        outString.print("{} .{s} {s}", .{ self.amount, self.tag.getName(), "on diagonal axis" });
+    } else {
+        outString.print("{} .{s} {s} {}", .{ self.amount, self.tag.getName(), "in diagonal range", self.range });
+    }
 }
