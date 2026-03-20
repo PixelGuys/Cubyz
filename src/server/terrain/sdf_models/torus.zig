7031b9ce6bad0aa839de365a18ab99c50c13b340
Vinywar123 <bahnsenvincent@gmail.com>
2026-03-19 17:27:51 -0700

    Update with requested changes

--- src/server/terrain/sdf_models/torus.zig
+++ src/server/terrain/sdf_models/torus.zig
@@ -31,32 +31,28 @@ pub fn generate(self: *@This(), output: main.utils.Array3D(f32), interpolationSm
 
 	const relPosF32: Vec3f = @floatFromInt(relPos);
 	const dimVector: Vec3f = @floatFromInt(@Vector(3, u32){output.width*voxelSize, output.depth*voxelSize, output.height*voxelSize});
-	const minXY = @max(@as(Vec3f, @splat(0)), relPosF32 - @as(Vec3f, @splat(radius + thickness + perimeter)));
-	const maxXY = @min(dimVector, relPosF32 + @as(Vec3f, @splat(radius + thickness + perimeter)));
-	const minZ = @max(@as(Vec3f, @splat(0)), relPosF32 - @as(Vec3f, @splat(thickness + perimeter)));
-	const maxZ = @min(dimVector, relPosF32 + @as(Vec3f, @splat(thickness + perimeter)));
+	const min = @max(@as(Vec3f, @splat(0)), relPosF32 - Vec3f{radius + thickness + perimeter, radius + thickness + perimeter, thickness + perimeter});
+	const max = @min(dimVector, relPosF32 + Vec3f{radius + thickness + perimeter, radius + thickness + perimeter, thickness + perimeter});
 
-	const minIntXY: @Vector(3, u31) = @intFromFloat(minXY);
-	const maxIntXY: Vec3i = @intFromFloat(@ceil(maxXY));
-	const minIntZ: @Vector(3, u31) = @intFromFloat(minZ);
-	const maxIntZ: Vec3i = @intFromFloat(@ceil(maxZ));
+	const minInt: @Vector(3, u31) = @intFromFloat(min);
+	const maxInt: Vec3i = @intFromFloat(@ceil(max));
 
-	var x = minIntXY[0] & ~(voxelSize - 1);
-	while (x < maxIntXY[0]) : (x += voxelSize) {
-		var y = minIntXY[1] & ~(voxelSize - 1);
-		while (y < maxIntXY[1]) : (y += voxelSize) {
-			var z = minIntZ[2] & ~(voxelSize - 1);
-			while (z < maxIntZ[2]) : (z += voxelSize) {
+	var x = minInt[0] & ~(voxelSize - 1);
+	while (x < maxInt[0]) : (x += voxelSize) {
+		var y = minInt[1] & ~(voxelSize - 1);
+		while (y < maxInt[1]) : (y += voxelSize) {
+			var z = minInt[2] & ~(voxelSize - 1);
+			while (z < maxInt[2]) : (z += voxelSize) {
 				const xDifference: f32 = @floatFromInt(x - relPos[0]);
 				const yDifference: f32 = @floatFromInt(y - relPos[1]);
 				const zDifference: f32 = @floatFromInt(z - relPos[2]);
-				const circularDistance: f32 = @sqrt(xDifference*xDifference + yDifference*yDifference);
-				const adjustedDistance: f32 = circularDistance - radius;
-				const radialDistance: f32 = @sqrt(adjustedDistance*adjustedDistance + zDifference*zDifference) - thickness;
-				if (radialDistance - perimeter > 0) continue;
+				const radialDistance: f32 = @sqrt(xDifference*xDifference + yDifference*yDifference);
+				const adjustedDistance: f32 = radialDistance - radius;
+				const toroidalDistance: f32 = @sqrt(adjustedDistance*adjustedDistance + zDifference*zDifference) - thickness;
+				if (toroidalDistance - perimeter > 0) continue;
 
 				const out = output.ptr(x >> voxelSizeShift, y >> voxelSizeShift, z >> voxelSizeShift);
-				out.* = sdf.smoothUnion(radialDistance, out.*, interpolationSmoothness.get(x >> voxelSizeShift, y >> voxelSizeShift, z >> voxelSizeShift));
+				out.* = sdf.smoothUnion(toroidalDistance, out.*, interpolationSmoothness.get(x >> voxelSizeShift, y >> voxelSizeShift, z >> voxelSizeShift));
 			}
 		}
 	}
