e254a8c11bffbe23c93e534c1bac7f75326c5ea8
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2026-03-31 23:55:18 +0200

    Reformat

--- src/callbacks/block/server/vine_decay.zig
+++ src/callbacks/block/server/vine_decay.zig
@@ -14,23 +14,23 @@ pub fn run(_: *@This(), params: main.callbacks.ServerBlockCallback.Params) main.
 	const wy = params.chunk.super.pos.wy + params.blockPos.y;
 	const wz = params.chunk.super.pos.wz + params.blockPos.z;
 
-	if(params.block.mode() != main.rotation.getByID("cubyz:hanging")) {
+	if (params.block.mode() != main.rotation.getByID("cubyz:hanging")) {
 		std.log.err("Expected {s} to have cubyz:hanging as rotation", .{params.block.id()});
 	}
 
 	const world = server.world orelse return .ignored;
 	const thisBlock = world.getBlock(wx, wy, wz) orelse return .ignored;
 
-	if(params.block != thisBlock) return .ignored;
+	if (params.block != thisBlock) return .ignored;
 
 	const blockAbove = world.getBlock(wx, wy, wz +% 1) orelse return .ignored;
-	if(blockAbove.typ == params.block.typ) return .ignored;
-	if(!blockAbove.replacable()) return .ignored;
+	if (blockAbove.typ == params.block.typ) return .ignored;
+	if (!blockAbove.replacable()) return .ignored;
 
 	const blockAboveModel = blocks.meshes.model(blockAbove).model();
-	if(blockAboveModel.isNeighborOccluded[main.chunk.Neighbor.dirDown.toInt()]) return .ignored;
+	if (blockAboveModel.isNeighborOccluded[main.chunk.Neighbor.dirDown.toInt()]) return .ignored;
 
-	if(world.cmpxchgBlock(wx, wy, wz, thisBlock, blocks.Block.air) == null) {
+	if (world.cmpxchgBlock(wx, wy, wz, thisBlock, blocks.Block.air) == null) {
 		return .handled;
 	}
 
