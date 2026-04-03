48bdea66cf8f52aa951d02a0254dc7a8c20472aa
Wunka <56411012+Wunka@users.noreply.github.com>
2026-04-03 18:24:58 +0200

    add target to spawn command

--- src/server/command/spawn.zig
+++ src/server/command/spawn.zig
@@ -13,14 +13,16 @@ pub const usage =
 
 pub fn execute(args: []const u8, source: *User) void {
 	var split = std.mem.splitScalar(u8, args, ' ');
+	const target = command.Target.init(&split, source) catch return;
+	defer target.deinit();
 	if (split.peek().?.len > 0) {
 		const pos = command.parseCoordinates(&split, source) catch return;
 		if (split.next()) |_| {
 			source.sendMessage("#ff0000Too many arguments for command /spawn", .{});
 			return;
 		}
-		source.spawnPos = pos;
+		target.user.spawnPos = pos;
 	} else {
-		source.sendMessage("#ffff00{}", .{source.spawnPos});
+		source.sendMessage("#ffff00{}", .{target.user.spawnPos});
 	}
 }
