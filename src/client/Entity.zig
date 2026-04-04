fce736cb9ee09e79b978a7c0c385b61ab1a2da39
RanPix <darkorden16@gmail.com>
2026-04-04 21:42:07 +0200

    improve test model

--- src/client/Entity.zig
+++ src/client/Entity.zig
@@ -81,6 +81,7 @@ pub fn updatePosition(self: *@This(), pos: *const [6]f64, vel: *const [6]f64, ti
 	self.interpolatedValues.updatePosition(pos, vel, time);
 }
 
+var ueee: f32 = 0;
 pub fn update(self: *@This(), time: i16, lastTime: i16) void {
 	self.interpolatedValues.update(time, lastTime);
 	self.pos[0] = self.interpolatedValues.outPos[0];
@@ -99,6 +100,14 @@ pub fn update(self: *@This(), time: i16, lastTime: i16) void {
 
 	self.nodes[6].rot = vec.quatFromAxisAngle(Vec3f{1, 0, 0}, self.rot[0]);
 	self.matrices[6] = getHierarchyMatrix(self.nodes, self.nodes[6]);
+
+	ueee += 0.05;
+
+	self.nodes[2].rot = vec.quatFromAxisAngle(Vec3f{1, 0, 0}, ueee/5);
+	self.matrices[2] = getHierarchyMatrix(self.nodes, self.nodes[2]);
+
+	self.nodes[3].rot = vec.quatFromAxisAngle(Vec3f{1, 0, 0}, ueee/3);
+	self.matrices[3] = getHierarchyMatrix(self.nodes, self.nodes[3]);
 }
 
 fn getHierarchyMatrix(nodes: [20]EntityModel.Node, node: EntityModel.Node) Mat4f {
