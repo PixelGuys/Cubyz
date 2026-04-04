e9d3070af03bdf545f4320ede1a808cdeb395705
RanPix <darkorden16@gmail.com>
2026-04-04 19:44:43 +0200

    remove bobik

--- src/client/entity_manager.zig
+++ src/client/entity_manager.zig
@@ -48,15 +48,6 @@ pub fn init() void {
 		std.log.err("Gltf loading error {s}", .{@errorName(err)});
 		break :blk .initEmpty();
 	};
-
-	// TODO: remove before merge
-	addEntity(ZonElement.parseFromString(main.globalArena, null,
-		\\ .{
-		\\    .id = 1,
-		\\    .name = "bobik",
-		\\
-		\\  }
-	));
 }
 
 pub fn deinit() void {
