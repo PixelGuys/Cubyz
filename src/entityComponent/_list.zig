3f9707de9a266b558cbd955859ede74d6ebb88d1
till++ <tillplusplus@gmail.com>
2026-04-04 15:50:59 +0200

    renamed stuff

--- src/entityComponent/_list.zig
+++ src/entityComponent/_list.zig
@@ -1,3 +1,3 @@
+pub const _template = @import("_template.zig");
 pub const model = @import("model.zig");
 pub const itemDrop = @import("itemDrop.zig");
-pub const template = @import("template.zig");
