6c1e0ab4a3491e01053afc48276ad6435cdb8acc
Wunka <56411012+Wunka@users.noreply.github.com>
2026-04-08 21:15:05 +0200

    make playerIndex optinal and rename manage_players

--- src/client/Entity.zig
+++ src/client/Entity.zig
@@ -30,7 +30,7 @@ rot: Vec3f = undefined,
 
 id: u32,
 name: []const u8,
-playerIndex: usize, // TODO extract into own component #2760
+playerIndex: ?usize, // TODO extract into own component #2760
 
 pub fn init(self: *@This(), zon: ZonElement, allocator: NeverFailingAllocator) void {
 	self.* = @This(){
@@ -38,7 +38,7 @@ pub fn init(self: *@This(), zon: ZonElement, allocator: NeverFailingAllocator) v
 		.width = zon.get(f64, "width", 1),
 		.height = zon.get(f64, "height", 1),
 		.name = allocator.dupe(u8, zon.get([]const u8, "name", "")),
-		.playerIndex = zon.get(usize, "playerIndex", std.math.maxInt(usize)),
+		.playerIndex = zon.get(?usize, "playerIndex", null),
 	};
 	self._interpolationPos = [_]f64{
 		self.pos[0],
@@ -75,9 +75,13 @@ pub fn update(self: *@This(), time: i16, lastTime: i16) void {
 }
 
 pub fn format(self: *const @This(), writer: *std.Io.Writer) std.Io.Writer.Error!void {
-	if (main.settings.showIdWithName) {
-		try writer.print("{s}@{d}", .{self.name, self.playerIndex});
+	if (main.settings.showIdWithName and self.playerIndex != null) {
+		try self.printWithIndex(writer);
 	} else {
 		try writer.print("{s}", .{self.name});
 	}
+}
+
+pub fn printWithIndex(self: *const @This(), writer: *std.Io.Writer) std.Io.Writer.Error!void {
+	try writer.print("{s}@{d}", .{self.name, self.playerIndex});
 }
