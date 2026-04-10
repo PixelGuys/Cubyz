64c487e5728574ff80e84af45b5517d24f710939
GasSpace630 <gasspace630@gmail.com>
2026-04-10 09:52:45 +0530

    Add support for /spawn world and world spawn display

--- src/server/command/spawn.zig
+++ src/server/command/spawn.zig
@@ -9,13 +9,37 @@ pub const description = "Get or set the player spawn point";
 pub const usage =
 	\\/spawn
 	\\/spawn <x> <y> <z>
+	\\/spawn world
+	\\/spawn world <x> <y> <z>
 ;
 
 pub fn execute(args: []const u8, source: *User) void {
 	var split = std.mem.splitScalar(u8, args, ' ');
 	if (split.peek().?.len > 0) {
-		const pos = command.parseCoordinates(&split, source) catch return;
-		if (split.next()) |_| {
+		const first = split.next().?;
+		if (std.mem.eql(u8, first, "world")) {
+			if (split.peek() == null or split.peek().?.len == 0) {
+				const world = main.server.world.?;
+				source.sendMessage("#ffff00World spawn: {}", .{world.spawn});
+				return;
+			}
+			const pos = command.parseCoordinates(&split, source) catch return;
+			if (split.next()) |_| {
+				source.sendMessage("#ff0000Too many arguments for command /spawn", .{});
+				return;
+			}
+			const world = main.server.world.?;
+			world.spawn = .{
+				@intFromFloat(pos[0]),
+				@intFromFloat(pos[1]),
+				@intFromFloat(pos[2]),
+			};
+			return;
+		}
+
+		var newSplit = std.mem.splitScalar(u8, args, ' ');
+		const pos = command.parseCoordinates(&newSplit, source) catch return;
+		if (newSplit.next()) |_| {
 			source.sendMessage("#ff0000Too many arguments for command /spawn", .{});
 			return;
 		}
