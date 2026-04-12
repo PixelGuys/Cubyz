8fef53538786d729108f5db06260a827a3e5ce59
till++ <tillplusplus@gmail.com>
2026-04-12 14:58:40 +0200

    nextIntBounded

--- src/server/server.zig
+++ src/server/server.zig
@@ -250,7 +250,8 @@ pub const User = struct { // MARK: User
 		freeId += 1;
 
 		if (world.?.playerEntityModels.items.len != 0) {
-			const defaultModel = world.?.playerEntityModels.items[main.random.nextInt(u32, &main.seed)%world.?.playerEntityModels.items.len];
+			
+			const defaultModel = world.?.playerEntityModels.items[main.random.nextIntBounded(u32, &main.seed, world.?.playerEntityModels.items.len)];
 			main.entity.components.@"cubyz:model".server.loadByID(self.id, defaultModel) catch unreachable;
 		}
 		world.?.loadPlayer(self) catch {
