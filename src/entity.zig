cf24fd7161c95d7dbe3b604176d6e7bb66a8070b
till++ <tillplusplus@gmail.com>
2026-04-12 06:46:23 +0200

    merhe

--- src/entity.zig
+++ src/entity.zig
@@ -27,8 +27,8 @@ pub const Entity = enum(u32) {
 	noValue = std.math.maxInt(u32),
 	_,
 };
-// Analogous to Protocols.
 pub const EntityComponentId = u32;
+// Analogous to Protocols.
 const EntityComponentVTable = struct {
 	serverLoad: *const fn (entityId: u32, reader: *main.utils.BinaryReader, version: u32) EntityComponentLoadError!void,
 	clientLoad: *const fn (entityId: u32, reader: *main.utils.BinaryReader, version: u32) EntityComponentLoadError!void,
