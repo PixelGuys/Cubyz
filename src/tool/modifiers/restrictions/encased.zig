b589237ed2c85c62dce85be4ee9e8a8fd7890131
Crepestrom <bahnsenvincent@gmail.com>
2026-04-12 16:23:56 -0700

    Apply suggestion from @IntegratedQuantum
    Co-authored-by: IntegratedQuantum <43880493+IntegratedQuantum@users.noreply.github.com>

--- src/tool/modifiers/restrictions/encased.zig
+++ src/tool/modifiers/restrictions/encased.zig
@@ -39,8 +39,7 @@ pub fn loadFromZon(allocator: NeverFailingAllocator, zon: ZonElement) *const Enc
 pub fn printTooltip(self: *const Encased, outString: *main.List(u8)) void {
 	if (self.range == 1) {
 		outString.print("encased in {} .{s}", .{self.amount, self.tag.getName()});
-	}
-	if (!(self.range == 1)) {
+	} else {
 		outString.print("encased in {} .{s} {s} {}", .{self.amount, self.tag.getName(), "in range", self.range});
 	}
 }
