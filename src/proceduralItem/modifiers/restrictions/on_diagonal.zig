280539fbb8f40bf5f4af20f7e450a39d6219b8fd
Vinywar123 <bahnsenvincent@gmail.com>
2026-04-19 23:17:04 -0700

    Update on_diagonal.zig

--- src/proceduralItem/modifiers/restrictions/on_diagonal.zig
+++ src/proceduralItem/modifiers/restrictions/on_diagonal.zig
@@ -14,8 +14,8 @@ const On_diagonal = struct {
 
 pub fn satisfied(self: *const On_diagonal, proceduralItem: *const ProceduralItem, x: i32, y: i32) bool {
 	var count: usize = 0;
-	const gridSize: usize = proceduralItem.materialGrid.len;
-	const rangeChecked = @min(self.range orelse (gridSize - 1), (gridSize - 1));
+	const gridSize: usize = proceduralItem.materialGrid.len - 1;
+	const rangeChecked = @min(self.range orelse gridSize, gridSize);
 	const lowBound = 0;
 	const highBound = rangeChecked*2 + 1;
 	for (lowBound..highBound) |i| {
@@ -26,7 +26,7 @@ pub fn satisfied(self: *const On_diagonal, proceduralItem: *const ProceduralItem
 	for (lowBound..highBound) |i| {
 		const checkedX = x + (@as(i32, @intCast(i)) - rangeChecked);
 		const checkedY = y - (@as(i32, @intCast(i)) - rangeChecked);
-		if (i != 0) {
+		if (i != 0) { // prevents double counting on items
 			if ((proceduralItem.getItemAt(checkedX, checkedY) orelse continue).hasTag(self.tag)) count += 1;
 		}
 	}
