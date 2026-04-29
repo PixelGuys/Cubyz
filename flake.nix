919598a940e103d1cd1defb8faa9cb8ea607969a
tenyoru <mail@tenyoru.io>
2026-04-29 14:55:01 +0200

    Add zig-overlay and zls inputs

--- flake.nix
+++ flake.nix
@@ -4,12 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    zig_overlay.url = "github:mitchellh/zig-overlay";
+    zls.url = "github:zigtools/zls";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, zig_overlay, zls, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        zig_version = "0.16.0";
+        zig = zig_overlay.packages.${system}.${zig_version};
 
         # Libraries GLFW dynamically loads (dlopen) at runtime on Linux.
         runtimeLibs = with pkgs; [
@@ -24,19 +29,20 @@
         ];
 
         libPath = pkgs.lib.makeLibraryPath runtimeLibs;
+        ldLibraryPath = "${libPath}\${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}";
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ zig ] ++ runtimeLibs;
+          packages = [ zig zls.packages.${system}.default ] ++ runtimeLibs;
 
           shellHook = ''
-            export LD_LIBRARY_PATH="${libPath}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+            export LD_LIBRARY_PATH="${ldLibraryPath}"
           '';
         };
 
-        # Run after `zig build` from the project root: `nix run .#`
+        # Run the built project
         packages.default = pkgs.writeShellScriptBin "cubyz" ''
-          export LD_LIBRARY_PATH="${libPath}''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+          export LD_LIBRARY_PATH="${ldLibraryPath}"
           exec "$PWD/zig-out/bin/Cubyz" "$@"
         '';
       }
