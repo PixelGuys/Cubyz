5a00aaaf9d6ed0d3c1057f31a08f2b3b1090dbcb
Wunka <56411012+Wunka@users.noreply.github.com>
2026-05-22 14:28:00 +0200

    add playerIndex option

--- src/server/command/gamemode.zig
+++ src/server/command/gamemode.zig
@@ -1,13 +1,17 @@
 const std = @import("std");
 
 const main = @import("main");
+const command = main.server.command;
 const User = main.server.User;
 
 pub const description = "Get or set your gamemode.";
-pub const usage = "/gamemode\n/gamemode <survival/creative>";
+pub const usage =
+	\\/gamemode <survival/creative>
+	\\/gamemode @playerIndex <survival/creative>
+;
 
 const Args = union(enum) {
-	@"/gamemode <mode>": struct { mode: main.game.Gamemode },
+	@"/gamemode <playerIndex> <mode>": struct { playerIndex: ?command.PlayerIndex, mode: main.game.Gamemode },
 };
 
 const ArgParser = main.argparse.Parser(Args, .{.commandName = "/gamemode"});
@@ -20,5 +24,8 @@ pub fn execute(args: []const u8, source: *User) void {
 		source.sendMessage("#ff0000{s}", .{errorMessage.items});
 		return;
 	};
-	main.sync.setGamemode(source, result.@"/gamemode <mode>".mode);
+	const target = command.Target.fromPlayerIndex(result.@"/gamemode <playerIndex> <mode>".playerIndex, source) catch return;
+	defer target.deinit();
+
+	main.sync.setGamemode(target.user, result.@"/gamemode <playerIndex> <mode>".mode);
 }
