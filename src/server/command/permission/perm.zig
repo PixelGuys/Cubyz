97004d8b42a6906e2c0c96ff4ae102dfde33c61d
Wunka <56411012+Wunka@users.noreply.github.com>
2026-05-22 17:28:46 +0200

    finish

--- src/server/command/permission/perm.zig
+++ src/server/command/permission/perm.zig
@@ -17,8 +17,8 @@ pub const usage =
 ;
 
 const Args = union(enum) {
-	@"/perm <add> <list> <playerIndex> <permissionPath>": struct {
-		add: enum { add },
+	@"/perm <action> <list> <playerIndex> <permissionPath>": struct {
+		action: enum { add, remove },
 		list: enum { whitelist, blacklist },
 		playerIndex: ?command.PlayerIndex,
 		permissionPath: Path,
@@ -36,36 +36,36 @@ pub fn execute(args: []const u8, source: *User) void {
 		source.sendMessage("#ff0000{s}", .{errorMessage.items});
 		return;
 	};
-	if (split.next()) |arg| {
-		if (std.ascii.eqlIgnoreCase(arg, "remove")) {
-			const helper = Helper.init(source, &split) catch return;
-			defer helper.deinit();
-			if (!helper.target.user.permissions.removePermission(helper.listType, helper.permissionPath)) {
-				source.sendMessage("#ff0000Permission path {s} is not present inside users permission {s}list", .{helper.permissionPath, @tagName(helper.listType)});
-			}
-		} else if (std.ascii.eqlIgnoreCase(arg, "add")) {
-			const helper = Helper.init(source, &split) catch return;
-			defer helper.deinit();
-			helper.target.user.permissions.addPermission(helper.listType, helper.permissionPath);
-		} else {
-			split.reset();
-			const target = command.Target.init(&split, source) catch return;
+
+	switch (result) {
+		.@"/perm <action> <list> <playerIndex> <permissionPath>" => |params| {
+			const target = command.Target.fromPlayerIndex(params.playerIndex, source) catch return;
 			defer target.deinit();
 
-			const permissionPath = split.next() orelse {
-				source.sendMessage("#ff0000Too few arguments for /perm", .{});
-				return;
+			const listType: ListType = switch (params.list) {
+				.whitelist => .white,
+				.blacklist => .black,
 			};
-			if (permissionPath[0] != '/') {
-				source.sendMessage("#ff0000Permission paths always begin with a \"/\", got: {s}", .{permissionPath});
-				return;
+
+			switch (params.action) {
+				.add => target.user.permissions.addPermission(listType, params.permissionPath.path),
+				.remove => {
+					if (!target.user.permissions.removePermission(listType, params.permissionPath.path)) {
+						source.sendMessage("#ff0000Permission path {s} is not present inside users permission {s}list", .{params.permissionPath.path, @tagName(listType)});
+					}
+				},
 			}
-			if (target.user.hasPermission(permissionPath)) {
-				source.sendMessage("#00ff00Player {s}§#00ff00 has permission for path: {s}", .{target.user.name, permissionPath});
+		},
+		.@"/perm <playerIndex> <permissionPath>" => |params| {
+			const target = command.Target.fromPlayerIndex(params.playerIndex, source) catch return;
+			defer target.deinit();
+
+			if (target.user.hasPermission(params.permissionPath.path)) {
+				source.sendMessage("#00ff00Player {s}§#00ff00 has permission for path: {s}", .{target.user.name, params.permissionPath.path});
 			} else {
-				source.sendMessage("#ff0000Player {s}§#ff0000 has no permission for path: {s}", .{target.user.name, permissionPath});
+				source.sendMessage("#ff0000Player {s}§#ff0000 has no permission for path: {s}", .{target.user.name, params.permissionPath.path});
 			}
-		}
+		},
 	}
 }
 
@@ -78,51 +78,5 @@ const Path = struct {
 			return error.ParseError;
 		}
 		return .{.path = arg};
-	}
-};
-
-const Helper = struct {
-	listType: ListType,
-	permissionPath: []const u8,
-	target: command.Target,
-
-	pub fn init(source: *User, split: *std.mem.SplitIterator(u8, .scalar)) error{InvalidArgs}!Helper {
-		var listType: ListType = undefined;
-		const arg = split.next() orelse {
-			source.sendMessage("#ff0000Too few arguments for command /perm", .{});
-			return error.InvalidArgs;
-		};
-		if (std.ascii.eqlIgnoreCase(arg, "whitelist")) {
-			listType = .white;
-		} else if (std.ascii.eqlIgnoreCase(arg, "blacklist")) {
-			listType = .black;
-		} else {
-			source.sendMessage("#ff0000Expected either whitelist or blacklist, found \"{s}\"", .{arg});
-			return error.InvalidArgs;
-		}
-
-		const target = command.Target.init(split, source) catch return error.InvalidArgs;
-		errdefer target.deinit();
-
-		const permissionPath = split.next() orelse {
-			source.sendMessage("#ff0000Too few arguments for command /perm.", .{});
-			return error.InvalidArgs;
-		};
-
-		if (permissionPath[0] != '/') {
-			source.sendMessage("#ff0000Permission paths always begin with a \"/\", got: {s}", .{arg});
-			return error.InvalidArgs;
-		}
-
-		if (split.next() != null) {
-			source.sendMessage("#ff0000Too many arguments for command /perm", .{});
-			return error.InvalidArgs;
-		}
-
-		return .{.listType = listType, .permissionPath = permissionPath, .target = target};
-	}
-
-	pub fn deinit(self: Helper) void {
-		self.target.deinit();
 	}
 };
