01751259b3da2132188de9afca5ac3a3bc8a135c
Wunka <56411012+Wunka@users.noreply.github.com>
2026-05-22 14:17:15 +0200

    gamemode update

--- src/server/command/gamemode.zig
+++ src/server/command/gamemode.zig
@@ -6,17 +6,19 @@ const User = main.server.User;
 pub const description = "Get or set your gamemode.";
 pub const usage = "/gamemode\n/gamemode <survival/creative>";
 
+const Args = union(enum) {
+	@"/gamemode <mode>": struct { mode: main.game.Gamemode },
+};
+
+const ArgParser = main.argparse.Parser(Args, .{.commandName = "/gamemode"});
+
 pub fn execute(args: []const u8, source: *User) void {
-	if (args.len == 0) {
-		source.sendMessage("#ffff00{s}", .{@tagName(source.gamemode.load(.monotonic))});
+	var errorMessage: main.ListUnmanaged(u8) = .{};
+	defer errorMessage.deinit(main.stackAllocator);
+
+	const result = ArgParser.parse(main.stackAllocator, args, &errorMessage) catch {
+		source.sendMessage("#ff0000{s}", .{errorMessage.items});
 		return;
-	}
-	if (std.ascii.eqlIgnoreCase(args, "survival")) {
-		main.sync.setGamemode(source, .survival);
-	} else if (std.ascii.eqlIgnoreCase(args, "creative")) {
-		main.sync.setGamemode(source, .creative);
-	} else {
-		source.sendMessage("#ff0000Invalid argument for command /gamemode. Must be 'survival' or 'creative'.", .{});
-		return;
-	}
+	};
+	main.sync.setGamemode(source, result.@"/gamemode <mode>".mode);
 }
