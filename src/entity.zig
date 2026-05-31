d7e072bbc98297b41621ebeaa1becf0d59136157
till++ <tillplusplus@gmail.com>
2026-05-31 17:09:39 +0200

    forgot defer

--- src/entity.zig
+++ src/entity.zig
@@ -197,7 +197,7 @@ pub const server = struct {
 		defer binaryWriter.deinit();
 
 		const users = main.server.getUserListAndIncreaseRefCount(main.stackAllocator);
-		main.server.freeUserListAndDecreaseRefCount(main.stackAllocator, users);
+		defer main.server.freeUserListAndDecreaseRefCount(main.stackAllocator, users);
 
 		if (EntityComponent.server.get(entity)) |ptr| {
 			if (ptr.save(&binaryWriter, .playerNearby) == .save) {
