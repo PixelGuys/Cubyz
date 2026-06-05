10f33f81530c0bec4f91b5127dc9fd79c77e3e65
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2026-06-05 21:32:04 +0200

    Update `/deselect` command (#3160)
    Related-to: #3073

--- src/server/command/worldedit/deselect.zig
+++ src/server/command/worldedit/deselect.zig
@@ -6,11 +6,20 @@ const User = main.server.User;
 pub const description = "Clears pos1 and pos2 of selection.";
 pub const usage = "/deselect";
 
+const Args = union(enum) {
+	@"/deselect": struct {},
+};
+
+const ArgParser = main.argparse.Parser(Args, .{.commandName = "/deselect"});
+
 pub fn execute(args: []const u8, source: *User) void {
-	if (args.len != 0) {
-		source.sendMessage("#ff0000Too many arguments for command /deselect. Expected no arguments.", .{});
+	var errorMessage: main.List(u8) = .{};
+	defer errorMessage.deinit(main.stackAllocator);
+
+	_ = ArgParser.parse(main.stackAllocator, args, &errorMessage) catch {
+		source.sendMessage("#ff0000{s}", .{errorMessage.items});
 		return;
-	}
+	};
 
 	source.worldEditData.selectionPosition1 = null;
 	source.worldEditData.selectionPosition2 = null;
