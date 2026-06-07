9f80b26936b0705fe6e093ca5e3afe323491756f
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2026-06-07 20:55:49 +0200

    Update `/paste` command (#3164)
    Related-to: #3073

--- src/server/command/worldedit/paste.zig
+++ src/server/command/worldedit/paste.zig
@@ -5,28 +5,28 @@ const User = main.server.User;
 const vec = main.vec;
 const Vec3i = vec.Vec3i;
 
-const copy = @import("copy.zig");
-
-const Block = main.blocks.Block;
 const Blueprint = main.blueprint.Blueprint;
 
 pub const description =
 	\\Paste clipboard content to current player position.
-	\\'-v' - Enable preserving void blocks. By default, void blocks are not preserved.
+	\\-v|--keep-void - Preserve void blocks. By default, void blocks are not preserved.
 ;
-pub const usage = "/paste [-v]";
+pub const usage = "/paste [-v|--keep-void]";
+
+const Args = union(enum) {
+	@"/paste [-v|--keep-void]": struct { void: ?enum { @"-v", @"--keep-void" } },
+};
+
+const ArgParser = main.argparse.Parser(Args, .{.commandName = "/paste"});
 
 pub fn execute(args: []const u8, source: *User) void {
-	var flags = Blueprint.PasteFlags{};
+	var errorMessage: main.List(u8) = .{};
+	defer errorMessage.deinit(main.stackAllocator);
 
-	if (args.len != 0) {
-		if (std.mem.eql(u8, args, "-v")) {
-			flags.preserveVoid = true;
-		} else {
-			source.sendMessage("#ff0000Argument(s) '{s}' not recognized.", .{args});
-			return;
-		}
-	}
+	const result = ArgParser.parse(main.stackAllocator, args, &errorMessage) catch {
+		source.sendMessage("#ff0000{s}", .{errorMessage.items});
+		return;
+	};
 
 	if (source.worldEditData.clipboard) |clipboard| {
 		const pos: Vec3i = @floor(source.player().pos);
@@ -44,7 +44,7 @@ pub fn execute(args: []const u8, source: *User) void {
 			},
 		}
 
-		clipboard.paste(pos, flags);
+		clipboard.paste(pos, .{.preserveVoid = result.@"/paste [-v|--keep-void]".void != null});
 	} else {
 		source.sendMessage("#ff0000Error: No clipboard content to paste.", .{});
 	}
