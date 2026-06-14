882422fb79b3d6639173715b33eab15b14fdfbd8
till++ <tillplusplus@gmail.com>
2026-06-14 14:26:38 +0200

    formatter forcing me to write ugly code

--- src/reload.zig
+++ src/reload.zig
@@ -14,8 +14,7 @@ pub fn deinit() void {
 	main.globalAllocator.free(lastWorldName);
 }
 pub fn storeWorldName(worldName: []const u8) void {
-	if (std.mem.eql(u8, lastWorldName, worldName))
-		return;
+	if (std.mem.eql(u8, lastWorldName, worldName)) return;
 	main.globalAllocator.free(lastWorldName);
 	lastWorldName = main.globalAllocator.dupe(u8, worldName);
 }
