2a964307fc7c219e3e47fdb38a66258c53e71b03
Wunka <56411012+Wunka@users.noreply.github.com>
2026-06-14 07:04:46 +0200

    added Text exampple

--- src/gui/windows/inventory.zig
+++ src/gui/windows/inventory.zig
@@ -48,6 +48,7 @@ fn sayHello() void {
 pub fn onOpen() void {
 	window.titleBar = HorizontalList.init();
 	window.titleBar.?.add(Button.initIcon(.{0, 0}, .{15, 15}, craftingIcon, false, .{.onAction = .init(sayHello)}));
+	window.titleBar.?.add(GuiComponent.Label.init(.{padding, 0}, 150, "Inventory", .left));
 	const list = VerticalList.init(.{padding, padding + 16}, 300, 0);
 	// Some miscellanious slots and buttons:
 	// TODO: armor slots, backpack slot + stack-based backpack inventory, other items maybe?
