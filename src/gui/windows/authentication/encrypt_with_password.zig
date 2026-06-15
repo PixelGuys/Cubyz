1c3d830cf32c1c5986127aed21033c22e04bfe97
IntegratedQuantum <43880493+IntegratedQuantum@users.noreply.github.com>
2026-06-15 15:55:58 +0200

    Improve the Account setup process (#3229)
    The start screen is much simpler:
    <img width="1280" height="745" alt="Screenshot at 2026-06-13 21-47-37"
    src="https://github.com/user-attachments/assets/70b89a3a-ce6f-4c11-acf2-4f1bda1e2332"
    />
    Account creation now starts with a small explanation, that is
    unskippable (the button is disabled for 8 seconds)
    <img width="1280" height="745" alt="Screenshot at 2026-06-13 21-47-46"
    src="https://github.com/user-attachments/assets/a334fe53-09cf-41d1-afb1-d88c6abc7489"
    />
    Then you have a set of options how you store it:
    <img width="1280" height="745" alt="Screenshot at 2026-06-13 21-51-28"
    src="https://github.com/user-attachments/assets/ad183891-1ab6-42b8-ad85-9b7091e791f9"
    />
    Password manager and write it down lead to a similar screen, again the
    button to continue is disabled, for 25 seconds this time, just to be
    sure that the player does something:
    <img width="1280" height="745" alt="Screenshot at 2026-06-13 21-47-58"
    src="https://github.com/user-attachments/assets/731e4b93-7686-4588-8e3b-c9777747dabf"
    />
    The save as file does not have a timer, but you have to enter a filename
    to continue.
    It is also missing a file select dialog (you have to enter the path
    manually, which might be annoying, but I will make an issue for this):
    <img width="1280" height="745" alt="Screenshot at 2026-06-13 21-49-07"
    src="https://github.com/user-attachments/assets/898fcbf8-a895-4bb1-ab1c-a50cb628c0fe"
    />
    Then you return to the login screen, after login there are two more
    windows for storing the Account Code:
    <img width="1280" height="745" alt="Screenshot at 2026-06-13 21-49-51"
    src="https://github.com/user-attachments/assets/155cb74f-7aef-4cae-bb51-4b841c46a0e1"
    />
    <img width="1280" height="745" alt="Screenshot at 2026-06-13 21-49-59"
    src="https://github.com/user-attachments/assets/764de9d4-0729-4de9-985b-fde81343790f"
    />
    
    progress towards #3192

---
+++ src/gui/windows/authentication/encrypt_with_password.zig
@@ -0,0 +1,123 @@
+const std = @import("std");
+
+const main = @import("main");
+const settings = main.settings;
+const Vec2f = main.vec.Vec2f;
+
+const gui = main.gui;
+const GuiComponent = gui.GuiComponent;
+const GuiWindow = gui.GuiWindow;
+const Button = GuiComponent.Button;
+const CheckBox = GuiComponent.CheckBox;
+const Label = GuiComponent.Label;
+const HorizontalList = GuiComponent.HorizontalList;
+const TextInput = GuiComponent.TextInput;
+const VerticalList = GuiComponent.VerticalList;
+
+pub var window = GuiWindow{
+	.contentSize = Vec2f{128, 256},
+	.closeIfMouseIsGrabbed = true,
+	.closeable = false,
+};
+
+var innerList: *VerticalList = undefined;
+var encryptWithPasswordCheckbox: *CheckBox = undefined;
+var passwordTextField: *TextInput = undefined;
+var passwordRow: *HorizontalList = undefined;
+
+var confirmButton: *Button = undefined;
+
+var encryptAccountCode: bool = true;
+
+const padding: f32 = 8;
+
+var accountCode: main.network.authentication.AccountCode = undefined;
+
+pub fn setAccountCode(accountCode_: main.network.authentication.AccountCode) void {
+	accountCode = accountCode_;
+}
+
+fn confirm() void {
+	if (encryptAccountCode) {
+		settings.storedAccount.deinit(main.globalAllocator);
+		settings.storedAccount = .initFromPassword(main.globalAllocator, accountCode, passwordTextField.currentString.items);
+	} else {
+		settings.storedAccount.deinit(main.globalAllocator);
+		settings.storedAccount = .initUnencoded(main.globalAllocator, accountCode);
+	}
+	settings.save();
+
+	gui.closeWindowFromRef(&window);
+	if (settings.playerName.len == 0) {
+		gui.openWindow("change_name");
+	} else {
+		gui.openWindow("main");
+	}
+}
+
+fn encryptAccountCodeCallback(encryptAccountCode_: bool) void {
+	encryptAccountCode = encryptAccountCode_;
+	refreshInner();
+}
+
+fn refreshInner() void {
+	innerList.children.clearRetainingCapacity();
+	innerList.children.append(encryptWithPasswordCheckbox.toComponent());
+	if (encryptAccountCode) {
+		innerList.children.append(passwordRow.toComponent());
+	}
+}
+
+fn back() void {
+	gui.windowlist.@"authentication/stay_logged_in".setAccountCode(accountCode);
+	accountCode = .{.text = &.{}};
+	gui.closeWindowFromRef(&window);
+	gui.openWindow("authentication/stay_logged_in");
+}
+
+pub fn onOpen() void {
+	const list = VerticalList.init(.{padding, 16 + padding}, 320, 8);
+	const width = 480;
+	list.add(Label.init(.{0, 0}, width, "Your Account Code will be stored in your settings to allow you to stay logged in. Please decide how we should store it:", .left));
+	innerList = VerticalList.init(.{0, 0}, 100, 16);
+	encryptWithPasswordCheckbox = CheckBox.init(.{0, 0}, width, "Encrypt it with a password (recommended)\n(The password needs to be entered every time)", encryptAccountCode, &encryptAccountCodeCallback);
+	innerList.add(encryptWithPasswordCheckbox);
+	passwordRow = HorizontalList.init();
+	passwordRow.add(Label.init(.{0, 0}, 130, "Local Password:", .left));
+	passwordTextField = TextInput.init(.{0, 0}, width - 130, 22, "", .{.onNewline = .{}});
+	passwordRow.add(passwordTextField);
+	passwordRow.finish(.{0, 0}, .center);
+	innerList.add(passwordRow);
+	innerList.finish(.center);
+	refreshInner();
+	list.add(innerList);
+	const buttonRow = HorizontalList.init();
+	buttonRow.add(Button.initText(.{0, 0}, 70, "Back", .{.onAction = .init(back)}));
+	confirmButton = Button.initText(.{10, 0}, 70, "Confirm", .{.onAction = .init(confirm)});
+	buttonRow.add(confirmButton);
+	buttonRow.finish(.{0, 0}, .center);
+	list.add(buttonRow);
+	list.finish(.center);
+	window.rootComponent = list.toComponent();
+	window.contentSize = window.rootComponent.?.pos() + window.rootComponent.?.size() + @as(Vec2f, @splat(padding));
+	gui.updateWindowPositions();
+}
+
+pub fn update() void {
+	confirmButton.disabled = encryptAccountCode and passwordTextField.currentString.items.len == 0;
+}
+
+pub fn onClose() void {
+	// Make sure there remains no trace of the account code or password in memory
+	accountCode.deinit();
+	main.Window.setClipboardString("");
+	gui.openWindow("clipboard_deleted");
+
+	if (!encryptAccountCode) {
+		passwordRow.deinit();
+	}
+
+	if (window.rootComponent) |*comp| {
+		comp.deinit();
+	}
+}
