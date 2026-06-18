9e9ca54fc80f54db3866243bcb7e396e0d14a5c8
André Fernandes <andres.c.fernandes@gmail.com>
2026-06-18 16:37:10 +0100

    Fix mark

--- src/game.zig
+++ src/game.zig
@@ -383,7 +383,7 @@ pub const World = struct { // MARK: World
 		self.dayTime.update(deltaTime);
 	}
 
-	pub const DayTime = struct { // MARK: DayCycle
+	pub const DayTime = struct { // MARK: DayTime
 		const dayCycleLength = 12000; // Length of one in-game day in 100ms. Midnight is at DAY_CYCLE/2. Sunrise and sunset each take about 1/16 of the day. Currently set to 20 minutes
 		const minimumAmbientLight: f32 = 0.1;
 		pub const nightStart = dayCycleLength/2;
