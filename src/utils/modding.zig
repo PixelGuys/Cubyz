94c14b4a73e37d0743322a1610372f40f50a0976
Wunka <56411012+Wunka@users.noreply.github.com>
2026-06-20 17:32:23 +0200

    that looks nicer

--- src/utils/modding.zig
+++ src/utils/modding.zig
@@ -11,7 +11,7 @@ pub fn getFeatures(comptime T: type) []const Feature {
 	inline for (@typeInfo(T).@"struct".decls) |mod| {
 		const mod_struct = @field(T, mod.name);
 		inline for (std.meta.declarations(mod_struct)) |feature| {
-			features = features ++ .{Feature{
+			features = features ++ [_]Feature{.{
 				.id = mod.name ++ ":" ++ feature.name,
 				.field = @field(mod_struct, feature.name),
 			}};
