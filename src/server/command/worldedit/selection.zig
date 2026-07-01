006b3f0942b9b9072ec76b1d4820b8ef02f00c6d
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2026-07-01 23:14:19 +0200

    Refactor and implement shrink and grow

--- src/server/command/worldedit/selection.zig
+++ src/server/command/worldedit/selection.zig
@@ -8,27 +8,25 @@ const Vec3i = main.vec.Vec3i;
 
 pub const description = "Operate on selection";
 pub const usage =
-	\\/selection norm
 	\\/selection normalize
 	\\  Ensure pos1 is set to minimal coordinates and pos2 is set to maximal coordinates from selection.
-	\\/selection query <info/blocks>
-	\\  info - log general information about selection (pos1, pos2, size, number blocks and entities inside)
-	\\  blocks - log list of unique blocks in selection and count them.
-	\\/selection edit <direction> <amount>
+	\\/selection cube <radius=5>
+	\\  Create a cube selection with center at current player position.
 	\\/selection shrink <limit=32>
 	\\  Automatically shrink the selection to fit a structure, non-air blocks stop shrinking process.
 	\\/selection grow <limit=32>
 	\\  Automatically grow the selection to fit a structure.
+	\\  Non-air blocks stop growing process.
+	\\/selection adjust <limit=32>
+	\\  Same as grow followed by shrink.
 ;
 
-const Mode = enum {info, blocks};
-const Direction = enum {@"+x", @"-x", @"+y", @"-y", @"+z", @"-z", @"front", @"back", @"left", @"rigth", @"up", @"down"};
-
 const Args = union(enum) {
-	@"/selection query <info/blocks>": struct {_: enum{query}, mode: Mode},
-	@"/selection edit <direction> <amount>": struct {_: enum{edit}, direction: Direction, amount: i32},
-	@"/selection shrink <limit>": struct {_: enum{@"shrink"}, limit: ?u32},
-	@"/selection grow <limit>": struct {_: enum{@"grow"}, limit: ?u32},
+	@"/selection normalize": struct { subcommand: enum { normalize } },
+	@"/selection cube <radius>": struct { subcommand: enum { cube }, radius: ?u32 },
+	@"/selection shrink <limit>": struct { subcommand: enum { shrink }, limit: ?u32 },
+	@"/selection grow <limit>": struct { subcommand: enum { grow }, limit: ?u32 },
+	@"/selection adjust <limit>": struct { subcommand: enum { adjust }, limit: ?u32 },
 };
 
 const ArgParser = main.argparse.Parser(Args, .{.commandName = "/selection"});
@@ -43,47 +41,41 @@ pub fn execute(args: []const u8, source: *User) void {
 	};
 
 	switch (result) {
-		.@"/selection query <info/blocks>" => |cmd| query(source, cmd.mode),
-		.@"/selection edit <direction> <amount>" => |cmd| edit(source, cmd.direction, cmd.amount),
-		.@"/selection shrink <limit>" => |cmd| shrink(source, @intCast(@as(u31, @truncate(cmd.limit orelse 32)))),
-		.@"/selection grow <limit>" => |cmd| autoGrow(source, @intCast(@as(u31, @truncate(cmd.limit orelse 32)))),
+		.@"/selection normalize" => normalize(source),
+		.@"/selection cube <radius>" => |cmd| cube(source, @intCast(@as(u31, @truncate(cmd.radius orelse 5)))),
+		.@"/selection shrink <limit>" => |cmd| adjust(.shrink, source, @intCast(@as(u31, @truncate(cmd.limit orelse 32)))),
+		.@"/selection grow <limit>" => |cmd| adjust(.grow, source, @intCast(@as(u31, @truncate(cmd.limit orelse 32)))),
+		.@"/selection adjust <limit>" => |cmd| {
+			adjust(.shrink, source, @intCast(@as(u31, @truncate(cmd.limit orelse 32))));
+			adjust(.grow, source, @intCast(@as(u31, @truncate(cmd.limit orelse 32))));
+		},
 	}
 }
 
-fn query(source: *User, mode: Mode) void {
-	_ = source;
-	_ = mode;
+fn normalize(source: *User) void {
+	const current = command.getCurrentSelection(source) catch return;
+	const minPos = current.minPos;
+	const maxPos = current.maxPos - Vec3i{1, 1, 1};
+
+	updateWorldEditPos(source, minPos, maxPos);
 }
 
-fn edit(source: *User, direction: Direction, amount: i32) void {
-	_ = source;
-	_ = direction;
-	_ = amount;
+fn cube(source: *User, radius: i32) void {
+	const pos: Vec3i = @floor(source.player().pos);
+	updateWorldEditPos(source, pos - @as(Vec3i, @splat(radius)), pos + @as(Vec3i, @splat(radius)));
 }
 
-fn shrink(source: *User, limit: i32) void {
-	if (limit <= 1) return; // This is a noop.
+fn adjust(comptime mode: ScannerMode, source: *User, limit: i32) void {
+	if (limit <= 1) return;
 
 	const current = command.getCurrentSelection(source) catch return;
+	const minPos = current.minPos;
+	const maxPos = current.maxPos - Vec3i{1, 1, 1};
 
-	updateWorldEditPos(source, current.minPos, current.maxPos);
+	var scanner: Scanner3D(mode) = .init(minPos, maxPos, limit);
+	const newMin, const newMax = scanner.scan3D();
 
-	const minX, const minY, const minZ = current.minPos;
-	const maxX, const maxY, const maxZ = current.maxPos;
-
-	const xRange: Range = .init(minX, maxX, 1);
-	const yRange: Range = .init(minY, maxY, 1);
-	const zRange: Range = .init(minZ, maxZ, 1);
-
-	const newMinX = Search(.xyz).search3D(.init(minX, @min(minX + limit, maxX), 1), yRange, zRange, limit) orelse minX;
-	const newMinY = Search(.yxz).search3D(.init(minY, @min(minY + limit, maxY), 1), xRange, zRange, limit) orelse minY;
-	const newMinZ = Search(.zyx).search3D(.init(minZ, @min(minZ + limit, maxZ), 1), yRange, xRange, limit) orelse minZ;
-
-	const newMaxX = Search(.xyz).search3D(.init(maxX, @max(minX, maxX - limit), -1), yRange, zRange, limit) orelse maxX;
-	const newMaxY = Search(.yxz).search3D(.init(maxY, @max(minY, maxY - limit), -1), xRange, zRange, limit) orelse maxY;
-	const newMaxZ = Search(.zyx).search3D(.init(maxZ, @max(minZ, maxZ - limit), -1), yRange, xRange, limit) orelse maxZ;
-
-	updateWorldEditPos(source, .{newMinX, newMinY, newMinZ}, .{newMaxX, newMaxY, newMaxZ});
+	updateWorldEditPos(source, newMin, newMax);
 }
 
 fn updateWorldEditPos(source: *User, pos1: Vec3i, pos2: Vec3i) void {
@@ -92,57 +84,6 @@ fn updateWorldEditPos(source: *User, pos1: Vec3i, pos2: Vec3i) void {
 
 	source.worldEditData.selectionPosition2 = pos2;
 	main.network.protocols.genericUpdate.sendWorldEditPos(source.conn, .selectedPos2, pos2);
-}
-
-
-fn Search(comptime orientation: enum{xyz, yxz, zyx}) type {
-	return struct {
-		const Self = @This();
-
-		fn search3D(iRange: Range, jRange: Range, kRange: Range, limit: i32) ?i32 {
-			std.log.debug("{s} {} {} {}", .{@tagName(orientation), iRange, jRange, kRange});
-			var iLimit: i32 = 0;
-			var iIterator = iRange.iter();
-			while (iIterator.next()) |i| {
-				var jLimit: i32 = 0;
-
-				var jIterator = jRange.iter();
-				while (jIterator.next()) |j| {
-					var kLimit: i32 = 0;
-
-					var kIterator = kRange.iter();
-					while (kIterator.next()) |k| {
-						if (Self.getBlock(i, j, k)) |block| {
-							if (block.typ != 0) {
-								return i;
-							}
-						}
-
-						kLimit += 1;
-						// We didn't even finish scanning one JK plane, so we can't return updated I
-						if (kLimit > limit) return null;
-					}
-
-					jLimit += 1;
-					// We didn't even finish scanning one JK plane, so we can't return updated I
-					if (jLimit > limit) return null;
-				}
-
-				iLimit += 1;
-				// We scanned one JK plane and it was empty (we didn't return) so we can update I
-				if (iLimit > limit) return i;
-			}
-			return null;
-		}
-
-		fn getBlock(i: i32, j: i32, k: i32) ?Block {
-			return switch (orientation) {
-				.xyz => main.server.world.?.getBlock(i, j, k),
-				.yxz => main.server.world.?.getBlock(j, i, k),
-				.zyx => main.server.world.?.getBlock(k, j, i),
-			};
-		}
-	};
 }
 
 const Range = struct {
@@ -181,8 +122,238 @@ const Range = struct {
 	}
 };
 
+const ScannerMode = enum { shrink, grow };
 
-fn autoGrow(source: *User, limit: ?i32) void {
-	_ = source;
-	_ = limit;
+fn Scanner3D(comptime mode: ScannerMode) type {
+	return struct {
+		const Self = @This();
+
+		min: [3]i32,
+		max: [3]i32,
+
+		limit: i32,
+
+		originalMin: [3]i32 = @splat(0),
+		originalMax: [3]i32 = @splat(0),
+
+		const Axis = enum(u2) {
+			x = 0,
+			y = 1,
+			z = 2,
+			const iterator: [3]Axis = .{.x, .y, .z};
+		};
+		const Candidate = enum(u1) {
+			min = 0,
+			max = 1,
+			const iterator: [2]Candidate = .{.min, .max};
+		};
+		const Stage = struct {
+			axis: Axis,
+			candidate: Candidate,
+			isComplete: bool,
+		};
+
+		fn init(min: Vec3i, max: Vec3i, limit: i32) Self {
+			return .{.min = min, .max = max, .limit = limit};
+		}
+
+		fn getRange(self: Self, axis: Axis) Range {
+			const i: usize = @intFromEnum(axis);
+			return .init(self.min[i], self.max[i] + 1, 1);
+		}
+
+		pub fn scan3D(self: *Self) struct { Vec3i, Vec3i } {
+			self.originalMin = self.min;
+			self.originalMax = self.max;
+
+			var scanningSequence: [6]Stage = .{
+				.{.axis = .x, .candidate = .min, .isComplete = false},
+				.{.axis = .x, .candidate = .max, .isComplete = false},
+				.{.axis = .y, .candidate = .min, .isComplete = false},
+				.{.axis = .y, .candidate = .max, .isComplete = false},
+				.{.axis = .z, .candidate = .min, .isComplete = false},
+				.{.axis = .z, .candidate = .max, .isComplete = false},
+			};
+
+			// For a simple shrinking process, this could have been much simpler: Just three nested loops in
+			// each out of 6 directions would be enough. However, if we want to properly implmenet growing,
+			// every consecutive direction of scanning needs to account for the previous iteration results.
+			// This is especially important when working with clusters of small objects, which could potentially
+			// be cut off if we just kept the original size of the selection and only scanned in a star shape.
+			//
+			//  Example:
+			//   Structure consisting of two L like parts, overalpping but not touching:
+			//
+			//         **
+			//     *    *
+			//     **
+			//               ┃   ┃
+			//               ┃   ┃ * this space is never checked if we don't account for previous iterations
+			//      ┏━━━┓    ╋━━━╋━━━
+			//      ┃   ┃    ┃ * ┃
+			//      ┗━━━┛    ┗━━━┻━━━
+			//
+			//                           ┃      ┃
+			//                     *     ┃    * ┃ if we account for previous iterations we capture clustered objects
+			//      ┏━━━┓    ╋━━━╋━━━    ╋━━━━━━╋
+			//      ┃   ┃    ┃ * ┃       ┃ *    ┃
+			//      ┗━━━┛    ┗━━━┻━━━    ┗━━━━━━┻
+			//
+			// Now, this code does an extra effort of altering between directions until all are saturated.
+			// This part might not be necessary, but I don't think it changes much in the design, so I did it this way.
+
+			while (true) doScan: {
+				for (&scanningSequence) |*stage| {
+					if (stage.isComplete) continue;
+
+					switch (mode) {
+						.shrink => self.shrink(stage),
+						.grow => self.grow(stage),
+					}
+				}
+				for (scanningSequence) |stage| if (!stage.isComplete) break :doScan;
+				break;
+			}
+
+			return .{self.min, self.max};
+		}
+
+		fn getCurrentValue(self: Self, axis: Axis, candidate: Candidate) i32 {
+			const i: usize = @intFromEnum(axis);
+			return switch (candidate) {
+				.min => self.min[i],
+				.max => self.max[i],
+			};
+		}
+
+		fn shrink(self: *Self, stage: *Stage) void {
+			const currentValue = self.getCurrentValue(stage.axis, stage.candidate);
+
+			switch (self.scanPerpendicularPlane(stage.axis, currentValue)) {
+				.failure, .limitExceeded => {
+					stage.isComplete = true;
+					return;
+				},
+				.success => {},
+			}
+
+			const newValue = self.getCandidate(stage.axis, stage.candidate);
+
+			if (!self.isValidCandidate(stage.axis, stage.candidate, newValue)) {
+				stage.isComplete = true;
+				return;
+			}
+
+			switch (stage.candidate) {
+				.min => self.min[@intFromEnum(stage.axis)] = newValue,
+				.max => self.max[@intFromEnum(stage.axis)] = newValue,
+			}
+		}
+
+		fn grow(self: *Self, stage: *Stage) void {
+			const newValue = self.getCandidate(stage.axis, stage.candidate);
+
+			if (!self.isValidCandidate(stage.axis, stage.candidate, newValue)) {
+				stage.isComplete = true;
+				return;
+			}
+
+			switch (self.scanPerpendicularPlane(stage.axis, newValue)) {
+				.failure, .limitExceeded => {
+					stage.isComplete = true;
+					return;
+				},
+				.success => {},
+			}
+
+			switch (stage.candidate) {
+				.min => self.min[@intFromEnum(stage.axis)] = newValue,
+				.max => self.max[@intFromEnum(stage.axis)] = newValue,
+			}
+		}
+
+		fn getCandidate(self: Self, axis: Axis, candidate: Candidate) i32 {
+			const i: usize = @intFromEnum(axis);
+			return switch (mode) {
+				.shrink => switch (candidate) {
+					.min => self.min[i] + 1,
+					.max => self.max[i] - 1,
+				},
+				.grow => switch (candidate) {
+					.min => self.min[i] - 1,
+					.max => self.max[i] + 1,
+				},
+			};
+		}
+
+		/// Check external limits to the iteration - fully collapsing the selection or exceeding the limit of iterations.
+		fn isValidCandidate(self: Self, axis: Axis, candidate: Candidate, newValue: i32) bool {
+			const i: usize = @intFromEnum(axis);
+			return switch (mode) {
+				.shrink => switch (candidate) {
+					.min => newValue < self.originalMax[i] and newValue < self.originalMin[i] + self.limit,
+					.max => newValue > self.originalMin[i] and newValue > self.originalMax[i] - self.limit,
+				},
+				.grow => switch (candidate) {
+					.min => newValue > self.originalMin[i] - self.limit,
+					.max => newValue < self.originalMax[i] + self.limit,
+				},
+			};
+		}
+
+		/// Scan a 2D plane of blocks perpendicular to the given axis.
+		/// `currentValue` determines which of infinitely many planes to choose using a coordinate on `axis`.
+		fn scanPerpendicularPlane(self: Self, axis: Axis, currentValue: i32) ScanStatus {
+			return switch (axis) {
+				.x => Scanner2D(.yz, mode).scanPlane(currentValue, self.getRange(.y), self.getRange(.z), self.limit),
+				.y => Scanner2D(.xz, mode).scanPlane(currentValue, self.getRange(.x), self.getRange(.z), self.limit),
+				.z => Scanner2D(.yx, mode).scanPlane(currentValue, self.getRange(.y), self.getRange(.x), self.limit),
+			};
+		}
+	};
+}
+
+const ScanStatus = enum { success, failure, limitExceeded };
+
+fn Scanner2D(comptime plane: enum { yz, xz, yx }, comptime mode: ScannerMode) type {
+	return struct {
+		const Self = @This();
+
+		fn scanPlane(i: i32, jRange: Range, kRange: Range, limit: i32) ScanStatus {
+			var jLimit: i32 = 0;
+
+			var jIterator = jRange.iter();
+			while (jIterator.next()) |j| {
+				var kLimit: i32 = 0;
+
+				var kIterator = kRange.iter();
+				while (kIterator.next()) |k| {
+					const x, const y, const z = Self.mapCoordinates(i, j, k);
+
+					if (main.server.world.?.getBlock(x, y, z)) |block| {
+						// Finding a non-air block in shrink mode means we have to stop contracting,
+						// but in growing mode it means we can continue expading to possibly find more.
+						if (block.typ != 0) return if (mode == .shrink) .failure else .success;
+					}
+
+					kLimit += 1;
+					// We didn't even finish scanning one JK plane, so we can't return updated I
+					if (kLimit > limit) return .limitExceeded;
+				}
+
+				jLimit += 1;
+				// We didn't even finish scanning one JK plane, so we can't return updated I
+				if (jLimit > limit) return .limitExceeded;
+			}
+			return if (mode == .shrink) .success else .failure;
+		}
+
+		fn mapCoordinates(i: i32, j: i32, k: i32) struct { i32, i32, i32 } {
+			return switch (plane) {
+				.yz => .{i, j, k},
+				.xz => .{j, i, k},
+				.yx => .{k, j, i},
+			};
+		}
+	};
 }
