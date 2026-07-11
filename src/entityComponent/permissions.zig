1f9bcf6f2c2f404845511e38efe975552f72233d
Wunka <56411012+Wunka@users.noreply.github.com>
2026-07-11 15:31:56 +0200

    remove most imports

--- src/entityComponent/permissions.zig
+++ src/entityComponent/permissions.zig
@@ -1,39 +1,18 @@
 const std = @import("std");
 
 const main = @import("main");
-const chunk = main.chunk;
 const Entity = main.entity.Entity;
-const ServerChunk = chunk.ServerChunk;
-const game = main.game;
-const graphics = main.graphics;
-const ZonElement = main.ZonElement;
-const renderer = main.renderer;
-const settings = main.settings;
 const utils = main.utils;
 const BinaryReader = utils.BinaryReader;
 const BinaryWriter = utils.BinaryWriter;
-const vec = main.vec;
-const Mat4f = vec.Mat4f;
-const Vec3d = vec.Vec3d;
-const Vec3f = vec.Vec3f;
-const Vec4f = vec.Vec4f;
-const Vec3i = vec.Vec3i;
 const NeverFailingAllocator = main.heap.NeverFailingAllocator;
-const blocks = main.blocks;
-const World = game.World;
-const ServerWorld = main.server.ServerWorld;
-const items = main.items;
-const ItemStack = items.ItemStack;
-const random = main.random;
-
-const c = @import("c");
 
 pub var entityComponentID: main.entity.EntityComponentId = undefined;
 pub const entityComponentVersion = 0;
 
 // ############################# Client only stuff ################################
 pub const client = struct {
-	pub fn load(entity: Entity, reader: *utils.BinaryReader, version: u32) main.entity.EntityComponentLoadError!void {
+	pub fn load(entity: Entity, reader: *BinaryReader, version: u32) main.entity.EntityComponentLoadError!void {
 		_ = entity;
 		_ = reader;
 		_ = version;
@@ -50,7 +29,7 @@ pub const server = struct {
 	pub const Component = struct {
 		permissions: main.server.permission.Permissions,
 
-		pub fn save(self: Component, writer: *utils.BinaryWriter, audience: main.entity.AudienceInfo) main.entity.ComponentSaveBehaviour {
+		pub fn save(self: Component, writer: *BinaryWriter, audience: main.entity.AudienceInfo) main.entity.ComponentSaveBehaviour {
 			if (audience != .disk) return .discard;
 			self.permissions.toBytes(writer);
 			return .save;
@@ -74,7 +53,7 @@ pub const server = struct {
 		return &(components.get(entity) orelse return null).permissions;
 	}
 
-	pub fn loadFromData(entity: Entity, reader: *utils.BinaryReader, version: u32) main.entity.EntityComponentLoadError!void {
+	pub fn loadFromData(entity: Entity, reader: *BinaryReader, version: u32) main.entity.EntityComponentLoadError!void {
 		if (version != entityComponentVersion) return error.InvalidComponentVersion;
 		const permissions = &components.add(main.globalAllocator, entity).permissions;
 		permissions.* = .init(main.globalAllocator);
