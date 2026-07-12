0bced26c10e30904a4dd648821402c6a1de14e3f
Vinywar123 <bahnsenvincent@gmail.com>
2026-07-11 22:16:36 -0700

    Update Button.zig

--- src/gui/components/Button.zig
+++ src/gui/components/Button.zig
@@ -182,7 +182,4 @@ pub fn renderButton(self: *Button, mousePosition: Vec2f) void {
 
 	textures.outlineTexture.bindTo(0);
 	graphics.draw.bound9SliceImage(self.pos, self.size, textures.outlineTextureSize, cornerSize, 2);
-
-	const oldColor = draw.setColor(if (self.disabled) 0xff808080 else 0xffffffff);
-	defer draw.restoreColor(oldColor);
 }
