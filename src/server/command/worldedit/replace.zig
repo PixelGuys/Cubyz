ee68f2fde742e575e000375f2f922782010faeb3
Wunka <56411012+Wunka@users.noreply.github.com>
2026-07-19 07:18:40 +0200

    replace cmd update

--- src/server/command/worldedit/replace.zig
+++ src/server/command/worldedit/replace.zig
@@ -13,23 +13,29 @@ const Mask = main.blueprint.Mask;
 pub const description = "Replace blocks in the world edit selection.";
 pub const usage = "/replace <old mask> <new pattern>";
 
+const Args = union(enum) {
+	@"/replace <old mask> <new pattern>": struct { oldMask: []const u8, newPattern: []const u8 },
+};
+
+const ArgParser = main.argparse.Parser(Args, .{.commandName = "/replace"});
+
 pub fn execute(args: []const u8, source: *User) void {
-	var argsSplit = std.mem.splitScalar(u8, args, ' ');
-	const oldMaskString = argsSplit.next() orelse {
-		return source.sendMessage("#ff0000Missing required <old> argument.", .{});
-	};
-	const newPatternString = argsSplit.next() orelse {
-		return source.sendMessage("#ff0000Missing required <new> argument.", .{});
+	var errorMessage: main.List(u8) = .empty;
+	defer errorMessage.deinit(main.stackAllocator);
+
+	const result = ArgParser.parse(main.stackAllocator, args, &errorMessage) catch {
+		source.sendMessage("#ff0000{s}", .{errorMessage.items});
+		return;
 	};
 
 	const selection = command.getCurrentSelection(source) catch return;
 
-	const oldMask = Mask.initFromString(main.stackAllocator, oldMaskString) catch |err| {
+	const oldMask = Mask.initFromString(main.stackAllocator, result.@"/replace <old mask> <new pattern>".oldMask) catch |err| {
 		return source.sendMessage("#ff0000Error parsing mask: {s}", .{@errorName(err)});
 	};
 	defer oldMask.deinit(main.stackAllocator);
 
-	const newPattern = Pattern.initFromString(main.stackAllocator, newPatternString) catch |err| {
+	const newPattern = Pattern.initFromString(main.stackAllocator, result.@"/replace <old mask> <new pattern>".newPattern) catch |err| {
 		return source.sendMessage("#ff0000Error parsing pattern: {s}", .{@errorName(err)});
 	};
 	defer newPattern.deinit(main.stackAllocator);
