88d3e46d6ca5ad970752bab5215a31855ba8a967
Vinywar123 <bahnsenvincent@gmail.com>
2026-07-19 20:50:52 -0700

    added functionality

--- src/proceduralItem/modifiers/restrictions/encased.zig
+++ src/proceduralItem/modifiers/restrictions/encased.zig
@@ -5,20 +5,26 @@ const NeverFailingAllocator = main.heap.NeverFailingAllocator;
 const ModifierRestriction = main.items.ModifierRestriction;
 const ProceduralItem = main.items.ProceduralItem;
 const ZonElement = main.ZonElement;
+const ModifierRestrictionOutput = main.items.ModifierRestrictionOutput;
 
 const Encased = struct {
 	tag: main.Tag,
 	amount: usize,
 };
 
-pub fn satisfied(self: *const Encased, proceduralItem: *const ProceduralItem, x: i32, y: i32) bool {
+pub fn satisfied(self: *const Encased, proceduralItem: *const ProceduralItem, x: i32, y: i32) ModifierRestrictionOutput {
 	var count: usize = 0;
 	for ([_]i32{-1, 0, 1}) |dx| {
 		for ([_]i32{-1, 0, 1}) |dy| {
 			if ((proceduralItem.getItemAt(x + dx, y + dy) orelse continue).hasTag(self.tag)) count += 1;
 		}
 	}
-	return count >= self.amount;
+	return .{
+		.ifSatisfied = count >= self.amount, 
+		.totalItemsChecked = count, 
+		.totalCountedItems = 9, 
+		.modifierPower = 1,
+	};
 }
 
 pub fn loadFromZon(allocator: NeverFailingAllocator, zon: ZonElement) *const Encased {
@@ -28,11 +34,15 @@ pub fn loadFromZon(allocator: NeverFailingAllocator, zon: ZonElement) *const Enc
 			std.log.err("Missing tag field for encased restriction.", .{});
 			break :blk "not specified";
 		}),
-		.amount = zon.get(usize, "amount") orelse 8,
+		.amount = zon.get(usize, "amount") orelse 0,
 	};
 	return result;
 }
 
 pub fn printTooltip(self: *const Encased, outString: *main.ListManaged(u8)) void {
-	outString.print("encased in {} .{s}", .{self.amount, self.tag.getName()});
+	if (self.amount == 0) {
+		outString.print("encased in .{s}", .{self.tag.getName()});
+	} else {
+		outString.print("encased in {} .{s}", .{self.amount, self.tag.getName()});
+	}
 }
