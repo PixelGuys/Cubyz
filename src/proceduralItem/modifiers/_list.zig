4484ed31aa82e23fbc88d38edcfa1c99241952cf
Vinywar123 <bahnsenvincent@gmail.com>
2026-07-20 16:36:09 -0700

    small tweaks

--- src/proceduralItem/modifiers/_list.zig
+++ src/proceduralItem/modifiers/_list.zig
@@ -1,9 +1,11 @@
-pub const bad_at = @import("bad_at.zig");
 pub const durable = @import("durable.zig");
 pub const fragile = @import("fragile.zig");
-pub const good_at = @import("good_at.zig");
-pub const heavy = @import("heavy.zig");
-pub const light = @import("light.zig");
-pub const powerful = @import("powerful.zig");
 pub const single_use = @import("single_use.zig");
+pub const good_at = @import("good_at.zig");
+pub const bad_at = @import("bad_at.zig");
+pub const light = @import("light.zig");
+pub const heavy = @import("heavy.zig");
+pub const powerful = @import("powerful.zig");
 pub const weak = @import("weak.zig");
+pub const long = @import("long.zig");
+pub const short = @import("short.zig");
