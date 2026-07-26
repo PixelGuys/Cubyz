PixelGuys/Cubyz

commit 63e2b6c5fa1b09b5e5355d5fe86edd84373c6698

Use the frame uniforms in more places (#3380)
progress towards #102

src/itemdrop.zig
@@ -507,9 +507,7 @@ pub const ItemDisplayManager = struct { // MARK: ItemDisplayManager
 pub const ItemDropRenderer = struct { // MARK: ItemDropRenderer
 	var itemPipeline: graphics.Pipeline = undefined;
 	var itemUniforms: struct {
-		projectionMatrix: c_int,
 		modelMatrix: c_int,
-		viewMatrix: c_int,
 		ambientLight: c_int,
 		modelIndex: c_int,
 		block: c_int,
@@ -521,6 +519,7 @@ pub const ItemDropRenderer = struct { // MARK: ItemDropRenderer
 	var itemModelSSBO: graphics.SSBO = undefined;
 	var modelData: main.ListManaged(u32) = undefined;
 	var freeSlots: main.ListManaged(*ItemVoxelModel) = undefined;
+	var displayItemUbo: graphics.frame_uniforms.StaticUbo = undefined;
 
 	const ItemVoxelModel = struct {
 		index: u31 = undefined,
@@ -628,6 +627,13 @@ pub const ItemDropRenderer = struct { // MARK: ItemDropRenderer
 
 		modelData = .init(main.globalAllocator);
 		freeSlots = .init(main.globalAllocator);
+
+		displayItemUbo = .init(.{
+			.projectionMatrix = Mat4f.perspective(std.math.degreesToRadians(65), @as(f32, @floatFromInt(main.renderer.lastWidth))/@as(f32, @floatFromInt(main.renderer.lastHeight)), 0.01, 3).toGl(),
+			.viewMatrix = Mat4f.identity().toGl(),
+			.playerPositionInteger = @splat(0),
+			.playerPositionFraction = @splat(0),
+		});
 	}
 
 	pub fn deinit() void {
@@ -648,12 +654,10 @@ pub const ItemDropRenderer = struct { // MARK: ItemDropRenderer
 		return voxelModels.findOrCreate(compareObject, ItemVoxelModel.init, null);
 	}
 
-	fn bindCommonUniforms(projMatrix: Mat4f, viewMatrix: Mat4f, ambientLight: Vec3f) void {
+	fn bindCommonUniforms(ambientLight: Vec3f) void {
 		itemPipeline.bind(null);
 		c.glUniform1f(itemUniforms.reflectionMapSize, main.renderer.reflectionCubeMapSize);
-		c.glUniformMatrix4fv(itemUniforms.projectionMatrix, 1, c.GL_TRUE, @ptrCast(&projMatrix));
 		c.glUniform3fv(itemUniforms.ambientLight, 1, @ptrCast(&ambientLight));
-		c.glUniformMatrix4fv(itemUniforms.viewMatrix, 1, c.GL_TRUE, @ptrCast(&viewMatrix));
 		c.glUniform1f(itemUniforms.contrast, 0.12);
 		var depthRange: [2]f32 = undefined;
 		c.glGetFloatv(c.GL_DEPTH_RANGE, &depthRange);
@@ -677,10 +681,10 @@ pub const ItemDropRenderer = struct { // MARK: ItemDropRenderer
 		c.glDrawElements(c.GL_TRIANGLES, vertices, c.GL_UNSIGNED_INT, null);
 	}
 
-	pub fn renderItemDrops(projMatrix: Mat4f, ambientLight: Vec3f, playerPos: Vec3d) void {
+	pub fn renderItemDrops(ambientLight: Vec3f, playerPos: Vec3d) void {
 		game.world.?.itemDrops.updateInterpolationData();
 
-		bindCommonUniforms(projMatrix, game.camera.viewMatrix, ambientLight);
+		bindCommonUniforms(ambientLight);
 		const itemDrops = &game.world.?.itemDrops.super;
 		for (itemDrops.indices[0..itemDrops.size]) |i| {
 			const item = itemDrops.list.items(.itemStack)[i].item;
@@ -731,9 +735,9 @@ pub const ItemDropRenderer = struct { // MARK: ItemDropRenderer
 	pub fn renderDisplayItems(ambientLight: Vec3f, playerPos: Vec3d) void {
 		if (!ItemDisplayManager.showItem) return;
 
-		const projMatrix: Mat4f = Mat4f.perspective(std.math.degreesToRadians(65), @as(f32, @floatFromInt(main.renderer.lastWidth))/@as(f32, @floatFromInt(main.renderer.lastHeight)), 0.01, 3);
-		const viewMatrix = Mat4f.identity();
-		bindCommonUniforms(projMatrix, viewMatrix, ambientLight);
+		displayItemUbo.bind();
+		defer displayItemUbo.unbind();
+		bindCommonUniforms(ambientLight);
 
 		const item = game.Player.inventory.getItem(game.Player.selectedSlot);
 		if (item != .null) {

assets/cubyz/shaders/background/vertex.vert
@@ -1,12 +1,11 @@
 #version 460
 
+#include "frame_uniforms.glsl"
+
 layout(location = 0) in vec3 vertexPos;
 layout(location = 1) in vec2 texCoords;
 
 layout(location = 0) out vec2 outTexCoords;
-
-layout(location = 0) uniform mat4 viewMatrix;
-layout(location = 1) uniform mat4 projectionMatrix;
 
 void main() {
 	gl_Position = projectionMatrix*viewMatrix*vec4(vertexPos, 1);

assets/cubyz/shaders/block_entity/sign.vert
@@ -1,5 +1,7 @@
 #version 460
 
+#include "frame_uniforms.glsl"
+
 layout(location = 0) out vec3 mvVertexPos;
 layout(location = 1) out vec3 direction;
 layout(location = 2) out vec3 light;
@@ -7,10 +9,6 @@ layout(location = 3) out vec2 uv;
 layout(location = 4) flat out vec3 normal;
 
 layout(location = 0) uniform vec3 ambientLight;
-layout(location = 1) uniform mat4 projectionMatrix;
-layout(location = 2) uniform mat4 viewMatrix;
-layout(location = 3) uniform ivec3 playerPositionInteger;
-layout(location = 4) uniform vec3 playerPositionFraction;
 layout(location = 5) uniform int quadIndex;
 layout(location = 6) uniform uvec4 lightData;
 layout(location = 7) uniform ivec3 chunkPos;

assets/cubyz/shaders/block_selection_vertex.vert
@@ -1,9 +1,9 @@
 #version 460
 
+#include "frame_uniforms.glsl"
+
 layout(location = 0) out vec3 mvVertexPos;
 
-layout(location = 0) uniform mat4 projectionMatrix;
-layout(location = 1) uniform mat4 viewMatrix;
 layout(location = 2) uniform vec3 modelPosition;
 layout(location = 3) uniform vec3 lowerBounds;
 layout(location = 4) uniform vec3 upperBounds;

assets/cubyz/shaders/bloom/color_extractor_downsample.frag
@@ -1,5 +1,7 @@
 #version 460
 
+#include "frame_uniforms.glsl"
+
 layout(location = 0) out vec4 fragColor;
 
 layout(location = 0) in vec2 texCoords;
@@ -12,9 +14,6 @@ layout(binding = 4) uniform sampler2D depthTexture;
 layout(location = 1) uniform vec2 tanXY;
 layout(location = 2) uniform float zNear;
 layout(location = 3) uniform float zFar;
-
-layout(location = 4) uniform ivec3 playerPositionInteger;
-layout(location = 5) uniform vec3 playerPositionFraction;
 
 struct Fog {
 	vec3 color;

assets/cubyz/shaders/deferred_render_pass.frag
@@ -1,5 +1,7 @@
 #version 460
 
+#include "frame_uniforms.glsl"
+
 layout(location = 0) out vec4 fragColor;
 
 layout(location = 0) in vec2 texCoords;
@@ -14,9 +16,6 @@ layout(binding = 5) uniform sampler2D bloomColor;
 layout(location = 1) uniform vec2 tanXY;
 layout(location = 2) uniform float zNear;
 layout(location = 3) uniform float zFar;
-
-layout(location = 4) uniform ivec3 playerPositionInteger;
-layout(location = 5) uniform vec3 playerPositionFraction;
 
 struct Fog {
 	vec3 color;

assets/cubyz/shaders/entity_vertex.vert
@@ -1,5 +1,7 @@
 #version 460
 
+#include "frame_uniforms.glsl"
+
 layout (location = 0) in vec3 inPos;
 layout (location = 1) in vec3 inNormal;
 layout (location = 2) in vec2 inUV;
@@ -10,8 +12,7 @@ layout(location = 1) out vec3 mvVertexPos;
 layout(location = 2) out vec3 outLight;
 layout(location = 3) flat out vec3 normal;
 
-layout(location = 0) uniform mat4 projectionMatrix;
-layout(location = 1) uniform mat4 viewMatrix;
+layout(location = 1) uniform mat4 modelViewMatrix;
 layout(location = 2) uniform vec3 ambientLight;
 layout(location = 3) uniform uint light;
 layout(location = 6) uniform uint nodeBufferOffset;
@@ -42,7 +43,7 @@ vec3 calcLight(uint fullLight) {
 void main() {
 	normal = inNormal;
 
-	vec4 mvPos = viewMatrix*nodeMatrices[nodeBufferOffset + inNodeId]*vec4(inPos, 1);
+	vec4 mvPos = modelViewMatrix*nodeMatrices[nodeBufferOffset + inNodeId]*vec4(inPos, 1);
 	gl_Position = projectionMatrix*mvPos;
 	mvVertexPos = mvPos.xyz;
 	outTexCoord = inUV;

assets/cubyz/shaders/item_drop.frag
@@ -1,5 +1,7 @@
 #version 460
 
+#include "frame_uniforms.glsl"
+
 layout(location = 0) in vec3 startPosition;
 layout(location = 1) in vec3 direction;
 layout(location = 2) in vec3 cameraSpacePos;
@@ -17,8 +19,6 @@ layout(binding = 0) uniform sampler2DArray textureSampler;
 layout(binding = 1) uniform sampler2DArray emissionSampler;
 layout(binding = 2) uniform sampler2DArray reflectivityAndAbsorptionSampler;
 layout(binding = 4) uniform samplerCube reflectionMap;
-
-layout(location = 0) uniform mat4 projectionMatrix;
 
 layout(location = 5) uniform vec3 ambientLight;
 layout(location = 7) uniform float sizeScale;

commit 5bf8dc313c7da124054c6710c6e0ce193315284d

fix item hand shader uniforms (#3429)
The mistake was that I thought the matrix could be statically
initialized, which is not true since it depends on window aspect ratio.

broken by #3380

fixes #3428

src/itemdrop.zig
@@ -519,7 +519,6 @@ pub const ItemDropRenderer = struct { // MARK: ItemDropRenderer
 	var itemModelSSBO: graphics.SSBO = undefined;
 	var modelData: main.ListManaged(u32) = undefined;
 	var freeSlots: main.ListManaged(*ItemVoxelModel) = undefined;
-	var displayItemUbo: graphics.frame_uniforms.StaticUbo = undefined;
 
 	const ItemVoxelModel = struct {
 		index: u31 = undefined,
@@ -627,13 +626,6 @@ pub const ItemDropRenderer = struct { // MARK: ItemDropRenderer
 
 		modelData = .init(main.globalAllocator);
 		freeSlots = .init(main.globalAllocator);
-
-		displayItemUbo = .init(.{
-			.projectionMatrix = Mat4f.perspective(std.math.degreesToRadians(65), @as(f32, @floatFromInt(main.renderer.lastWidth))/@as(f32, @floatFromInt(main.renderer.lastHeight)), 0.01, 3).toGl(),
-			.viewMatrix = Mat4f.identity().toGl(),
-			.playerPositionInteger = @splat(0),
-			.playerPositionFraction = @splat(0),
-		});
 	}
 
 	pub fn deinit() void {
@@ -735,6 +727,13 @@ pub const ItemDropRenderer = struct { // MARK: ItemDropRenderer
 	pub fn renderDisplayItems(ambientLight: Vec3f, playerPos: Vec3d) void {
 		if (!ItemDisplayManager.showItem) return;
 
+		const displayItemUbo = graphics.frame_uniforms.StaticUbo.init(.{
+			.projectionMatrix = Mat4f.perspective(std.math.degreesToRadians(65), @as(f32, @floatFromInt(main.renderer.lastWidth))/@as(f32, @floatFromInt(main.renderer.lastHeight)), 0.01, 3).toGl(),
+			.viewMatrix = Mat4f.identity().toGl(),
+			.playerPositionInteger = @splat(0),
+			.playerPositionFraction = @splat(0),
+		});
+		defer displayItemUbo.deinit();
 		displayItemUbo.bind();
 		defer displayItemUbo.unbind();
 		bindCommonUniforms(ambientLight);