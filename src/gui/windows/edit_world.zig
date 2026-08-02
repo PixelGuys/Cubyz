73745c010d9f36de48778f29f4be8d9bca4cdbf9
Abhay Kagdyal <kagdiyal.a11@gmail.com>
2026-08-02 20:37:22 +0530

    add edit-world window

---
+++ src/gui/windows/edit_world.zig
@@ -0,0 +1,59 @@
+const std = @import("std");
+
+const main = @import("main");
+const Vec2f = main.vec.Vec2f;
+const NeverFailingAllocator = main.heap.NeverFailingAllocator;
+const ZonElement = main.ZonElement;
+
+const gui = @import("../gui.zig");
+const GuiWindow = gui.GuiWindow;
+const Button = @import("../components/Button.zig");
+const Label = @import("../components/Label.zig");
+const TextInput = @import("../components/TextInput.zig");
+const HorizontalList = @import("../components/HorizontalList.zig");
+const VerticalList = @import("../components/VerticalList.zig");
+
+fn pass() void {}
+
+pub var window = GuiWindow{
+    .contentSize = Vec2f{ 128, 256 },
+};
+
+const padding: f32 = 8;
+
+var editWorldName: []const u8 = "";
+
+var worldInfo: *ZonElement = undefined;
+
+var nameInput: *TextInput = undefined;
+
+pub fn init() void {
+    editWorldName = "";
+}
+
+pub fn deinit() void {
+    main.globalAllocator.free(editWorldName);
+}
+
+pub fn setEditWorldName(name: []const u8) void {
+    main.globalAllocator.free(editWorldName);
+    editWorldName = main.globalAllocator.dupe(u8, name);
+}
+
+pub fn onOpen() void {
+    //const list = VerticalList.init(.{ padding, 16 + padding }, 300, 8);
+
+    //    nameInput = TextInput.init(.{ 0, 0 }, 128, 22, editWorldName, .{ .onNewline = .init(pass) });
+    //    list.add(nameInput);
+
+    //list.finish(.center);
+    //window.rootComponent = list.toComponent();
+    window.contentSize = window.rootComponent.?.pos() + window.rootComponent.?.size() + @as(Vec2f, @splat(padding));
+    gui.updateWindowPositions();
+}
+
+pub fn onClose() void {
+    if (window.rootComponent) |*comp| {
+        comp.deinit();
+    }
+}
--- src/gui/windows/save_selection.zig
+++ src/gui/windows/save_selection.zig
@@ -17,7 +17,7 @@ const TextInput = @import("../components/TextInput.zig");
 const VerticalList = @import("../components/VerticalList.zig");
 
 pub var window = GuiWindow{
-	.contentSize = Vec2f{128, 256},
+    .contentSize = Vec2f{ 128, 256 },
 };
 
 const padding: f32 = 8;
@@ -30,150 +30,160 @@ pub var mode: main.server.ServerWorld.Mode = undefined;
 
 var deleteIcon: Texture = undefined;
 var fileExplorerIcon: Texture = undefined;
+var editIcon: Texture = undefined;
 
 const WorldInfo = struct {
-	lastUsedTime: i64,
-	name: []const u8,
-	fileName: []const u8,
+    lastUsedTime: i64,
+    name: []const u8,
+    fileName: []const u8,
 };
 var worldList: main.List(WorldInfo) = .empty;
 
 pub fn init() void {
-	deleteIcon = Texture.initFromFile("assets/cubyz/ui/delete_icon.png");
-	fileExplorerIcon = Texture.initFromFile("assets/cubyz/ui/file_explorer_icon.png");
+    deleteIcon = Texture.initFromFile("assets/cubyz/ui/delete_icon.png");
+    fileExplorerIcon = Texture.initFromFile("assets/cubyz/ui/file_explorer_icon.png");
+    editIcon = Texture.initFromFile("assets/cubyz/ui/edit_icon.png");
 }
 
 pub fn deinit() void {
-	deleteIcon.deinit();
-	fileExplorerIcon.deinit();
+    deleteIcon.deinit();
+    fileExplorerIcon.deinit();
+    editIcon.deinit();
 }
 
 pub fn openWorld(name: []const u8) void {
-	const clientConnection = ConnectionManager.init(0, .{}) catch |err| {
-		std.log.err("Encountered error while opening connection: {s}", .{@errorName(err)});
-		return;
-	};
+    const clientConnection = ConnectionManager.init(0, .{}) catch |err| {
+        std.log.err("Encountered error while opening connection: {s}", .{@errorName(err)});
+        return;
+    };
 
-	std.log.info("Opening world {s}", .{name});
-	main.server.thread = std.Thread.spawn(.{}, main.server.startFromNewThread, .{name, clientConnection.localPort, mode}) catch |err| {
-		std.log.err("Encountered error while starting server thread: {s}", .{@errorName(err)});
-		return;
-	};
-	main.server.thread.?.setName(main.io, "Server") catch |err| {
-		std.log.err("Failed to rename Server thread: {s}", .{@errorName(err)});
-	};
+    std.log.info("Opening world {s}", .{name});
+    main.server.thread = std.Thread.spawn(.{}, main.server.startFromNewThread, .{ name, clientConnection.localPort, mode }) catch |err| {
+        std.log.err("Encountered error while starting server thread: {s}", .{@errorName(err)});
+        return;
+    };
+    main.server.thread.?.setName(main.io, "Server") catch |err| {
+        std.log.err("Failed to rename Server thread: {s}", .{@errorName(err)});
+    };
 
-	while (!main.server.running.load(.acquire)) {
-		main.io.sleep(.fromMilliseconds(1), .awake) catch {};
-		main.heap.GarbageCollection.syncPoint();
-	}
-	const ipPort = main.stackAllocator.print("127.0.0.1:{}", .{main.server.connectionManager.localPort});
-	defer main.stackAllocator.free(ipPort);
-	const zon = main.game.testWorld.init(ipPort, clientConnection) catch |err| {
-		std.log.err("Encountered error while opening world: {s}", .{@errorName(err)});
-		return;
-	};
-	main.game.testWorld.finishHandshake(zon) catch |err| {
-		std.log.err("Encountered error while opening world: {s}", .{@errorName(err)});
-		return;
-	};
-	for (gui.openWindows.items) |openWindow| {
-		gui.closeWindowFromRef(openWindow);
-	}
-	gui.openHud();
+    while (!main.server.running.load(.acquire)) {
+        main.io.sleep(.fromMilliseconds(1), .awake) catch {};
+        main.heap.GarbageCollection.syncPoint();
+    }
+    const ipPort = main.stackAllocator.print("127.0.0.1:{}", .{main.server.connectionManager.localPort});
+    defer main.stackAllocator.free(ipPort);
+    const zon = main.game.testWorld.init(ipPort, clientConnection) catch |err| {
+        std.log.err("Encountered error while opening world: {s}", .{@errorName(err)});
+        return;
+    };
+    main.game.testWorld.finishHandshake(zon) catch |err| {
+        std.log.err("Encountered error while opening world: {s}", .{@errorName(err)});
+        return;
+    };
+    for (gui.openWindows.items) |openWindow| {
+        gui.closeWindowFromRef(openWindow);
+    }
+    gui.openHud();
 }
 
 fn openWorldWrap(index: usize) void { // TODO: Improve this situation. Maybe it makes sense to always use 2 arguments in the Callback.
-	openWorld(worldList.items[index].fileName);
+    openWorld(worldList.items[index].fileName);
 }
 
 fn deleteWorld(index: usize) void {
-	main.gui.closeWindow("delete_world_confirmation");
-	main.gui.windowlist.delete_world_confirmation.setDeleteWorldName(worldList.items[index].fileName);
-	main.gui.openWindow("delete_world_confirmation");
+    main.gui.closeWindow("delete_world_confirmation");
+    main.gui.windowlist.delete_world_confirmation.setDeleteWorldName(worldList.items[index].fileName);
+    main.gui.openWindow("delete_world_confirmation");
 }
 
 fn openFolder(index: usize) void {
-	const path = main.stackAllocator.print("{s}/saves/{s}", .{main.files.cubyzDirStr(), worldList.items[index].fileName});
-	defer main.stackAllocator.free(path);
+    const path = main.stackAllocator.print("{s}/saves/{s}", .{ main.files.cubyzDirStr(), worldList.items[index].fileName });
+    defer main.stackAllocator.free(path);
 
-	main.files.openDirInWindow(path);
+    main.files.openDirInWindow(path);
+}
+
+fn editWorld(index: usize) void {
+    main.gui.closeWindow("edit_world");
+    main.gui.windowlist.edit_world.setEditWorldName(worldList.items[index].fileName);
+    main.gui.openWindow("edit_world");
 }
 
 pub fn update() void {
-	if (needsUpdate) {
-		needsUpdate = false;
-		onClose();
-		onOpen();
-	}
+    if (needsUpdate) {
+        needsUpdate = false;
+        onClose();
+        onOpen();
+    }
 }
 
 pub fn onOpen() void {
-	buttonNameArena = main.heap.NeverFailingArenaAllocator.init(main.globalAllocator);
-	const list = VerticalList.init(.{padding, 16 + padding}, 300, 8);
-	list.add(Label.init(.{0, 0}, width, if (mode == .singleplayer) "**Select World**" else "**Select World to Host**", .center));
-	list.add(Button.initText(.{0, 0}, 128, "Create New World", .{.onAction = gui.openWindowCallback("save_creation")}));
-	readingSaves: {
-		var dir = main.files.cubyzDir().openIterableDir("saves") catch |err| {
-			list.add(Label.init(.{0, 0}, 128, "Encountered error while trying to open saves folder:", .center));
-			list.add(Label.init(.{0, 0}, 128, @errorName(err), .center));
-			break :readingSaves;
-		};
-		defer dir.close();
+    buttonNameArena = main.heap.NeverFailingArenaAllocator.init(main.globalAllocator);
+    const list = VerticalList.init(.{ padding, 16 + padding }, 300, 8);
+    list.add(Label.init(.{ 0, 0 }, width, if (mode == .singleplayer) "**Select World**" else "**Select World to Host**", .center));
+    list.add(Button.initText(.{ 0, 0 }, 128, "Create New World", .{ .onAction = gui.openWindowCallback("save_creation") }));
+    readingSaves: {
+        var dir = main.files.cubyzDir().openIterableDir("saves") catch |err| {
+            list.add(Label.init(.{ 0, 0 }, 128, "Encountered error while trying to open saves folder:", .center));
+            list.add(Label.init(.{ 0, 0 }, 128, @errorName(err), .center));
+            break :readingSaves;
+        };
+        defer dir.close();
 
-		var iterator = dir.iterate();
-		while (iterator.next(main.io) catch |err| {
-			list.add(Label.init(.{0, 0}, 128, "Encountered error while iterating over saves folder:", .center));
-			list.add(Label.init(.{0, 0}, 128, @errorName(err), .center));
-			break :readingSaves;
-		}) |entry| {
-			if (entry.kind == .directory) {
-				const worldInfoPath = main.stackAllocator.print("saves/{s}/world.zig.zon", .{entry.name});
-				defer main.stackAllocator.free(worldInfoPath);
-				const worldInfo = main.files.cubyzDir().readToZon(main.stackAllocator, worldInfoPath) catch |err| {
-					std.log.err("Couldn't open save {s}: {s}", .{worldInfoPath, @errorName(err)});
-					continue;
-				};
-				defer worldInfo.deinit(main.stackAllocator);
+        var iterator = dir.iterate();
+        while (iterator.next(main.io) catch |err| {
+            list.add(Label.init(.{ 0, 0 }, 128, "Encountered error while iterating over saves folder:", .center));
+            list.add(Label.init(.{ 0, 0 }, 128, @errorName(err), .center));
+            break :readingSaves;
+        }) |entry| {
+            if (entry.kind == .directory) {
+                const worldInfoPath = main.stackAllocator.print("saves/{s}/world.zig.zon", .{entry.name});
+                defer main.stackAllocator.free(worldInfoPath);
+                const worldInfo = main.files.cubyzDir().readToZon(main.stackAllocator, worldInfoPath) catch |err| {
+                    std.log.err("Couldn't open save {s}: {s}", .{ worldInfoPath, @errorName(err) });
+                    continue;
+                };
+                defer worldInfo.deinit(main.stackAllocator);
 
-				worldList.append(main.globalAllocator, .{
-					.fileName = main.globalAllocator.dupe(u8, entry.name),
-					.lastUsedTime = worldInfo.get(i64, "lastUsedTime") orelse 0,
-					.name = main.globalAllocator.dupe(u8, worldInfo.get([]const u8, "name") orelse entry.name),
-				});
-			}
-		}
-	}
+                worldList.append(main.globalAllocator, .{
+                    .fileName = main.globalAllocator.dupe(u8, entry.name),
+                    .lastUsedTime = worldInfo.get(i64, "lastUsedTime") orelse 0,
+                    .name = main.globalAllocator.dupe(u8, worldInfo.get([]const u8, "name") orelse entry.name),
+                });
+            }
+        }
+    }
 
-	std.sort.insertion(WorldInfo, worldList.items, {}, struct {
-		fn lessThan(_: void, lhs: WorldInfo, rhs: WorldInfo) bool {
-			return rhs.lastUsedTime -% lhs.lastUsedTime < 0;
-		}
-	}.lessThan);
+    std.sort.insertion(WorldInfo, worldList.items, {}, struct {
+        fn lessThan(_: void, lhs: WorldInfo, rhs: WorldInfo) bool {
+            return rhs.lastUsedTime -% lhs.lastUsedTime < 0;
+        }
+    }.lessThan);
 
-	for (worldList.items, 0..) |worldInfo, i| {
-		const row = HorizontalList.init();
-		row.add(Button.initText(.{0, 0}, 128, worldInfo.name, .{.onAction = .initWithInt(openWorldWrap, i)}));
-		row.add(Button.initIcon(.{8, 0}, .{16, 16}, fileExplorerIcon, .{.onAction = .initWithInt(openFolder, i)}));
-		row.add(Button.initIcon(.{8, 0}, .{16, 16}, deleteIcon, .{.onAction = .initWithInt(deleteWorld, i)}));
-		row.finish(.{0, 0}, .center);
-		list.add(row);
-	}
+    for (worldList.items, 0..) |worldInfo, i| {
+        const row = HorizontalList.init();
+        row.add(Button.initText(.{ 0, 0 }, 128, worldInfo.name, .{ .onAction = .initWithInt(openWorldWrap, i) }));
+        row.add(Button.initIcon(.{ 4, 0 }, .{ 16, 16 }, editIcon, .{ .onAction = .initWithInt(editWorld, i) }));
+        row.add(Button.initIcon(.{ 4, 0 }, .{ 16, 16 }, fileExplorerIcon, .{ .onAction = .initWithInt(openFolder, i) }));
+        row.add(Button.initIcon(.{ 4, 0 }, .{ 16, 16 }, deleteIcon, .{ .onAction = .initWithInt(deleteWorld, i) }));
+        row.finish(.{ 0, 0 }, .center);
+        list.add(row);
+    }
 
-	list.finish(.center);
-	window.rootComponent = list.toComponent();
-	window.contentSize = window.rootComponent.?.pos() + window.rootComponent.?.size() + @as(Vec2f, @splat(padding));
-	gui.updateWindowPositions();
+    list.finish(.center);
+    window.rootComponent = list.toComponent();
+    window.contentSize = window.rootComponent.?.pos() + window.rootComponent.?.size() + @as(Vec2f, @splat(padding));
+    gui.updateWindowPositions();
 }
 
 pub fn onClose() void {
-	for (worldList.items) |worldInfo| {
-		main.globalAllocator.free(worldInfo.fileName);
-		main.globalAllocator.free(worldInfo.name);
-	}
-	worldList.clearAndFree(main.globalAllocator);
-	buttonNameArena.deinit();
-	if (window.rootComponent) |*comp| {
-		comp.deinit();
-	}
+    for (worldList.items) |worldInfo| {
+        main.globalAllocator.free(worldInfo.fileName);
+        main.globalAllocator.free(worldInfo.name);
+    }
+    worldList.clearAndFree(main.globalAllocator);
+    buttonNameArena.deinit();
+    if (window.rootComponent) |*comp| {
+        comp.deinit();
+    }
 }
