dbff7b4bfeef421140bd5ee1434e53e59c67dd18
Delta <delliedelta@proton.me>
2026-08-20 20:12:46 +0200

    Remove unneeded `vec` reference

--- src/blocks.zig
+++ src/blocks.zig
@@ -29,10 +29,6 @@ const sbb = main.server.terrain.sbb;
 const blueprint = main.blueprint;
 const Assets = main.assets.Assets;
 const BlockDrop = main.server.BlockDrop;
-const vec = main.vec;
-const Vec3i = vec.Vec3i;
-const Vec3d = vec.Vec3d;
-const Vec3f = vec.Vec3f;
 
 const c = @import("c");
 
