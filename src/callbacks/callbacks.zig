0fa573ef8544c5094f1a474585cb341a0be9cd4e
Vinywar123 <bahnsenvincent@gmail.com>
2026-08-22 12:38:39 -0700

    added status effects

--- src/callbacks/callbacks.zig
+++ src/callbacks/callbacks.zig
@@ -10,6 +10,9 @@ pub const ServerBlockCallback = Callback(struct { block: Block, chunk: *main.chu
 
 pub const BlockTouchCallback = Callback(struct { entity: *main.server.Entity, source: Block, blockPos: Vec3i, deltaTime: f64 }, @import("block/touch/_list.zig"));
 
+pub const StatusUpdateCallback = Callback(struct { entity: *main.server.Entity, deltaTime: f64 }, @import("status/_list.zig"));
+pub const StatusEndCallback = Callback(struct { entity: *main.server.Entity, deltaTime: f64 }, @import("status/_list.zig"));
+
 pub const Result = enum { handled, ignored };
 
 pub fn init() void {
