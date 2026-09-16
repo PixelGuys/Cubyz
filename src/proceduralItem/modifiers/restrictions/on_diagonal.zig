9cdfc5f8b972bb0505de4891319473a0bcd83a6b
Vinywar123 <bahnsenvincent@gmail.com>
2026-09-16 12:29:47 -0700

    updated to current master

--- src/proceduralItem/modifiers/restrictions/on_diagonal.zig
+++ src/proceduralItem/modifiers/restrictions/on_diagonal.zig
@@ -14,17 +14,19 @@ const OnDiagonal = struct {
 pub fn satisfied(self: *const OnDiagonal, proceduralItem: *const ProceduralItem, x: i32, y: i32) bool {
 	var count: usize = 0;
 	const gridSize: usize = proceduralItem.materialGrid.len - 1;
-	const rangeChecked = gridSize;
+	const rangeChecked: i32 = gridSize;
 	const lowBound = 0;
 	const highBound = rangeChecked*2 + 1;
 	for (lowBound..highBound) |i| {
-		const checkedX = x + (@as(i32, @intCast(i)) - rangeChecked);
-		const checkedY = y + (@as(i32, @intCast(i)) - rangeChecked);
+		const iterator: i32 = @intCast(i);
+		const checkedX: i32 = x + (iterator - rangeChecked);
+		const checkedY: i32 = y + (iterator - rangeChecked);
 		if ((proceduralItem.getItemAt(checkedX, checkedY) orelse continue).hasTag(self.tag)) count += 1;
 	}
 	for (lowBound..highBound) |i| {
-		const checkedX = x + (@as(i32, @intCast(i)) - rangeChecked);
-		const checkedY = y - (@as(i32, @intCast(i)) - rangeChecked);
+		const iterator: i32 = @intCast(i);
+		const checkedX: i32 = x + (iterator - rangeChecked);
+		const checkedY: i32 = y + (iterator - rangeChecked);
 		if (i != 0) { // prevents double counting
 			if ((proceduralItem.getItemAt(checkedX, checkedY) orelse continue).hasTag(self.tag)) count += 1;
 		}
@@ -36,12 +38,15 @@ pub fn satisfied(self: *const OnDiagonal, proceduralItem: *const ProceduralItem,
 pub fn loadFromZon(allocator: NeverFailingAllocator, zon: ZonElement) *const OnDiagonal {
 	const result = allocator.create(OnDiagonal);
 	result.* = .{
-		.tag = main.Tag.find(zon.get([]const u8, "tag", "not specified")),
-		.amount = zon.get(usize, "amount", 8),
+		.tag = main.Tag.find(zon.get([]const u8, "tag") orelse blk: {
+			std.log.err("Missing tag field for on diagonal restriction.", .{});
+			break :blk "not specified";
+		}),
+		.amount = zon.get(usize, "amount") orelse 8,
 	};
 	return result;
 }
 
-pub fn printTooltip(self: *const OnDiagonal, outString: *main.List(u8)) void {
+pub fn printTooltip(self: *const OnDiagonal, outString: *main.ListManaged(u8)) void {
 	outString.print("{} .{s} {s}", .{self.amount, self.tag.getName(), "on diagonal axis"});
 }
