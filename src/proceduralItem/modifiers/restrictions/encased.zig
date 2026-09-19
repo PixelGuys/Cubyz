f21e2776eeecb9a07bb84e0cf9bf0239ab84b977
Vinywar123 <bahnsenvincent@gmail.com>
2026-09-19 13:03:03 -0700

    Update encased.zig

--- src/proceduralItem/modifiers/restrictions/encased.zig
+++ src/proceduralItem/modifiers/restrictions/encased.zig
@@ -34,5 +34,5 @@ pub fn loadFromZon(allocator: NeverFailingAllocator, zon: ZonElement) *const Enc
 }
 
 pub fn printTooltip(self: *const Encased, outString: *main.ListManaged(u8)) void {
-	outString.print("*encased* in {} #7eb1c9*.{s}*", .{self.amount, self.tag.getName()});
+	outString.print("#918768*encased*#808080 in {} #7eb1c9*.{s}*", .{self.amount, self.tag.getName()});
 }
