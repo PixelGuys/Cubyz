1b09c2b2feb7c47053c5ed12776b919c03de6745
Vinywar123 <bahnsenvincent@gmail.com>
2026-09-19 14:53:27 -0700

    renamed and added tool names

--- src/proceduralItem/modifiers/_list.zig
+++ src/proceduralItem/modifiers/_list.zig
@@ -1,4 +1,4 @@
 pub const bad_at = @import("bad_at.zig");
 pub const good_at = @import("good_at.zig");
 pub const single_use = @import("single_use.zig");
-pub const basic_stat = @import("basic_stat.zig");
+pub const base_stat = @import("base_stat.zig");
