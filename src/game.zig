79dcfea240a89c9b30783c0aae241e0fffefe139
Vinywar123 <bahnsenvincent@gmail.com>
2026-09-19 20:42:01 -0700

    oops update that back

--- src/game.zig
+++ src/game.zig
@@ -660,7 +660,7 @@ pub fn update(deltaTime: f64) void { // MARK: update()
 					movementDir[2] += 5.5;
 				}
 			} else if ((Player.onGround or Player.jumpCoyote > 0.0) and Player.jumpCooldown <= 0) {
-				main.audio.addSound("cubyz:jump");
+				main.audio.playSound("cubyz:jump");
 				jumping = true;
 				Player.jumpCooldown = Player.jumpCooldownConstant;
 				if (!Player.onGround) {
