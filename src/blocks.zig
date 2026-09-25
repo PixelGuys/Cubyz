724a40d4f8755610c5bf97aa31f9720b53aa38ea
Vinywar123 <bahnsenvincent@gmail.com>
2026-09-25 11:58:28 -0700

    Update blocks.zig

--- src/blocks.zig
+++ src/blocks.zig
@@ -200,8 +200,6 @@ pub fn register(_: []const u8, id: []const u8, zon: ZonElement) u16 {
 			std.log.err("Ore must have rotation mode \"cubyz:ore\"!", .{});
 			break :blk;
 		}
-		const targetBlockTags = Tag.loadTagsFromZon(main.stackAllocator, oreProperties.getChild("targetTags"));
-		defer main.stackAllocator.free(targetBlockTags);
 		ores.append(main.worldArena, .{
 			.veins = oreProperties.get(f32, "veins") orelse 0,
 			.size = oreProperties.get(f32, "size") orelse 0,
@@ -209,7 +207,7 @@ pub fn register(_: []const u8, id: []const u8, zon: ZonElement) u16 {
 			.minHeight = oreProperties.get(i32, "minHeight") orelse std.math.minInt(i32),
 			.density = oreProperties.get(f32, "density") orelse 0.5,
 			.blockType = @intCast(size),
-			.targetTags = targetBlockTags,
+			.targetTags = Tag.loadTagsFromZon(main.worldArena, oreProperties.getChild("targetTags")),
 			.seed = std.hash.Wyhash.hash(0, id),
 		});
 	}
